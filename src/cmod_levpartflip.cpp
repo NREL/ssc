55203f8875d827cc09b3a3f8965721026080ebe1
Steve Janzou <steven.janzou@nrel.gov>
2010-11-22 12:17:44 +0000

    update cmod_cashloan.cpp - added hard and soft cost and fixed sales tax total. Matches exactly with SAM rev 947. Added validation/cashloan_validation_20101121.sscdat, validation/cashloan_validation_script_20101121.ss and validation/cashloan_validation_20101121.zsam in sscdev/deploy/ that match exactly. continued work on cmod_levpartflip.cpp

--- src/cmod_levpartflip.cpp
+++ src/cmod_levpartflip.cpp
@@ -3,41 +3,140 @@
 static var_info _cm_vtab_levpartflip[] = {
 
 /*   VARTYPE           DATATYPE         NAME                         LABEL                              UNITS     META                      GROUP          REQUIRED_IF                 CONSTRAINTS                      UI_HINTS*/
-	{ SSC_INPUT,        SSC_ARRAY,      "system_enet",				"Annual energy produced by system",	"kWh",   "",                      "",             "*",						   "",                              "" },
+	{ SSC_INPUT,        SSC_ARRAY,      "system_enet",				"Annual energy produced by system",	"kWh",   "",                      "DHF",             "*",						   "",                              "" },
 /* constraint is > 0 */
-	{ SSC_INPUT,        SSC_NUMBER,     "system_nameplate",			"System nameplate capacity",		"kW",    "",                      "",             "*",						   "MIN=1",                         "" },
-/* want analysis years determined by enet length - system degradation, availablity compute module output
-	{ SSC_INPUT,        SSC_NUMBER,     "analysis_years",           "Number of years in analysis",     "years",  "",                      "",             "?=30",                      "INTEGER,MIN=1,MAX=40",          "" }, */
-	{ SSC_INPUT,        SSC_NUMBER,     "inflation",			    "Inflation rate",					"%",     "",                      "",             "*",						   "MIN=0,MAX=100",					"" },
-	{ SSC_INPUT,        SSC_NUMBER,     "discount_real",			"Real discount rate",				"%",     "",                      "",             "*",						   "MIN=0,MAX=100",					"" },
-	{ SSC_INPUT,        SSC_NUMBER,     "sales_tax_rate",		    "Sales tax rate",	    			"%",     "",                      "",             "*",						   "MIN=0,MAX=100",					"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "system_nameplate",			"System nameplate capacity",		"kW",    "",                      "DHF",             "*",						   "MIN=1",                         "" },
+/* want analysis years determined by enet length - system degradation, availablity compute module output */
+	{ SSC_INPUT,        SSC_NUMBER,     "analysis_years",           "Number of years in analysis",     "years",  "",                      "DHF",             "?=30",                      "INTEGER,MIN=1,MAX=40",          "" }, 
+	{ SSC_INPUT,        SSC_NUMBER,     "inflation",			    "Inflation rate",					"%",     "",                      "DHF",             "*",						   "MIN=0,MAX=100",					"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "discount_real",			"Real discount rate",				"%",     "",                      "DHF",             "*",						   "MIN=0,MAX=100",					"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "sales_tax_rate",		    "Sales tax rate",	    			"%",     "",                      "DHF",             "*",						   "MIN=0,MAX=100",					"" },
 
 /* costs - to be updated based on meetings with DHF */
-	{ SSC_INPUT,        SSC_NUMBER,     "cost_gen_equip",           "Generation equiptment cost",		"$",	 "",					  "",             "?=24000000",              "MIN=0",                         "" },
-	{ SSC_INPUT,        SSC_NUMBER,     "cost_bop",					"Balance of plant cost",			"$",	 "",					  "",             "?=8000000",               "MIN=0",                         "" },
-	{ SSC_INPUT,        SSC_NUMBER,     "cost_network",             "Network upgrade cost",				"$",	 "",					  "",             "?=3500000",               "MIN=0",                         "" },
-	{ SSC_INPUT,        SSC_NUMBER,     "percent_contingency",      "Contingency percent",				"%",	 "",					  "",             "?=1",                       "MIN=0,MAX=100",		        	"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "cost_gen_equip",           "Generation equiptment cost",		"$",	 "",					  "DHF",             "?=24000000",              "MIN=0",                         "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "cost_bop",					"Balance of plant cost",			"$",	 "",					  "DHF",             "?=8000000",               "MIN=0",                         "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "cost_network",             "Network upgrade cost",				"$",	 "",					  "DHF",             "?=3500000",               "MIN=0",                         "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "percent_contingency",      "Contingency percent",				"%",	 "",					  "DHF",             "?=1",                       "MIN=0,MAX=100",		        	"" },
 
-	{ SSC_INPUT,        SSC_NUMBER,     "cost_developer",           "Developer cost & fees",		    "$",	 "",					  "",             "?=2000000",               "MIN=0",                         "" },
-	{ SSC_INPUT,        SSC_NUMBER,     "cost_land_improve",        "Land improvements cost",		    "$",	 "",					  "",             "?=200000",                 "MIN=0",                         "" },
-	{ SSC_INPUT,        SSC_NUMBER,     "cost_other",               "Other cost",						"$",	 "",					  "",             "?=75000",                  "MIN=0",                         "" },
-	{ SSC_INPUT,        SSC_NUMBER,     "percent_taxable",          "Taxable cost",				        "%",	 "",					  "",             "?=100",                     "MIN=0,MAX=100",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "cost_developer",           "Developer cost & fees",		    "$",	 "",					  "DHF",             "?=2000000",               "MIN=0",                         "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "cost_land_improve",        "Land improvements cost",		    "$",	 "",					  "DHF",             "?=200000",                 "MIN=0",                         "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "cost_other",               "Other cost",						"$",	 "",					  "DHF",             "?=75000",                  "MIN=0",                         "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "percent_taxable",          "Taxable cost",				        "%",	 "",					  "DHF",             "?=100",                     "MIN=0,MAX=100",      			"" },
 
+/* inputs in DHF model not currently in SAM 11/15/10 */
+	{ SSC_INPUT,        SSC_NUMBER,     "reserves_interest",        "Interest on reserves",				"%",	 "",					  "DHF",             "?=1.75",                     "MIN=0,MAX=100",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "prop_tax_cost_assessed",   "Percent of pre-financing costs assessed",	"%",					  "DHF",			 "",             "?=95",                     "MIN=0,MAX=100",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "prop_tax_assessed_decline","Assessed value annual decline",	"%",	 "",					  "DHF",             "?=95",                     "MIN=0,MAX=100",      			"" },
+/* DHF market specific inputs - leveraged partnership flip */
+	{ SSC_INPUT,        SSC_NUMBER,		"ppa_soln_mode",            "PPA solution mode",                "0/1",   "0=specify ppa,1=solve ppa", "DHF",         "?=0",                     "INTEGER,MIN=0,MAX=1",            "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "ppa_price",				"Initial year PPA price",			"cents/kWh",	 "",			  "DHF",			 "ppa_soln_mode=1",         "",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "ppa_escalation",           "PPA escalation",					"%",	 "",					  "DHF",             "?=0",                     "MIN=0,MAX=100",      			"" },
+/* DHF construction period */
+	{ SSC_INPUT,        SSC_NUMBER,     "constr_period",            "Construction period",				"months", "",				      "DHF",             "?=10",					"INTEGER,MIN=0",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "constr_int_rate",          "Construction interest rate",		"%",	 "",					  "DHF",             "?=4",                     "MIN=0,MAX=100",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "ppa_escalation",           "Construction up-front fee",    	"%",	 "",					  "DHF",             "?=1",                     "MIN=0,MAX=100",      			"" },
+/* DHF term financing */
+	{ SSC_INPUT,        SSC_NUMBER,     "term_tenor",               "Term financing tenor",				"years", "",				      "DHF",             "?=10",					"INTEGER,MIN=0",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "term_int_rate",            "Term financing interest rate",		"%",	 "",					  "DHF",             "?=8.5",                   "MIN=0,MAX=100",      			"" },
+	{ SSC_INPUT,        SSC_NUMBER,     "dscr",						"Debt service coverage ratio",		"",	     "",				      "DHF",             "?=1.5",					"MIN=0",      			        "" },
+	{ SSC_INPUT,        SSC_NUMBER,     "dscr_reserve",				"Debt service reserve account",		"months P&I","",			      "DHF",             "?=6",					    "INTEGER,MIN=0",      			        "" },
 
-/* o and m */
-
-/* market specific inputs */
 
 /* intermediate outputs */
-	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_contingency",        "Contingency cost",                 "$",     "",					   "",			   "*",                         "",                             "" },
-	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_hard",               "Hard cost",                        "$",     "",					   "",			   "*",                         "",                             "" },
-	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_soft",               "Soft cost",                        "$",     "",					   "",			   "*",                         "",                             "" },
-	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_salestax",           "Sales tax",                        "$",     "",					   "",			   "*",                         "",                             "" },
-	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_installed",          "Installed cost",                   "$",     "",					   "",			   "*",                         "",                             "" },
-	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_installedperwatt",   "Installed cost",                   "$/W",   "",					   "",			   "*",                         "",                             "" },
-	{ SSC_OUTPUT,       SSC_NUMBER,      "discount_nominal",        "Nominal discount rate",            "%",     "",					   "",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_contingency",        "Contingency cost",                 "$",     "",					   "DHF",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_hard",               "Hard cost",                        "$",     "",					   "DHF",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_soft",               "Soft cost",                        "$",     "",					   "DHF",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_salestax",           "Sales tax",                        "$",     "",					   "DHF",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_installed",          "Installed cost",                   "$",     "",					   "DHF",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "cost_installedperwatt",   "Installed cost per watt",          "$/W",   "",					   "DHF",			   "*",                         "",                             "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "discount_nominal",        "Nominal discount rate",            "%",     "",					   "DHF",			   "*",                         "",                             "" },
 
 var_info_invalid };
+
+extern var_info
+	vtab_standard_financial[],
+	vtab_standard_loan[],
+	vtab_oandm[],
+	vtab_depreciation[],
+	vtab_utility_rate[],
+	vtab_tax_credits[],
+	vtab_payment_incentives[];
+
+enum {
+	CF_energy_net,
+	CF_energy_value,
+
+	CF_om_fixed_expense,
+	CF_om_production_expense,
+	CF_om_capacity_expense,
+	CF_om_fuel_expense,
+	CF_property_tax_expense,
+	CF_insurance_expense,
+	CF_operating_expenses,
+
+	CF_deductible_expenses,
+
+	CF_debt_balance,
+	CF_debt_payment_interest,
+	CF_debt_payment_principal,
+	CF_debt_payment_total,
+	
+	CF_ibi_fed_amt,
+	CF_ibi_sta_amt,
+	CF_ibi_uti_amt,
+	CF_ibi_oth_amt,
+	CF_ibi_fed_per,
+	CF_ibi_sta_per,
+	CF_ibi_uti_per,
+	CF_ibi_oth_per,
+	CF_ibi_total,
+	
+	CF_cbi_fed,
+	CF_cbi_sta,
+	CF_cbi_uti,
+	CF_cbi_oth,
+	CF_cbi_total,
+
+	CF_pbi_fed,
+	CF_pbi_sta,
+	CF_pbi_uti,
+	CF_pbi_oth,
+	CF_pbi_total,
+	
+	CF_ptc_fed,
+	CF_ptc_sta,
+	
+	CF_itc_fed_amt,
+	CF_itc_fed_per,
+	CF_itc_fed_total,
+
+	CF_itc_sta_amt,
+	CF_itc_sta_per,
+	CF_itc_sta_total,
+	
+	CF_sta_depr_sched,
+	CF_sta_depreciation,
+	CF_sta_incentive_income_less_deductions,
+	CF_sta_taxable_income_less_deductions,
+	CF_sta_tax_savings,
+	
+	CF_fed_depr_sched,
+	CF_fed_depreciation,
+	CF_fed_incentive_income_less_deductions,
+	CF_fed_taxable_income_less_deductions,
+	CF_fed_tax_savings,
+
+	CF_sta_and_fed_tax_savings,
+	CF_after_tax_net_equity_cost_flow,
+	CF_after_tax_cash_flow,
+
+	CF_payback_with_expenses,
+	CF_cumulative_payback_with_expenses,
+	
+	CF_payback_without_expenses,
+	CF_cumulative_payback_without_expenses,
+
+	CF_max };
 
 
 
@@ -54,7 +153,7 @@ public:
 	{
 		double inf = as_double("inflation") / 100.0;
 		double disc_real = as_double("discount_real") / 100.0;
-		assign( "discount_nominal", var_data( (1+inf)*(1+disc_real)-1 ) );
+		assign( "discount_nominal", var_data((ssc_number_t) ((1+inf)*(1+disc_real)-1) ) );
 
 		double gen = as_double("cost_gen_equip");
 		double bop = as_double("cost_bop");
@@ -72,16 +171,16 @@ public:
 
 		double cost_soft = dev + land + other;
 		double cost_salestax = (cost_hard + cost_cont + cost_soft) * cost_taxable * sales_tax_rate;
-		assign( "cost_contingency", var_data( cost_cont ) );
-		assign( "cost_hard", var_data( cost_hard + cost_cont) );
-		assign( "cost_salestax", var_data( cost_salestax ) );
-		assign( "cost_soft", var_data( cost_soft + cost_salestax ) );
+		assign( "cost_contingency", var_data((ssc_number_t) cost_cont ) );
+		assign( "cost_hard", var_data( (ssc_number_t)(cost_hard + cost_cont)) );
+		assign( "cost_salestax", var_data((ssc_number_t)cost_salestax ) );
+		assign( "cost_soft", var_data((ssc_number_t) (cost_soft + cost_salestax) ) );
 
 		double cost_installed = cost_soft + cost_salestax + cost_hard + cost_cont;
-		assign( "cost_installed", var_data( cost_installed ) );
+		assign( "cost_installed", var_data((ssc_number_t) cost_installed ) );
 
 		double nameplate = as_double("system_nameplate");
-		assign( "cost_installedperwatt", var_data( cost_installed / nameplate / 1000.0 ) );
+		assign( "cost_installedperwatt", var_data((ssc_number_t)( cost_installed / nameplate / 1000.0 ) ));
 	}
 };
 
