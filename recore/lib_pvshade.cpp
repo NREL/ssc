86d6e1c3ad8c2f205613f2574ae8bd0b914664e8
Steve Janzou <steven.janzou@nrel.gov>
2012-03-02 12:59:57 +0000

    lib_pvshade stub added for porting of geometry from sam_shading_type241.f90

---
+++ recore/lib_pvshade.cpp
@@ -0,0 +1,22 @@
+#include "lib_pvshade.h"
+#include <math.h>
+#include <limits>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327
+#endif
+
+
+selfshade_t::selfshade_t()
+{
+}
+
+
+selfshade_t::selfshade_t( ssarrdat &arr,
+		double solzen,
+		double solazi,
+		double beamnorm,
+		double globhoriz )
+{
+}
+
---
+++ recore/lib_pvshade.h
@@ -0,0 +1,41 @@
+#ifndef __pvshade_h
+#define __pvshade_h
+
+#include <string>
+
+
+//   Porting of sam_shading_type241.f90 to new orientation
+
+
+struct ssarrdat
+{
+	double tilt, azimuth;
+	int nmodx, nmody, nrows;
+	double length, width;
+	int mod_orient, str_orient;
+	double row_space, mod_space;
+	int ncellx, ncelly, ndiode;
+	double slope_ns, slope_ew;
+};
+
+
+
+class selfshade_t
+{
+public:
+	selfshade_t();
+	selfshade_t( ssarrdat &arr,
+		double solzen,
+		double solazi,
+		double beamnorm,
+		double globhoriz  );
+
+	double dc_derate() {return m_dc_derate;}
+
+private:
+	double m_dc_derate;
+};
+
+
+
+#endif
