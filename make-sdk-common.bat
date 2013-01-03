3581807fc99c39599c01c8a3522d64d3b4121a62
Aron Dobos <aron.dobos@outlook.com>
2013-01-03 21:25:49 +0000

    Updated documentation and scripts on how to build SSC SDK release package.

---
+++ make-sdk-common.bat
@@ -0,0 +1,17 @@
+copy matlab\ssccall.m sdk-release\languages\matlab
+copy matlab\modules.m sdk-release\languages\matlab
+copy matlab\pvwatts.m sdk-release\languages\matlab
+copy python\sscapi.py sdk-release\languages\python
+
+copy examples\abilene.tm2 sdk-release\examples
+copy examples\daggett.tm2 sdk-release\examples
+copy examples\rocksprings.tm2 sdk-release\examples
+copy examples\example1_pvwatts.c sdk-release\examples
+copy examples\pvwattsfunc_example.c sdk-release\examples
+copy examples\ver.c sdk-release\examples
+copy examples\pvsamv1_c3d_comppa.lk sdk-release\examples
+copy examples\pvsamv1_c3d_rescom.lk sdk-release\examples
+copy examples\pvwattsfunc_test.lk sdk-release\examples
+
+copy ssc\sscapi.h sdk-release\
+copy doc\ssc_guide.pdf sdk-release\
