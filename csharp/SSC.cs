ade01ba62455210ff727d0729c1ce4b167aa3279
Steve Janzou <steven.janzou@nrel.gov>
2013-01-19 13:04:03 +0000

    Update and simplify c# wrapper - one file with two classes. Still need to add several functions to c# class and test. PVWattsFunc working.

--- csharp/SSC.cs
+++ csharp/SSC.cs
@@ -7,6 +7,196 @@ using System.Runtime.InteropServices;
 
 namespace CS_SSC_API
 {
+
+    class sscapiPINVOKE
+    {
+
+
+        static sscapiPINVOKE()
+        {
+        }
+
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_version")]
+        public static extern int ssc_version();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_build_info")]
+        public static extern IntPtr ssc_build_info();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_INVALID_get")]
+        public static extern int SSC_INVALID_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_STRING_get")]
+        public static extern int SSC_STRING_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_NUMBER_get")]
+        public static extern int SSC_NUMBER_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_ARRAY_get")]
+        public static extern int SSC_ARRAY_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_MATRIX_get")]
+        public static extern int SSC_MATRIX_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_TABLE_get")]
+        public static extern int SSC_TABLE_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_create")]
+        public static extern IntPtr ssc_data_create();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_free")]
+        public static extern void ssc_data_free(HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_clear")]
+        public static extern void ssc_data_clear(HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_unassign")]
+        public static extern void ssc_data_unassign(HandleRef cxtData, string variableName);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_query")]
+        public static extern int ssc_data_query(HandleRef cxtData, string variableName);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_first")]
+        public static extern string ssc_data_first(HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_next")]
+        public static extern string ssc_data_next(HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_set_string")]
+        public static extern void ssc_data_set_string(HandleRef cxtData, string name, string value);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_set_number")]
+        public static extern void ssc_data_set_number(HandleRef cxtData, string name, float value);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_set_array")]
+        public static extern void ssc_data_set_array(HandleRef cxtData, string name, [In, MarshalAs(UnmanagedType.LPArray)]float[] array, int length);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_set_matrix")]
+        public static extern void ssc_data_set_matrix(HandleRef cxtData, string name, [In, MarshalAs(UnmanagedType.LPArray)]float[][] matrix, int nRows, int nCols);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_set_table")]
+        public static extern void ssc_data_set_table(HandleRef cxtData, string name, HandleRef cxtTable);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_get_string")]
+        public static extern string ssc_data_get_string(HandleRef cxtData, string name);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_get_number")]
+        public static extern int ssc_data_get_number(HandleRef cxt_dat, string name, out float number);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_get_array")]
+        public static extern IntPtr ssc_data_get_array(HandleRef cxt_data, string name, out int len);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_get_matrix")]
+        public static extern IntPtr ssc_data_get_matrix(HandleRef cxtData, string name, out int nRows, out int nCols);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_data_get_table")]
+        public static extern IntPtr ssc_data_get_table(HandleRef cxtData, string name);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_entry")]
+        public static extern IntPtr ssc_module_entry(int moduleIndex);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_entry_name")]
+        public static extern IntPtr ssc_entry_name(HandleRef cxtEntry);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_entry_description")]
+        public static extern IntPtr ssc_entry_description(HandleRef cxtEntry);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_entry_version")]
+        public static extern int ssc_entry_version(HandleRef cxtEntry);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_create")]
+        public static extern IntPtr ssc_module_create(string moduleName);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_free")]
+        public static extern void ssc_module_free(HandleRef cxtModule);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_INPUT_get")]
+        public static extern int SSC_INPUT_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_OUTPUT_get")]
+        public static extern int SSC_OUTPUT_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_INOUT_get")]
+        public static extern int SSC_INOUT_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_var_info")]
+        public static extern IntPtr ssc_module_var_info(HandleRef cxtInfo, int jarg2);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_var_type")]
+        public static extern int ssc_info_var_type(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_data_type")]
+        public static extern int ssc_info_data_type(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_name")]
+        public static extern string ssc_info_name(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_label")]
+        public static extern string ssc_info_label(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_units")]
+        public static extern string ssc_info_units(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_meta")]
+        public static extern string ssc_info_meta(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_group")]
+        public static extern string ssc_info_group(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_required")]
+        public static extern string ssc_info_required(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_constraints")]
+        public static extern string ssc_info_constraints(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_info_uihint")]
+        public static extern string ssc_info_uihint(HandleRef cxtInfo);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_exec_simple")]
+        public static extern int ssc_module_exec_simple(string moduleName, HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_exec_simple_nothread")]
+        public static extern string ssc_module_exec_simple_nothread(string moduleName, HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_LOG_get")]
+        public static extern int SSC_LOG_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_UPDATE_get")]
+        public static extern int SSC_UPDATE_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_EXECUTE_get")]
+        public static extern int SSC_EXECUTE_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_exec")]
+        public static extern int ssc_module_exec(HandleRef cxtModule, HandleRef cxtData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_exec_with_handler")]
+        public static extern int ssc_module_exec_with_handler(HandleRef cxtModule, HandleRef cxtData, HandleRef cxtHandler, HandleRef cxtUserData);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_extproc_output")]
+        public static extern void ssc_module_extproc_output(HandleRef cxtModule, string outputLine);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_NOTICE_get")]
+        public static extern int SSC_NOTICE_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_WARNING_get")]
+        public static extern int SSC_WARNING_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SSC_ERROR_get")]
+        public static extern int SSC_ERROR_get();
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ssc_module_log")]
+        public static extern string ssc_module_log(HandleRef cxtModule, int index, HandleRef messageType, [Out, MarshalAs(UnmanagedType.LPArray)]float[] time);
+
+        [DllImport("ssc.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "__ssc_segfault")]
+        public static extern void __ssc_segfault();
+    }
+    
+    
+    
+    
+    
+    
     public class SSC 
     {
         private HandleRef ssc_data_ptr;
@@ -51,16 +241,16 @@ namespace CS_SSC_API
       public float GetNumber(String name)
       {
           float result = 0;
-          float[] arr = new float[1];
+          float value=0;
 
-          int res = sscapiPINVOKE.ssc_data_get_number(ssc_data_ptr, name, arr);
+          int res = sscapiPINVOKE.ssc_data_get_number(ssc_data_ptr, name, out value);
           if (res == 0)
           {
-              result = 0;
+              result = float.NaN;
           }
           else
           {
-              result = arr[0];
+              result = value;
           }
           return result;
       }
