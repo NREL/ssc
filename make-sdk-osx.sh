4b80c349b9002581e1bd6fcce7b60a3a29c82cdc
Steve Janzou <steven.janzou@nrel.gov>
2013-01-31 14:21:45 +0000

    Updated sdk make file for OSX

--- make-sdk-osx.sh
+++ make-sdk-osx.sh
@@ -1,3 +1,3 @@
 cp -v mac_dylib/ssc64.dylib sdk-release/osx64/ssc64.dylib
 mkdir -p sdk-release/osx64/SSCdev.app
-cp -v -R dev/SSCdev.app/* sdk-release/osx64/SSCdev.app
+cp -v -R dev/osx_wx3/SSCdev.app/* sdk-release/osx64/SSCdev.app
