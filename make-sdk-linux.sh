2f7216c6259bef7d2981a14e7e7992f7bba1353f
Steve Janzou <steven.janzou@nrel.gov>
2013-02-04 13:40:14 +0000

    Update linux sdk shell file for SSCDev

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -1,6 +1,6 @@
 # script to copy binaries to <ssc>/sdk-release folder
 cp -v linux_so/ssc64.so sdk-release/linux64/ssc64.so
-cp -v dev/linux_wx3/SSCdev sdk-release/linux64/sscdev64.bin
+cp -v dev/linux_wx3/SSCdev sdk-release/linux64/SSCdev
 file sdk-release/linux64/sscdev64.bin > sdk-release/linux64/filetypes.txt
 file sdk-release/linux64/ssc64.so >> sdk-release/linux64/filetypes.txt
 echo "Dynamic library dependencies: compiled on Fedora 16" > sdk-release/linux64/dependencies.txt
