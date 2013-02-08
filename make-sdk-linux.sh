955ac4cbee955c9f2f1f756a2b27420f18e9d9c1
Steve Janzou <steven.janzou@nrel.gov>
2013-02-08 11:26:53 +0000

    CentOS update of ssc sdk

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -3,7 +3,7 @@ cp -v linux_so/ssc64.so sdk-release/linux64/ssc64.so
 cp -v dev/linux_wx3/SSCdev sdk-release/linux64/SSCdev
 file sdk-release/linux64/SSCdev > sdk-release/linux64/filetypes.txt
 file sdk-release/linux64/ssc64.so >> sdk-release/linux64/filetypes.txt
-echo "Dynamic library dependencies: compiled on Fedora 16" > sdk-release/linux64/dependencies.txt
+echo "Dynamic library dependencies: compiled on CentOS 6.3" > sdk-release/linux64/dependencies.txt
 echo "SSCdev:" >> sdk-release/linux64/dependencies.txt
 ldd sdk-release/linux64/SSCdev >> sdk-release/linux64/dependencies.txt
 echo "ssc64.so:" >> sdk-release/linux64/dependencies.txt
