2fb1e45b4a7a4e7d28e08bcc3893c86c9d10acbc
Steve Janzou <steven.janzou@nrel.gov>
2013-02-08 11:16:47 +0000

    Updated make-sdk-linux.sh file - sscDev instead of sscDev.bin

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -4,8 +4,8 @@ cp -v dev/linux_wx3/SSCdev sdk-release/linux64/SSCdev
 file sdk-release/linux64/sscdev64.bin > sdk-release/linux64/filetypes.txt
 file sdk-release/linux64/ssc64.so >> sdk-release/linux64/filetypes.txt
 echo "Dynamic library dependencies: compiled on Fedora 16" > sdk-release/linux64/dependencies.txt
-echo "sscdev64.bin:" >> sdk-release/linux64/dependencies.txt
-ldd sdk-release/linux64/sscdev64.bin >> sdk-release/linux64/dependencies.txt
+echo "sscdev64:" >> sdk-release/linux64/dependencies.txt
+ldd sdk-release/linux64/sscdev64 >> sdk-release/linux64/dependencies.txt
 echo "ssc64.so:" >> sdk-release/linux64/dependencies.txt
 ldd sdk-release/linux64/ssc64.so >> sdk-release/linux64/dependencies.txt
 
