5114044de984c3d471ea1c409d8a72a805fb5c2b
Aron Dobos <aron.dobos@outlook.com>
2013-07-26 14:16:59 +0000

    bug fix in pv6parmod to define wdir input variable revision in script to build windows sdk release - use ssc instead of renaming to ssc32 or ssc64

--- make-sdk-win.bat
+++ make-sdk-win.bat
@@ -1,10 +1,10 @@
-copy Win32\Release\ssc.dll sdk-release\win32\ssc32.dll
-copy Win32\Release\ssc.exp sdk-release\win32\ssc32.exp
-copy Win32\Release\ssc.lib sdk-release\win32\ssc32.lib
-copy Win32\Release\dev.exe sdk-release\win32\sscdev32.exe
+copy Win32\Release\ssc.dll sdk-release\win32\ssc.dll
+copy Win32\Release\ssc.exp sdk-release\win32\ssc.exp
+copy Win32\Release\ssc.lib sdk-release\win32\ssc.lib
+copy Win32\Release\dev.exe sdk-release\win32\sscdev.exe
 
-copy x64\Release\ssc.dll sdk-release\win64\ssc64.dll
-copy x64\Release\ssc.exp sdk-release\win64\ssc64.exp
-copy x64\Release\ssc.lib sdk-release\win64\ssc64.lib
-copy x64\Release\dev.exe sdk-release\win64\sscdev64.exe
+copy x64\Release\ssc.dll sdk-release\win64\ssc.dll
+copy x64\Release\ssc.exp sdk-release\win64\ssc.exp
+copy x64\Release\ssc.lib sdk-release\win64\ssc.lib
+copy x64\Release\dev.exe sdk-release\win64\sscdev.exe
 
