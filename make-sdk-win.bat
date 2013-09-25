4e64133820c3c0366df66a40f4c50ffb0e060cfa
Aron Dobos <aron.dobos@outlook.com>
2013-09-25 20:36:55 +0000

    restructured build files organization - working on vc2012/win32/x64

--- make-sdk-win.bat
+++ make-sdk-win.bat
@@ -1,10 +1,10 @@
-copy Win32\Release\ssc.dll sdk-release\win32\ssc.dll
-copy Win32\Release\ssc.exp sdk-release\win32\ssc.exp
-copy Win32\Release\ssc.lib sdk-release\win32\ssc.lib
-copy Win32\Release\dev.exe sdk-release\win32\sscdev.exe
+copy build_vc2012\Win32\Release\ssc.dll sdk-release\win32\ssc.dll
+copy build_vc2012\Win32\Release\ssc.exp sdk-release\win32\ssc.exp
+copy build_vc2012\Win32\Release\ssc.lib sdk-release\win32\ssc.lib
+copy build_vc2012\Win32\Release\dev.exe sdk-release\win32\sscdev.exe
 
-copy x64\Release\ssc.dll sdk-release\win64\ssc.dll
-copy x64\Release\ssc.exp sdk-release\win64\ssc.exp
-copy x64\Release\ssc.lib sdk-release\win64\ssc.lib
-copy x64\Release\dev.exe sdk-release\win64\sscdev.exe
+copy build_vc2012\x64\Release\ssc.dll sdk-release\win64\ssc.dll
+copy build_vc2012\x64\Release\ssc.exp sdk-release\win64\ssc.exp
+copy build_vc2012\x64\Release\ssc.lib sdk-release\win64\ssc.lib
+copy build_vc2012\x64\Release\dev.exe sdk-release\win64\sscdev.exe
 
