72b4484aa5ddd512d1d2df4ebc51c47ac1b4f5f7
Steve Janzou <steven.janzou@nrel.gov>
2013-10-02 08:50:44 +0000

    Remove old project files and updates ssc_w_tcs_vc2012.sln to include tcs projects and to build all on all platforms and place files in correct locations. If waterprop issue then double click the ssc\tcs\waterprop\runme_VS2012.bat file after reviewing the ssc\tcs\waterprop\README.txt file.

--- tcs/waterprop/waterprop.bat
+++ tcs/waterprop/waterprop.bat
@@ -8,8 +8,10 @@ copy /y c:\mingw\bin\libquadmath-0.dll ..\win32\Release
 mingw32-make clean
 mingw32-make
 mingw32-make vclib32
-copy /y waterprop32.dll ..\Win32\Debug
-copy /y waterprop32.dll ..\Win32\Release
+copy /y waterprop32.dll ..\..\build_vc2012\Win32\Debug
+copy /y waterprop32.dll ..\..\build_vc2012\Win32\Release
+copy /y waterprop32.lib ..\..\build_vc2012\Win32\Debug
+copy /y waterprop32.lib ..\..\build_vc2012\Win32\Release
 
 
 mkdir ..\x64
@@ -22,5 +24,7 @@ copy /y c:\mingw64\bin\libquadmath-0.dll ..\x64\Release
 mingw32-make CFG=64 clean
 mingw32-make CFG=64
 mingw32-make CFG=64 vclib64
-copy /y waterprop64.dll ..\x64\Debug
-copy /y waterprop64.dll ..\x64\Release
+copy /y waterprop64.dll ..\..\build_vc2012\x64\Debug
+copy /y waterprop64.dll ..\..\build_vc2012\x64\Release
+copy /y waterprop64.lib ..\..\build_vc2012\x64\Debug
+copy /y waterprop64.lib ..\..\build_vc2012\x64\Release
