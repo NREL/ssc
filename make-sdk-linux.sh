0a184ca149ae6b538609b5d3ad6b44468f716373
Aron Dobos <aron.dobos@outlook.com>
2013-11-02 02:46:58 +0000

    updated sdk-release package scripts.

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -1,11 +1,11 @@
 # script to copy binaries to <ssc>/sdk-release folder
-cp -v linux_so/ssc64.so sdk-release/linux64/ssc64.so
-cp -v dev/linux_wx3/SSCdev sdk-release/linux64/SSCdev
-file sdk-release/linux64/SSCdev > sdk-release/linux64/filetypes.txt
-file sdk-release/linux64/ssc64.so >> sdk-release/linux64/filetypes.txt
+cp -v build_linux/ssc.so sdk-release/linux64/ssc.so
+cp -v build_linux/SDKtool sdk-release/linux64/SDKtool
+file sdk-release/linux64/SDKtool > sdk-release/linux64/filetypes.txt
+file sdk-release/linux64/ssc.so >> sdk-release/linux64/filetypes.txt
 echo "Dynamic library dependencies: compiled on CentOS 6.3" > sdk-release/linux64/dependencies.txt
-echo "SSCdev:" >> sdk-release/linux64/dependencies.txt
-ldd sdk-release/linux64/SSCdev >> sdk-release/linux64/dependencies.txt
-echo "ssc64.so:" >> sdk-release/linux64/dependencies.txt
-ldd sdk-release/linux64/ssc64.so >> sdk-release/linux64/dependencies.txt
+echo "SDKtool:" >> sdk-release/linux64/dependencies.txt
+ldd sdk-release/linux64/SDKtool >> sdk-release/linux64/dependencies.txt
+echo "ssc.so:" >> sdk-release/linux64/dependencies.txt
+ldd sdk-release/linux64/ssc.so >> sdk-release/linux64/dependencies.txt
 
