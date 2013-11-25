4398d94a031537c313abd043e8bfc4d68578fc37
Steve Janzou <steven.janzou@nrel.gov>
2013-11-25 07:31:24 +0000

    2013.9.20 SDK update - linux file copy for all sdk-release files to use no 32 or 64 extensions in names

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -2,10 +2,10 @@
 cp -v linux_so/ssc64.so sdk-release/linux64/ssc.so
 cp -v dev/linux_wx3/SSCdev sdk-release/linux64/SSCdev
 file sdk-release/linux64/SSCdev > sdk-release/linux64/filetypes.txt
-file sdk-release/linux64/ssc64.so >> sdk-release/linux64/filetypes.txt
+file sdk-release/linux64/ssc.so >> sdk-release/linux64/filetypes.txt
 echo "Dynamic library dependencies: compiled on CentOS 6.3" > sdk-release/linux64/dependencies.txt
 echo "SSCdev:" >> sdk-release/linux64/dependencies.txt
 ldd sdk-release/linux64/SSCdev >> sdk-release/linux64/dependencies.txt
-echo "ssc64.so:" >> sdk-release/linux64/dependencies.txt
-ldd sdk-release/linux64/ssc64.so >> sdk-release/linux64/dependencies.txt
+echo "ssc.so:" >> sdk-release/linux64/dependencies.txt
+ldd sdk-release/linux64/ssc.so >> sdk-release/linux64/dependencies.txt
 
