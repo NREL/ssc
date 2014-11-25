9bbaa59d68ffdc880489047cbfc4f08e63c748d1
Aron Dobos <aron.dobos@outlook.com>
2014-11-25 01:10:53 +0000

    Linux64 SDK Release update 2014-11-24

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -3,7 +3,7 @@ cp -v build_linux/ssc.so sdk-release/linux64/ssc.so
 cp -v build_linux/SDKtool sdk-release/linux64/SDKtool
 file sdk-release/linux64/SDKtool > sdk-release/linux64/filetypes.txt
 file sdk-release/linux64/ssc.so >> sdk-release/linux64/filetypes.txt
-echo "Dynamic library dependencies: compiled on CentOS 6.3" > sdk-release/linux64/dependencies.txt
+echo "Dynamic library dependencies: compiled on CentOS 6.4" > sdk-release/linux64/dependencies.txt
 echo "SDKtool:" >> sdk-release/linux64/dependencies.txt
 ldd sdk-release/linux64/SDKtool >> sdk-release/linux64/dependencies.txt
 echo "ssc.so:" >> sdk-release/linux64/dependencies.txt
