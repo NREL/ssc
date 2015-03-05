91a8c73ef9f8303859465030147dd31b8c083c21
Nick DiOrio <nicholas.diorio@nrel.gov>
2015-03-05 18:17:20 +0000

    Added lifetime (Rainflow Counting) model

--- shared/lib_battery.cpp
+++ shared/lib_battery.cpp
@@ -17,6 +17,19 @@ capacity_t::capacity_t(double q20, double I20, double V)
 	_DOD = 0;
 }
 
+bool capacity_t::chargeChanged()
+{
+	return _chargeChange;
+}
+
+double capacity_t::getDOD()
+{
+	return _DOD;
+}
+/*
+Define KiBam Capacity Model
+*/
+
 capacity_kibam_t::capacity_kibam_t(double q20, double I20, double V, double t1, double t2, double q1, double q2) : 
 capacity_t(q20, I20, V)
 {
@@ -36,6 +49,8 @@ capacity_t(q20, I20, V)
 	_q0 = q20;
 	_q1_0 = q20*_c;
 	_q2_0 = _q0 - _q1_0;
+	_chargeChange = false;
+	_prev_charging = false;
 
 	// Initialize other variables
 	// Assumes initial current is 20 hour discharge current
@@ -140,6 +155,7 @@ output* capacity_kibam_t::updateCapacity(double P, double V, double dt)
 	double Ic = 0.;
 	double q1 = 0.;
 	double q2 = 0.;
+	bool charging = false;
 
 	if (I > 0)
 	{
@@ -152,7 +168,14 @@ output* capacity_kibam_t::updateCapacity(double P, double V, double dt)
 		Icmax = Icmax_compute(_q1_0, _q0, dt);
 		Ic = -fmin(fabs(I), fabs(Icmax));
 		I = Ic;
+		charging = true;
 	}
+
+	// Check if charge changed
+	if (charging != _prev_charging)
+		_chargeChange = true;
+	else
+		_chargeChange = false;
 
 	// new charge levels
 	q1 = q1_compute(_q1_0, _q0, dt, I);
@@ -186,28 +209,304 @@ output* capacity_kibam_t::updateCapacity(double P, double V, double dt)
 	return _output;
 }
 
+
+
 /*
 Define Lifetime Model
 */
-/*
-lifetime_t::lifetime_t( double * DOD_vect, double * cycle_vect )
+
+lifetime_t::lifetime_t( std::vector<double> DOD_vect, std::vector<double> cycle_vect, int n )
 {
-	_DOD_vect = DOD_vect;
-	_cycle_vect = cycle_vect;
+	_DOD_vect = new double[n];
+	_cycle_vect = new double[n]; 
+
+	for (int i = 0; i != n; i++)
+	{
+		_DOD_vect[i] = DOD_vect[i];
+		_cycle_vect[i] = cycle_vect[i];
+	}
+
+	// Perform Curve fit
+	_a = new double[n];
+	for (int i = 0; i != n; i++)
+		_a[i] = 0;
+	int info = lsqfit(life_vs_DOD, 0, _a, n, _DOD_vect, _cycle_vect, n);
+	int j;
+
+	// initialize other member variables
+	_nCycles = 0;
+	_Dlt = 0;
+	_jlt = 0;
+	_klt = 0; 
+	_Xlt = 0;
+	_Ylt = 0;
+	_Slt = 0;
+	_Range = 0;
+
+	// initialize output
+	_output = new output[TOTAL_LIFETIME_OUT];
+	_output[FRACTIONAL_DAMAGE].name = "Dlt";
+	_output[NUMBER_OF_CYCLES].name = "nCycles";
 }
 
-*/
+lifetime_t::~lifetime_t()
+{
+	delete[] _DOD_vect;
+	delete[] _cycle_vect;
+	delete[] _a;
+	delete[] _output;
+}
+
+output* lifetime_t::rainflow(double DOD)
+{
+	// initialize return code
+	int retCode = LT_GET_DATA;
+
+	// Begin algorithm
+	_Peaks.push_back(DOD);
+	bool atStepTwo = true;
+
+	// Assign S, which is the starting peak or valley
+	if (_jlt == 0)
+	{
+		_Slt = DOD;
+		_klt = _jlt;
+	}
+
+	// Loop until break
+	while (atStepTwo)
+	{
+		// Rainflow: Step 2: Form ranges X,Y
+		if (_jlt >= 2)
+			rainflow_ranges();
+		else
+		{
+			// Get more data (Step 1)
+			retCode = LT_GET_DATA;
+			break;
+		}
+
+		// Rainflow: Step 3: Compare ranges
+		retCode = rainflow_compareRanges();
+
+		// We break to get more data, or if we are done with step 5
+		if (retCode == LT_GET_DATA)
+			break;
+	}
+
+	if (retCode == LT_GET_DATA)
+		_jlt++;
+
+	// Return output
+	_output[FRACTIONAL_DAMAGE].value = _Dlt;
+	_output[NUMBER_OF_CYCLES].value = _nCycles;
+
+	return _output;
+}
+
+void lifetime_t::rainflow_ranges()
+{
+	_Ylt = fabs(_Peaks[_jlt - 1] - _Peaks[_jlt - 2]);
+	_Xlt = fabs(_Peaks[_jlt] - _Peaks[_jlt - 1]);
+}
+void lifetime_t::rainflow_ranges_circular(int index)
+{
+	int end = _Peaks.size() - 1;
+	if (index == 0)
+	{
+		_Xlt = fabs(_Peaks[0] - _Peaks[end]);
+		_Ylt = fabs(_Peaks[end] - _Peaks[end - 1]);
+	}
+	else if (index == 1)
+	{
+		_Xlt = fabs(_Peaks[1] - _Peaks[0]);
+		_Ylt = fabs(_Peaks[0] - _Peaks[end]);
+	}
+	else
+		rainflow_ranges();
+}
+
+int lifetime_t::rainflow_compareRanges()
+{
+	int retCode = LT_SUCCESS;
+	bool contained = true;
+
+	if (_Xlt < _Ylt)
+		retCode = LT_GET_DATA;
+	else if (_Xlt == _Ylt)
+	{
+		if ((_Slt == _Peaks[_jlt - 1]) || (_Slt == _Peaks[_jlt - 2]))
+			retCode = LT_GET_DATA;
+		else
+			contained = false;
+	}
+	else if (_Xlt >= _Ylt)
+	{
+		if (_Xlt > _Ylt)
+		{
+			if ((_Slt == _Peaks[_jlt - 1]) || (_Slt == _Peaks[_jlt - 2]))
+			{
+				// Step 4: Move S to next point in vector, then go to step 1
+				_klt++;
+				_Slt = _Peaks[_klt];
+				retCode = LT_GET_DATA;
+			}
+			else
+				contained = false;
+		}
+		else if (_Xlt == _Ylt)
+		{
+			if ((_Slt != _Peaks[_jlt - 1]) && (_Slt != _Peaks[_jlt - 2]))
+				contained = false;
+		}
+
+	}
+
+	// Step 5: Count range Y, discard peak & valley of Y, go to Step 2
+	if (!contained)
+	{
+		_Range = _Ylt;
+		double Cf = life_vs_DOD(_Range, _a, 0);
+		_Dlt += 1. / Cf;
+		_nCycles++;
+		// discard peak & valley of Y
+		double save = _Peaks[_jlt];
+		_Peaks.pop_back(); 
+		_Peaks.pop_back();
+		_Peaks.pop_back();
+		_Peaks.push_back(save);
+		_jlt -= 2;
+		// stay in while loop
+		retCode = LT_RERANGE;
+	}
+
+	return retCode;
+}
+
+output* lifetime_t::rainflow_finish()
+{
+	// starting indices, must decrement _jlt by 1
+	int ii = 0;
+	_jlt--;
+	double P = 0.;
+	int rereadCount = 0;
+
+
+	while ( rereadCount <= 1 )
+	{
+		if (ii < _Peaks.size())
+			P = _Peaks[ii];
+		else
+			break;
+
+		// Step 6
+		if (P == _Slt)
+			rereadCount++;
+
+		bool atStepSeven = true;
+
+		// Step 7: Form ranges X,Y
+		while (atStepSeven)
+		{
+			if (_jlt >= 2)
+				rainflow_ranges_circular(ii);
+			else
+			{
+				atStepSeven = false;
+				if (_jlt == 1)
+				{
+					_Peaks.push_back(P);
+					_jlt++;
+					// move to end point
+					ii = _jlt;
+					rainflow_ranges_circular(ii);
+				}
+				else
+					break;
+			}
+
+			// Step 8: compare X,Y
+			if (_Xlt < _Ylt)
+			{
+				atStepSeven = false;
+				// move to next point (Step 6)
+				ii++;
+			}
+			else
+			{
+				_Range = _Ylt;
+				double Cf = life_vs_DOD(_Range, _a, 0);
+				_Dlt += 1. / Cf;
+				_nCycles++;
+				// Discard peak and vally of Y
+				double save = _Peaks[_jlt];
+				_Peaks.pop_back();
+				_Peaks.pop_back();
+				_Peaks.pop_back();
+				_Peaks.push_back(save);
+				_jlt -= 2;
+			}
+		}
+	}
+	// Return output
+	_output[FRACTIONAL_DAMAGE].value = _Dlt;
+	_output[NUMBER_OF_CYCLES].value = _nCycles;
+
+	return _output;
+}
+
+double life_vs_DOD(double R, double * a, void * user_data)
+{
+	// may need to figure out how to make more robust (if user enters only three pairs)
+	return (a[0] + a[1] * exp(a[2] * R) + a[3] * exp(a[4] * R));
+}
 
 /* 
 Define Battery 
 */
-battery_t::battery_t(capacity_t *capacity, double dt)
+battery_t::battery_t(capacity_t *capacity, lifetime_t * lifetime, double dt)
 {
 	_capacity = capacity;
+	_lifetime = lifetime;
 	_dt = dt;
+	_firstStep = true;
+}
+void battery_t::run(double P, double V)
+{
+	double lastDOD = _capacity->getDOD();
+
+	if (lastDOD <= 1. && lastDOD >= 0.)
+	{
+		if (_capacity->chargeChanged() || _firstStep)
+		{
+			_LifetimeOutput = runLifetimeModel(lastDOD);
+			_firstStep = false;
+		}
+	}
+	_CapacityOutput = runCapacityModel(P, V);
+}
+
+void battery_t::finish()
+{
+	_LifetimeOutput = _lifetime->rainflow_finish();
 }
 
 output* battery_t::runCapacityModel(double P, double V)
 {
 	return _capacity->updateCapacity(P, V, _dt);
+}
+
+output* battery_t::runLifetimeModel(double DOD)
+{
+	return _lifetime->rainflow(DOD);
+}
+
+output* battery_t::getCapacityOutput()
+{
+	return _CapacityOutput;
+}
+
+output* battery_t::getLifetimeOutput()
+{
+	return _LifetimeOutput;
 }
