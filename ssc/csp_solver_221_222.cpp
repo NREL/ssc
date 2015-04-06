5f845ff706840e3d6f65146d7604e30a069c8ea9
Ty Neises <ty.neises@nrel.gov>
2015-04-06 22:29:56 +0000

    #ignore ... csp solver updates

--- ssc/csp_solver_221_222.cpp
+++ ssc/csp_solver_221_222.cpp
@@ -229,9 +229,8 @@ void C_csp_mspt_221_222::init()
 		land_bound_type = (int)value(P_land_bound_type);
 		land_max = value(P_land_max);
 		land_min = value(P_land_min);
-		land_bound_table = value(P_land_bound_table, nrows1, ncols1);
-		land_bound_table = value(P_land_bound_table, nrows1, ncols1);
-		land_bound_list = value(P_land_bound_list, nrows2);
+		land_bound_table = value(P_land_bound_table, &nrows1, &ncols1);
+		land_bound_list = value(P_land_bound_list, &nrows2);
 		p_start = value(P_p_start);
 		p_track = value(P_p_track);
 		hel_stow_deploy = value(P_hel_stow_deploy)*pi / 180.;
@@ -255,8 +254,8 @@ void C_csp_mspt_221_222::init()
 		pos_dim = 2;	//initiaize with 2 dimensions (x,y) on helio positions
 		if( run_type != C_csp_mspt_221_222::RUN_TYPE::USER_FIELD ) break;
 
-		helio_positions = value(P_helio_positions, N_hel, pos_dim);
-		helio_aim_points = value(P_helio_aim_points, nrows4, ncols4);
+		helio_positions = value(P_helio_positions, &N_hel, &pos_dim);
+		helio_aim_points = value(P_helio_aim_points, &nrows4, &ncols4);
 
 		break;
 	case C_csp_mspt_221_222::RUN_TYPE::USER_DATA:
@@ -265,24 +264,24 @@ void C_csp_mspt_221_222::init()
 		land_bound_type = (int)value(P_land_bound_type);
 		land_max = value(P_land_max);
 		land_min = value(P_land_min);
-		land_bound_table = value(P_land_bound_table, nrows1, ncols1);
-		land_bound_list = value(P_land_bound_list, nrows2);
+		land_bound_table = value(P_land_bound_table, &nrows1, &ncols1);
+		land_bound_list = value(P_land_bound_list, &nrows2);
 		p_start = value(P_p_start);
 		p_track = value(P_p_track);
 		hel_stow_deploy = value(P_hel_stow_deploy)*pi / 180.;
 		v_wind_max = value(P_v_wind_max);
 		interp_nug = value(P_interp_nug);
 		interp_beta = value(P_interp_beta);
-		helio_positions = value(P_helio_positions, N_hel, pos_dim);
-		helio_aim_points = value(P_helio_aim_points, nrows4, ncols4);
+		helio_positions = value(P_helio_positions, &N_hel, &pos_dim);
+		helio_aim_points = value(P_helio_aim_points, &nrows4, &ncols4);
 		//N_hel = (int)value(P_N_hel);
-		eta_map = value(P_eta_map, nrows5, ncols5);
+		eta_map = value(P_eta_map, &nrows5, &ncols5);
 		n_flux_x = (int)value(P_n_flux_x);
 		n_flux_y = (int)value(P_n_flux_y);
 		/*int nfluxpos, nfposdim;
 		int nfluxmap, nfluxcol;*/
-		flux_positions = value(P_flux_positions, nfluxpos, nfposdim);
-		flux_maps = value(P_flux_maps, nfluxmap, nfluxcol);
+		flux_positions = value(P_flux_positions, &nfluxpos, &nfposdim);
+		flux_maps = value(P_flux_maps, &nfluxmap, &nfluxcol);
 		c_atm_0 = value(P_c_atm_0);
 		c_atm_1 = value(P_c_atm_1);
 		c_atm_2 = value(P_c_atm_2);
@@ -323,251 +322,257 @@ void C_csp_mspt_221_222::init()
 	
 
 
-	////do initial runs of SolarPILOT and/or set up tables
-	//switch (run_type)
-	//{
-	//case C_csp_mspt_221_222::RUN_TYPE::AUTO:
-	//case C_csp_mspt_221_222::RUN_TYPE::USER_FIELD:
-	//{
-	//	AutoPilot_S sapi;
+	//do initial runs of SolarPILOT and/or set up tables
+	switch (run_type)
+	{
+	case C_csp_mspt_221_222::RUN_TYPE::AUTO:
+	case C_csp_mspt_221_222::RUN_TYPE::USER_FIELD:
+	{
+		AutoPilot_S sapi;
 
-	//	sp_optimize opt;
-	//	sp_ambient amb;
-	//	sp_cost cost;
-	//	sp_heliostats helios;
-	//	sp_receivers recs;
-	//	sp_layout layout;
-	//
-	//	var_set V;
-	//	ioutil::parseDefinitionArray(V);
-	//
-	//	// define stuff and load default values
-	//	opt.LoadDefaults(V);
-	//	amb.LoadDefaults(V);
-	//	cost.LoadDefaults(V);
-	//	helios.resize(1);
-	//	helios.front().LoadDefaults(V);
-	//	recs.resize(1);
-	//	recs.front().LoadDefaults(V);
-	//	layout.LoadDefaults(V);
+		sp_optimize opt;
+		sp_ambient amb;
+		sp_cost cost;
+		sp_heliostats helios;
+		sp_receivers recs;
+		sp_layout layout;
+	
+		var_set V;
+		ioutil::parseDefinitionArray(V);
+	
+		// define stuff and load default values
+		opt.LoadDefaults(V);
+		amb.LoadDefaults(V);
+		cost.LoadDefaults(V);
+		helios.resize(1);
+		helios.front().LoadDefaults(V);
+		recs.resize(1);
+		recs.front().LoadDefaults(V);
+		layout.LoadDefaults(V);
 
-	//	helios.front().width = helio_width;
-	//	helios.front().height = helio_height;
-	//	helios.front().optical_error = helio_optical_error;
-	//	helios.front().active_fraction = helio_active_fraction * dens_mirror;   //availability * mirror area fraction
-	//	helios.front().reflectance = helio_reflectance;
-	//	int cmap[5];
-	//	cmap[0] = sp_heliostat::CANT_TYPE::FLAT;
-	//	cmap[1] = sp_heliostat::CANT_TYPE::AT_SLANT;
-	//	cmap[2] = sp_heliostat::CANT_TYPE::AT_DAY_HOUR;
-	//	cmap[3] = sp_heliostat::CANT_TYPE::AT_DAY_HOUR;
-	//	cmap[4] = sp_heliostat::CANT_TYPE::AT_DAY_HOUR;
-	//	helios.front().cant_type = cmap[ cant_type ];
-	//	if( cant_type == 2 ){
-	//		helios.front().cant_settings.point_day = 81;  //spring equinox
-	//		helios.front().cant_settings.point_hour = 12.;
-	//	}
-	//	else if( cant_type == 3 ){
-	//		helios.front().cant_settings.point_day = 172;  //Summer solstice
-	//		helios.front().cant_settings.point_hour = 12.;
-	//	}
-	//	else if( cant_type == 4){
-	//		helios.front().cant_settings.point_day = 355;  //Winter solstice
-	//		helios.front().cant_settings.point_hour = 12.;
-	//	}
+		helios.front().width = helio_width;
+		helios.front().height = helio_height;
+		helios.front().optical_error = helio_optical_error;
+		helios.front().active_fraction = helio_active_fraction * dens_mirror;   //availability * mirror area fraction
+		helios.front().reflectance = helio_reflectance;
+		int cmap[5];
+		cmap[0] = sp_heliostat::CANT_TYPE::FLAT;
+		cmap[1] = sp_heliostat::CANT_TYPE::AT_SLANT;
+		cmap[2] = sp_heliostat::CANT_TYPE::AT_DAY_HOUR;
+		cmap[3] = sp_heliostat::CANT_TYPE::AT_DAY_HOUR;
+		cmap[4] = sp_heliostat::CANT_TYPE::AT_DAY_HOUR;
+		helios.front().cant_type = cmap[ cant_type ];
+		if( cant_type == 2 ){
+			helios.front().cant_settings.point_day = 81;  //spring equinox
+			helios.front().cant_settings.point_hour = 12.;
+		}
+		else if( cant_type == 3 ){
+			helios.front().cant_settings.point_day = 172;  //Summer solstice
+			helios.front().cant_settings.point_hour = 12.;
+		}
+		else if( cant_type == 4){
+			helios.front().cant_settings.point_day = 355;  //Winter solstice
+			helios.front().cant_settings.point_hour = 12.;
+		}
 
-	//	int fmap[2];
-	//	fmap[0] = sp_heliostat::FOCUS_TYPE::FLAT;
-	//	fmap[1] = sp_heliostat::FOCUS_TYPE::AT_SLANT;
-	//	helios.front().focus_type = fmap[ focus_type ];
+		int fmap[2];
+		fmap[0] = sp_heliostat::FOCUS_TYPE::FLAT;
+		fmap[1] = sp_heliostat::FOCUS_TYPE::AT_SLANT;
+		helios.front().focus_type = fmap[ focus_type ];
 
-	//	recs.front().absorptance = rec_absorptance;
-	//	recs.front().height = rec_height;
-	//	recs.front().aspect = rec_aspect;
-	//	recs.front().q_hl_perm2 = rec_hl_perm2;
-	//		
-	//	layout.q_design = q_design;
-	//	layout.dni_design = dni_des;
-	//	layout.land_max = land_max;
-	//	layout.land_min = land_min;
-	//	layout.h_tower = h_tower;
+		recs.front().absorptance = rec_absorptance;
+		recs.front().height = rec_height;
+		recs.front().aspect = rec_aspect;
+		recs.front().q_hl_perm2 = rec_hl_perm2;
+			
+		layout.q_design = q_design;
+		layout.dni_design = dni_des;
+		layout.land_max = land_max;
+		layout.land_min = land_min;
+		layout.h_tower = h_tower;
 
-	//	//set up the weather data for simulation
-	//	const char *wffile = weather_file.c_str();
-	//	
-	//	//if ( !wffile ) message(TCS_WARNING,  "solarpilot: no weather file specified" );
-	//	
-	//	weatherfile wf( wffile );
-	//	
-	//	//if ( !wf.ok() || wf.type() == weatherfile::INVALID ) message( TCS_WARNING, "solarpilot: could not open weather file or invalid weather file format");
-
-
-	//	amb.site_latitude = wf.lat;
-	//	amb.site_longitude = wf.lon;
-	//	amb.site_time_zone = wf.tz;
-	//	amb.atten_model = sp_ambient::ATTEN_MODEL::USER_DEFINED;
-	//	amb.user_atten_coefs.clear();
-	//	amb.user_atten_coefs.push_back(c_atm_0);
-	//	amb.user_atten_coefs.push_back(c_atm_1);
-	//	amb.user_atten_coefs.push_back(c_atm_2);
-	//	amb.user_atten_coefs.push_back(c_atm_3);
-
-	//	if( run_type == C_csp_mspt_221_222::RUN_TYPE::AUTO )
-	//	{
-	//		/* 
-	//		Generate the heliostat field layout using the settings provided by the user				
-	//		*/
-	//		vector<string> wfdata;
-	//		wfdata.reserve( 8760 );
-	//		char buf[1024];
-	//		for( int i=0;i<8760;i++ )
-	//		{
-	//			if( !wf.read() ){
-	//				string msg = "solarpilot: could not read data line " + util::to_string(i+1) + " of 8760 in weather file";
-	//				message(TCS_WARNING, msg.c_str());
-	//			}
-
-	//			mysnprintf(buf, 1023, "%d,%d,%d,%.2lf,%.1lf,%.1lf,%.1lf", wf.day, wf.hour, wf.month, wf.dn, wf.tdry, wf.pres/1000., wf.wspd);
-	//			wfdata.push_back( std::string(buf) );
-	//		}
-
-	//		sapi.SetDetailCallback( solarpilot_callback, (void*)this);
-	//		sapi.SetSummaryCallbackStatus(false);
-
-	//		sapi.GenerateDesignPointSimulations( amb, V, wfdata );
-	//
-	//		sapi.Setup(amb, cost, layout, helios, recs);
-
-	//		sapi.CreateLayout();
+		//set up the weather data for simulation
+		const char *wffile = weather_file.c_str();
+		
+		//if ( !wffile ) message(TCS_WARNING,  "solarpilot: no weather file specified" );
+		
+		weatherfile wf( wffile );
+		
+		//if ( !wf.ok() || wf.type() == weatherfile::INVALID ) message( TCS_WARNING, "solarpilot: could not open weather file or invalid weather file format");
 
 
-	//		//Copy the heliostat field positions into the 'helio_positions' data structure
-	//		N_hel = (int)layout.heliostat_positions.size();
- //           string msg = "Auto-generated field: Number of heliostats " + util::to_string(N_hel);
- //           message(TCS_NOTICE, msg.c_str());
-	//		helio_positions = allocate(P_helio_positions, N_hel, pos_dim);
-	//		for( int i=0; i<N_hel; i++){
-	//			TCS_MATRIX_INDEX( var(P_helio_positions), i, 0 ) = layout.heliostat_positions.at(i).location.x;
-	//			TCS_MATRIX_INDEX( var(P_helio_positions), i, 1 ) = layout.heliostat_positions.at(i).location.y;
-	//			if(pos_dim==3)
-	//				TCS_MATRIX_INDEX( var(P_helio_positions), i, 2) = layout.heliostat_positions.at(i).location.z;
-	//		}
-	//			
-	//		//update the callbacks
-	//		sapi.SetDetailCallbackStatus(false);
-	//			
-	//	}
-	//	else{
+		amb.site_latitude = wf.lat;
+		amb.site_longitude = wf.lon;
+		amb.site_time_zone = wf.tz;
+		amb.atten_model = sp_ambient::ATTEN_MODEL::USER_DEFINED;
+		amb.user_atten_coefs.clear();
+		amb.user_atten_coefs.push_back(c_atm_0);
+		amb.user_atten_coefs.push_back(c_atm_1);
+		amb.user_atten_coefs.push_back(c_atm_2);
+		amb.user_atten_coefs.push_back(c_atm_3);
 
-	//		/* 
-	//		Load in the heliostat field positions that are provided by the user.
-	//		*/
-	//		layout.heliostat_positions.clear();
-	//		layout.heliostat_positions.resize(N_hel);
-	//			
-	//		for( int i=0; i<N_hel; i++){
+		if( run_type == C_csp_mspt_221_222::RUN_TYPE::AUTO )
+		{
+			/* 
+			Generate the heliostat field layout using the settings provided by the user				
+			*/
+			vector<string> wfdata;
+			wfdata.reserve( 8760 );
+			char buf[1024];
+			for( int i=0;i<8760;i++ )
+			{
+				if( !wf.read() )
+				{
+					string msg = "solarpilot: could not read data line " + util::to_string(i+1) + " of 8760 in weather file";
+					log(msg);
+					//message(TCS_WARNING, msg.c_str());
+				}
 
-	//			layout.heliostat_positions.at(i).location.x = TCS_MATRIX_INDEX( var(P_helio_positions), i, 0 );
-	//			layout.heliostat_positions.at(i).location.y = TCS_MATRIX_INDEX( var(P_helio_positions), i, 1 );
-	//			if(pos_dim==3)
-	//				layout.heliostat_positions.at(i).location.z = TCS_MATRIX_INDEX( var(P_helio_positions), i, 2);
-	//				
-	//		}
+				mysnprintf(buf, 1023, "%d,%d,%d,%.2lf,%.1lf,%.1lf,%.1lf", wf.day, wf.hour, wf.month, wf.dn, wf.tdry, wf.pres/1000., wf.wspd);
+				wfdata.push_back( std::string(buf) );
+			}
 
- //           sapi.Setup(amb, cost, layout, helios, recs);
-	//							
-	//	}
- //       //land area update
-	//	value(P_land_area, layout.land_area);
- //       //number of heliostats
-	//	value(P_N_hel, (double)N_hel);
+			sapi.SetDetailCallback( solarpilot_callback, (void*)this);
+			sapi.SetSummaryCallbackStatus(false);
 
-	//	sapi.SetSummaryCallbackStatus(true);
-	//	sapi.SetSummaryCallback( solarpilot_callback, (void*)this);
+			sapi.GenerateDesignPointSimulations( amb, V, wfdata );
+	
+			sapi.Setup(amb, cost, layout, helios, recs);
 
-	//	//set up flux map resolution
-	//	fluxtab.is_user_spacing = true;
-	//	fluxtab.n_flux_days = n_flux_days;
-	//	fluxtab.delta_flux_hrs = delta_flux_hrs;
-
-	//	//run the flux maps
-	//	if(! sapi.CalculateFluxMaps(fluxtab, n_flux_x, n_flux_y, true) ){
- //           message(TCS_ERROR, "Simulation cancelled during fluxmap preparation");
- //           return -1;
- //       }
-
-	//	//collect efficiencies
-	//	sunpos.clear();
-	//	effs.clear();
-	//	int npos = (int)fluxtab.azimuths.size();
-	//	sunpos.reserve(npos);
-	//	effs.reserve(npos);
-
- //       eta_map = allocate( P_eta_map, npos, 3, 0.);
- //       m_flux_positions.resize(npos, VectDoub(2) );
-
-	//	for(int i=0; i<npos; i++){
-	//		sunpos.push_back( vector<double>(2, 0.) );
-
-	//		sunpos.back().at(0) = fluxtab.azimuths.at(i) / az_scale;
-	//		sunpos.back().at(1) = fluxtab.zeniths.at(i) / zen_scale;
-	//		effs.push_back( fluxtab.efficiency.at(i) / eff_scale );
-
- //           //fill the parameter matrix to return this data to calling program
- //           //also fill the flux sun positions matrix
- //           eta_map[i*3    ] = m_flux_positions.at(i).at(0) = fluxtab.azimuths.at(i)*180./pi;
- //           eta_map[i*3 + 1] = m_flux_positions.at(i).at(1) = fluxtab.zeniths.at(i)*180./pi;
- //           eta_map[i*3 + 2] = fluxtab.efficiency.at(i);
-
-	//	}
+			sapi.CreateLayout();
 
 
-	//	//collect flux's
-	//	flux_maps = allocate(P_flux_maps, n_flux_y * npos, n_flux_x);
+			//Copy the heliostat field positions into the 'helio_positions' data structure
+			N_hel = (int)layout.heliostat_positions.size();
+            string msg = "Auto-generated field: Number of heliostats " + util::to_string(N_hel);
+            
+			log(msg);
+			//message(TCS_NOTICE, msg.c_str());
+			
+			helio_positions = allocate(P_helio_positions, N_hel, pos_dim);
+			for( int i=0; i<N_hel; i++)
+			{
+				TCS_MATRIX_INDEX( var(P_helio_positions), i, 0 ) = layout.heliostat_positions.at(i).location.x;
+				TCS_MATRIX_INDEX( var(P_helio_positions), i, 1 ) = layout.heliostat_positions.at(i).location.y;
+				if(pos_dim==3)
+					TCS_MATRIX_INDEX( var(P_helio_positions), i, 2) = layout.heliostat_positions.at(i).location.z;
+			}
+				
+			//update the callbacks
+			sapi.SetDetailCallbackStatus(false);
+				
+		}
+		else{
 
-	//	block_t<double> *f = &fluxtab.flux_surfaces.front().flux_data;
+			/* 
+			Load in the heliostat field positions that are provided by the user.
+			*/
+			layout.heliostat_positions.clear();
+			layout.heliostat_positions.resize(N_hel);
+				
+			for( int i=0; i<N_hel; i++){
 
-	//	int nfl = f->nlayers();
+				layout.heliostat_positions.at(i).location.x = TCS_MATRIX_INDEX( var(P_helio_positions), i, 0 );
+				layout.heliostat_positions.at(i).location.y = TCS_MATRIX_INDEX( var(P_helio_positions), i, 1 );
+				if(pos_dim==3)
+					layout.heliostat_positions.at(i).location.z = TCS_MATRIX_INDEX( var(P_helio_positions), i, 2);
+					
+			}
 
-	//	for( int i = 0; i<nfl; i++ ){
-	//		for( int j = 0; j<n_flux_y; j++ ){
-	//			for( int k = 0; k<n_flux_x; k++ ){
-	//				TCS_MATRIX_INDEX(var(P_flux_maps), i*n_flux_y + j, k) = f->at(j, k, i);
-	//			}
-	//		}
-	//	}
+            sapi.Setup(amb, cost, layout, helios, recs);
+								
+		}
+        //land area update
+		value(P_land_area, layout.land_area);
+        //number of heliostats
+		value(P_N_hel, (double)N_hel);
 
-	//	break;
-	//}
-	//case C_csp_mspt_221_222::RUN_TYPE::USER_DATA:
-	//{
+		sapi.SetSummaryCallbackStatus(true);
+		sapi.SetSummaryCallback( solarpilot_callback, (void*)this);
 
-	//	int nrows, ncols;
-	//	double *p_map = value( P_eta_map, &nrows, &ncols);
-	//	
-	//	if(ncols != 3){
-	//		message(TCS_ERROR,  "The heliostat field efficiency file is not formatted correctly. Type expects 3 columns"
-	//			" (zenith angle, azimuth angle, efficiency value) and instead has %d cols.", ncols);
-	//		return -1;
-	//	}
-	//	
-	//	//read the data from the array into the local storage arrays
-	//	sunpos.resize(nrows, VectDoub(2));
-	//	effs.resize(nrows);
-	//	for(int i=0; i<nrows; i++){
-	//		sunpos.at(i).at(0) = TCS_MATRIX_INDEX( var( P_eta_map ), i, 0 ) / az_scale * pi/180.;
-	//		sunpos.at(i).at(1) = TCS_MATRIX_INDEX( var( P_eta_map ), i, 1 ) / zen_scale * pi/180.;
-	//		effs.at(i) = TCS_MATRIX_INDEX( var( P_eta_map ), i, 2 ) / eff_scale;
-	//	}
+		//set up flux map resolution
+		fluxtab.is_user_spacing = true;
+		fluxtab.n_flux_days = n_flux_days;
+		fluxtab.delta_flux_hrs = delta_flux_hrs;
 
- //       break;
-	//}
-	//default:
-	//	break;
-	//}
+		//run the flux maps
+		if(! sapi.CalculateFluxMaps(fluxtab, n_flux_x, n_flux_y, true) ){
+            message(TCS_ERROR, "Simulation cancelled during fluxmap preparation");
+            return -1;
+        }
 
- //   //size the output
-	//flux_map = allocate( O_flux_map, n_flux_y, n_flux_x );
+		//collect efficiencies
+		sunpos.clear();
+		effs.clear();
+		int npos = (int)fluxtab.azimuths.size();
+		sunpos.reserve(npos);
+		effs.reserve(npos);
+
+        eta_map = allocate( P_eta_map, npos, 3, 0.);
+        m_flux_positions.resize(npos, VectDoub(2) );
+
+		for(int i=0; i<npos; i++){
+			sunpos.push_back( vector<double>(2, 0.) );
+
+			sunpos.back().at(0) = fluxtab.azimuths.at(i) / az_scale;
+			sunpos.back().at(1) = fluxtab.zeniths.at(i) / zen_scale;
+			effs.push_back( fluxtab.efficiency.at(i) / eff_scale );
+
+            //fill the parameter matrix to return this data to calling program
+            //also fill the flux sun positions matrix
+            eta_map[i*3    ] = m_flux_positions.at(i).at(0) = fluxtab.azimuths.at(i)*180./pi;
+            eta_map[i*3 + 1] = m_flux_positions.at(i).at(1) = fluxtab.zeniths.at(i)*180./pi;
+            eta_map[i*3 + 2] = fluxtab.efficiency.at(i);
+
+		}
+
+
+		//collect flux's
+		flux_maps = allocate(P_flux_maps, n_flux_y * npos, n_flux_x);
+
+		block_t<double> *f = &fluxtab.flux_surfaces.front().flux_data;
+
+		int nfl = f->nlayers();
+
+		for( int i = 0; i<nfl; i++ ){
+			for( int j = 0; j<n_flux_y; j++ ){
+				for( int k = 0; k<n_flux_x; k++ ){
+					TCS_MATRIX_INDEX(var(P_flux_maps), i*n_flux_y + j, k) = f->at(j, k, i);
+				}
+			}
+		}
+
+		break;
+	}
+	case C_csp_mspt_221_222::RUN_TYPE::USER_DATA:
+	{
+
+		int nrows, ncols;
+		double *p_map = value( P_eta_map, &nrows, &ncols);
+		
+		if(ncols != 3){
+			message(TCS_ERROR,  "The heliostat field efficiency file is not formatted correctly. Type expects 3 columns"
+				" (zenith angle, azimuth angle, efficiency value) and instead has %d cols.", ncols);
+			return -1;
+		}
+		
+		//read the data from the array into the local storage arrays
+		sunpos.resize(nrows, VectDoub(2));
+		effs.resize(nrows);
+		for(int i=0; i<nrows; i++){
+			sunpos.at(i).at(0) = TCS_MATRIX_INDEX( var( P_eta_map ), i, 0 ) / az_scale * pi/180.;
+			sunpos.at(i).at(1) = TCS_MATRIX_INDEX( var( P_eta_map ), i, 1 ) / zen_scale * pi/180.;
+			effs.at(i) = TCS_MATRIX_INDEX( var( P_eta_map ), i, 2 ) / eff_scale;
+		}
+
+        break;
+	}
+	default:
+		break;
+	}
+
+    //size the output
+	flux_map = allocate( O_flux_map, n_flux_y, n_flux_x );
 
 
 
