5f233fcd10882c2befe1abc5a9dc9c702c224ddf
Ty Neises <ty.neises@nrel.gov>
2015-04-15 05:16:51 +0000

    #ignore .. csp solver updates

--- tcs/csp_solver_core.h
+++ tcs/csp_solver_core.h
@@ -1,6 +1,8 @@
 #ifndef __csp_solver_core_
 #define __csp_solver_core_
 
+#include "lib_weatherfile.h"
+#include "csp_solver_util.h"
 
 class C_csp_collector_receiver
 {
@@ -13,6 +15,74 @@ public:
 	virtual void init() = 0;
 };
 
+class C_csp_weatherreader
+{
+private:
+	weatherfile m_wf;
+	bool m_first;		// flag to indicate whether this is the first call
+
+	// member string for exception messages
+	std::string m_error_msg;
+
+public:
+	C_csp_weatherreader();
+
+	~C_csp_weatherreader(){};
+
+	void init();
+
+	void timestep_call(double time, double step, int ncall);
+
+	// Class to save messages for up stream classes
+	C_csp_messages mc_csp_messages;
+
+	struct S_outputs
+	{
+		int m_year;
+		int m_month;
+		int m_day;
+		int m_hour;
+		double m_minute;
+
+		double m_global;
+		double m_beam;
+		double m_diffuse;
+		double m_tdry;
+		double m_twet;
+		double m_tdew;
+		double m_wspd;
+		double m_wdir;
+		double m_rhum;
+		double m_pres;
+		double m_snow;
+		double m_albedo;
+
+		double m_poa;
+		double m_solazi;
+		double m_solzen;
+		double m_lat;
+		double m_lon;
+		double m_tz;
+		double m_shift;
+		double m_elev;
+
+		S_outputs()
+		{
+			m_year = m_month = m_day = m_hour = -1;
+
+			m_global = m_beam = m_diffuse = m_tdry = m_twet = m_tdew = m_wspd = m_wdir = m_rhum = m_pres = m_snow = m_albedo =
+				m_poa = m_solazi = m_solzen = m_lat = m_lon = m_tz = m_shift = m_elev = std::numeric_limits<double>::quiet_NaN();
+		}
+	};
+
+	// Member data - public so can be set from up stream code
+	std::string m_filename;
+	int m_trackmode;
+	double m_tilt;
+	double m_azimuth;
+	
+	S_outputs ms_outputs;
+};
 
 
 
---
+++ tcs/csp_solver_weatherreader.cpp
@@ -0,0 +1,100 @@
+#include "csp_solver_core.h"
+#include "csp_solver_util.h"
+#include "sam_csp_util.h"
+
+#include <shared/lib_weatherfile.h>
+#include <shared/lib_irradproc.h>
+
+C_csp_weatherreader::C_csp_weatherreader()
+{
+	m_filename = "";
+	m_trackmode = -1;
+	m_tilt = std::numeric_limits<double>::quiet_NaN();
+	m_azimuth = std::numeric_limits<double>::quiet_NaN();
+}
+
+
+void C_csp_weatherreader::init()
+{
+	if( !m_wf.open(m_filename) )
+	{
+		m_error_msg = util::format("Could not open %s for reading", m_filename);
+		throw(C_csp_exception(m_error_msg, ""));
+	}
+	m_first = true;		// True the first time call() is accessed
+
+	if(m_trackmode < 0 || m_trackmode > 2)
+	{
+		m_error_msg = util::format("invalid tracking mode specified %d [0..2]", m_trackmode);
+		throw(C_csp_exception(m_error_msg, ""));
+	}
+}
+
+
+void C_csp_weatherreader::timestep_call(double time, double step, int ncall)
+{
+	if( ncall == 0 ) // only read data values once per timestep
+	{
+		//If the start time does not correspond to the first record in the weather file, loop to the correct record
+		int nread = 1;
+		if( m_first )
+		{
+			nread = (int)time / step;
+			m_first = false;
+		}
+
+		for( int i = 0; i<nread; i++ )		//for all calls except the first, nread=1
+		{
+			if( !m_wf.read() )
+			{
+				m_error_msg = util::format("failed to read from weather file %s at time %lg", m_wf.filename().c_str(), time);
+				throw(C_csp_exception(m_error_msg, ""));
+			}
+		}
+	}
+
+	double sunn[9], angle[5], poa[3], diffc[3];
+
+	poa[0] = poa[1] = poa[2] = 0;
+	angle[0] = angle[1] = angle[2] = angle[3] = angle[4] = 0;
+	diffc[0] = diffc[1] = diffc[2] = 0;
+
+	solarpos(m_wf.year, m_wf.month, m_wf.day, m_wf.hour, m_wf.minute,
+		m_wf.lat, m_wf.lon, m_wf.tz, sunn);
+
+	if( sunn[2] > 0.0087 )
+	{
+		/* sun elevation > 0.5 degrees */
+		incidence(m_trackmode, m_tilt, m_azimuth, 45.0, sunn[1], sunn[0], 0, 0, angle);
+		perez(sunn[8], m_wf.dn, m_wf.df, 0.2, angle[0], angle[1], sunn[1], poa, diffc);
+	}
+	
+	ms_outputs.m_year = m_wf.year;
+	ms_outputs.m_month = m_wf.month;
+	ms_outputs.m_day = m_wf.day;
+	ms_outputs.m_hour = m_wf.hour;
+	ms_outputs.m_minute = m_wf.minute;
+
+	ms_outputs.m_global = m_wf.gh;
+	ms_outputs.m_beam = m_wf.dn;
+	ms_outputs.m_diffuse = m_wf.df;
+	ms_outputs.m_tdry = m_wf.tdry;
+	ms_outputs.m_twet = m_wf.twet;
+	ms_outputs.m_tdew = m_wf.tdew;
+	ms_outputs.m_wspd = m_wf.wspd;
+	ms_outputs.m_wdir = m_wf.wdir;
+	ms_outputs.m_rhum = m_wf.rhum;
+	ms_outputs.m_pres = m_wf.pres;
+	ms_outputs.m_snow = m_wf.snow;
+	ms_outputs.m_albedo = m_wf.albedo;
+
+	ms_outputs.m_poa = poa[0] + poa[1] + poa[2];
+	ms_outputs.m_solazi = sunn[0] * 180 / CSP::pi;
+	ms_outputs.m_solzen = sunn[1] * 180 / CSP::pi;
+	ms_outputs.m_lat = m_wf.lat;
+	ms_outputs.m_lon = m_wf.lon;
+	ms_outputs.m_tz = m_wf.tz;
+	ms_outputs.m_shift = (m_wf.lon - m_wf.tz*15.0);
+	ms_outputs.m_elev = m_wf.elev;
+
+}
