a1153dbd0c23c994783f901209af8f0238634ad0
Paul Gilman <paulgilman@earthlink.net>
2015-04-30 22:44:32 +0000

    #new ssc changes for lcoe calculator

--- ssc/cmod_lcoefcr.cpp
+++ ssc/cmod_lcoefcr.cpp
@@ -3,25 +3,18 @@
 
 static var_info vtab_lcoefcr[] = 
 {	
-/*   VARTYPE            DATATYPE         NAME                        LABEL                       UNITS     META      GROUP          REQUIRED_IF    CONSTRAINTS UI_HINTS*/
-	{ SSC_INPUT,        SSC_NUMBER,      "capital_cost",             "Capital cost",             "$",      "",       "Simple LCOE", "*",           "",         "" },
-	{ SSC_INPUT,        SSC_NUMBER,      "operating_cost",           "Annual operating cost",    "$/yr",   "",       "Simple LCOE", "*",           "",         "" },
-	{ SSC_INPUT,        SSC_NUMBER,      "fixed_charge_rate",        "Fixed charge rate",        "",       "",       "Simple LCOE", "*",           "",         "" },
-	{ SSC_INPUT,        SSC_NUMBER,      "annual_energy",            "Annual energy production", "kWh/yr", "",       "Simple LCOE", "*",           "",         "" },
+/*   VARTYPE            DATATYPE         NAME                        LABEL                             UNITS     META      GROUP          REQUIRED_IF    CONSTRAINTS UI_HINTS*/
+	{ SSC_INPUT,        SSC_NUMBER,      "capital_cost",             "Capital cost",                   "$",      "",       "Simple LCOE", "*",           "",         "" },
+	{ SSC_INPUT,        SSC_NUMBER,      "fixed_operating_cost",     "Annual fixed operating cost",    "$/kW",   "",       "Simple LCOE", "*",           "",         "" },
+	{ SSC_INPUT,        SSC_NUMBER,      "variable_operating_cost",  "Annual variable operating cost", "$/kWh",  "",       "Simple LCOE", "*",           "",         "" },
+	{ SSC_INPUT,        SSC_NUMBER,      "fixed_charge_rate",        "Fixed charge rate",              "",       "",       "Simple LCOE", "*",           "",         "" },
+	{ SSC_INPUT,        SSC_NUMBER,      "annual_energy",            "Annual energy production",       "kWh/yr", "",       "Simple LCOE", "*",           "",         "" },
+	{ SSC_INPUT,        SSC_NUMBER,      "system_capacity",          "System capacity",                "kW",     "",       "Simple LCOE", "*",           "",         "" },
 	
-	{ SSC_OUTPUT,       SSC_NUMBER,       "lcoe",                     "Levelized cost of energy", "$/kWh", "",       "Simple LCOE", "*",           "",         "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,       "lcoe_fcr",                "Levelized cost of energy",      "$/kWh",  "",       "Simple LCOE", "*",           "",         "" },
 
 var_info_invalid };
 
-// Implementation of "shortcut LCOE calculation" from Short, W. et al. (1995) 
-// "A Manual for the Economic Evaluation of Energy Efficiency and Renewable Energy 
-// Technologies" NREL/TP-462-4173, p. 49.
-//
-// This approach requires:
-// 1. Constant output (no degradation)
-// 2. Constant O&M (no O&M escalation)
-// 3. No financing (no debt)
-//
 class cm_lcoefcr : public compute_module
 {
 private:
@@ -33,20 +26,24 @@ public:
 
 	void exec( ) throw( general_error )
 	{
-		double aep = 0; // annual output, get from performance model
+		double aep = 1; // annual output, get from performance model
+		double p = 0;   // system capacity
 		double aoe = 0; // annual operating costs
 		double fcr = 1; // fixed charge rate, before tax revenues required
 		double icc = 0; // initial investment, or capital cost
+		double voc = 0; // variable operating cost
+		double foc = 0; // fixed operating cost
 
-		aep = as_double("annual_energy");     // kWh
-		aoe = as_double("operating_cost");    // $
-		fcr = as_double("fixed_charge_rate"); // unitless fraction
-		icc = as_double("capital_cost");      // $
-
-		// Short (1995) Equation 4-11, p. 49
+		aep = as_double("annual_energy");           // kWh
+		p = as_double("system_capacity");           // kW
+		foc = as_double("fixed_operating_cost");    // $/kW
+		voc = as_double("variable_operating_cost"); // $/kWh
+		fcr = as_double("fixed_charge_rate");       // unitless fraction
+		icc = as_double("capital_cost");            // $
+		
 		double lcoe;
-		lcoe = (fcr*icc + aoe) / aep; //$/kWh
-		assign("lcoe", var_data((ssc_number_t)lcoe));
+		lcoe = (fcr*icc + foc*p) / aep + voc; //$/kWh
+		assign("lcoe_fcr", var_data((ssc_number_t)lcoe));
 
 	}
 
