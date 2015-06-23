d0c500bc32a7645ef5cf448b619b302ce698c75c
Aron Dobos <aron.dobos@outlook.com>
2015-06-23 22:00:56 +0000

    updated linux sdk build script

--- make-sdk-linux.sh
+++ make-sdk-linux.sh
@@ -1,9 +1,12 @@
 # script to copy binaries to <ssc>/sdk-release folder
 cp -v build_linux/ssc.so sdk-release/linux64/ssc.so
 cp -v build_linux/SDKtool sdk-release/linux64/SDKtool
+strip sdk-release/linux64/ssc.so
+strip sdk-release/linux64/SDKtool
 file sdk-release/linux64/SDKtool > sdk-release/linux64/filetypes.txt
 file sdk-release/linux64/ssc.so >> sdk-release/linux64/filetypes.txt
-echo "Dynamic library dependencies: compiled on CentOS 6.4" > sdk-release/linux64/dependencies.txt
+echo "Dynamic library dependencies and build system information:" > sdk-release/linux64/dependencies.txt
+cat /etc/*-release >> sdk-release/linux64/dependencies.txt
 echo "SDKtool:" >> sdk-release/linux64/dependencies.txt
 ldd sdk-release/linux64/SDKtool >> sdk-release/linux64/dependencies.txt
 echo "ssc.so:" >> sdk-release/linux64/dependencies.txt
