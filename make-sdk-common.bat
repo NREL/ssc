06b02b58c868e6046532ddbc026c47b224173e16
Aron Dobos <aron.dobos@outlook.com>
2015-06-29 18:07:45 +0000

    revised sdk-release copy script

--- make-sdk-common.bat
+++ make-sdk-common.bat
@@ -1,3 +1,14 @@
+rmdir /s sdk-release\languages
+rmdir /s sdk-release\examples
+rmdir /s sdk-release\doc
+
+mkdir sdk-release\languages\matlab
+mkdir "sdk-release\languages\matlab\+SSC"
+mkdir "sdk-release\languages\matlab\+SSC\@API"
+mkdir "sdk-release\languages\matlab\+SSC\@Data"
+mkdir "sdk-release\languages\matlab\+SSC\@Entry"
+mkdir "sdk-release\languages\matlab\+SSC\@Info"
+mkdir "sdk-release\languages\matlab\+SSC\@Module"
 copy apiwrappers\matlab\ReadMe.txt sdk-release\languages\matlab
 copy apiwrappers\matlab\modules.m sdk-release\languages\matlab
 copy apiwrappers\matlab\pvwatts.m sdk-release\languages\matlab
@@ -10,20 +21,21 @@ copy "apiwrappers\matlab\+SSC\@Entry\Entry.m" "sdk-release\languages\matlab\+SSC
 copy "apiwrappers\matlab\+SSC\@Info\Info.m" "sdk-release\languages\matlab\+SSC\@Info"
 copy "apiwrappers\matlab\+SSC\@Module\Module.m" "sdk-release\languages\matlab\+SSC\@Module"
 
+mkdir sdk-release\languages\python
 copy apiwrappers\python\sscapi.py sdk-release\languages\python
 copy apiwrappers\python\ssc.py sdk-release\languages\python
 copy apiwrappers\python\testssc.py sdk-release\languages\python
-copy apiwrappers\python\pvsamv1.py sdk-release\languages\python
-copy apiwrappers\python\belpe.py sdk-release\languages\python
 copy apiwrappers\python\openei_utilityrate_billcalculator.py sdk-release\languages\python
-copy apiwrappers\python\cashloan.py sdk-release\languages\python
 
+mkdir sdk-release\languages\php
 copy apiwrappers\php\down-arrow.png sdk-release\languages\php
 copy apiwrappers\php\jquery.searchabledropdown-1.0.8.min.js sdk-release\languages\php
 copy apiwrappers\php\Makefile sdk-release\languages\php
 copy apiwrappers\php\pvwatts.php sdk-release\languages\php
 copy apiwrappers\php\sscphp.c sdk-release\languages\php
 
+mkdir sdk-release\languages\csharp
+mkdir sdk-release\languages\csharp\Properties
 copy apiwrappers\csharp\SSC.cs sdk-release\languages\csharp
 copy apiwrappers\csharp\App.config sdk-release\languages\csharp
 copy apiwrappers\csharp\Program.cs sdk-release\languages\csharp
@@ -36,6 +48,8 @@ copy apiwrappers\csharp\Properties\AssemblyInfo.cs sdk-release\languages\csharp\
 copy apiwrappers\csharp\Properties\Resources.Designer.cs sdk-release\languages\csharp\Properties
 copy apiwrappers\csharp\Properties\Resources.resx sdk-release\languages\csharp\Properties
 
+mkdir sdk-release\languages\java
+mkdir sdk-release\languages\java\SSC
 copy apiwrappers\java\TestBelpe.data sdk-release\languages\java
 copy apiwrappers\java\TestBelpe.java sdk-release\languages\java
 copy apiwrappers\java\TestBelpe.make sdk-release\languages\java
@@ -54,6 +68,8 @@ copy apiwrappers\java\TestUtilityRate3.Manifest.txt sdk-release\languages\java
 copy apiwrappers\java\ReadMe.txt sdk-release\languages\java
 copy apiwrappers\java\sscapi_wrap.c sdk-release\languages\java
 copy apiwrappers\java\TestSSCAPI.java sdk-release\languages\java
+copy apiwrappers\java\TestSSCAPI.make sdk-release\languages\java
+copy apiwrappers\java\TestSSCAPI.Manifest.txt sdk-release\languages\java
 copy apiwrappers\java\SSC\API.java sdk-release\languages\java\SSC
 copy apiwrappers\java\SSC\Data.java sdk-release\languages\java\SSC
 copy apiwrappers\java\SSC\Entry.java sdk-release\languages\java\SSC
@@ -61,6 +77,7 @@ copy apiwrappers\java\SSC\Info.java sdk-release\languages\java\SSC
 copy apiwrappers\java\SSC\Module.java sdk-release\languages\java\SSC
 copy apiwrappers\java\SSC\SSCAPIJNI.java sdk-release\languages\java\SSC
 
+mkdir sdk-release\examples
 copy examples\abilene.tm2 sdk-release\examples
 copy examples\daggett.tm2 sdk-release\examples
 copy examples\rocksprings.tm2 sdk-release\examples
@@ -68,7 +85,6 @@ copy examples\example1_pvwatts.c sdk-release\examples
 copy examples\pvwatts1ts_ex.c sdk-release\examples
 copy examples\pvwattsv5run.c sdk-release\examples
 copy examples\ver.c sdk-release\examples
-
 
 copy ..\lk\docs\lk_guide.pdf sdk-release\
 copy ssc\sscapi.h sdk-release\
