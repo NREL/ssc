2c7729b5abd68b82c0190e4678c8f6ede6c3489b
Ty Neises <ty.neises@nrel.gov>
2015-12-07 18:25:59 +0000

    sCO2 FIT Properties: added updated license agreement from John Dyrbey per 12/7/2015 email

--- tcs/CO2_properties.cpp
+++ tcs/CO2_properties.cpp
@@ -1,6 +1,9 @@
 /*********************************** FIT Carbon Dioxide (NREL v1) **********************************
+—
 Copyright (c) 2014, Northland Numerics LLC
 All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following condition is met:
+•     Redistributions of source code must retain the above copyright notice, this condition and the following disclaimer.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND
@@ -13,6 +16,8 @@ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; OR LOSS OF USE, DATA, OR PROFITS) H
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
+—
+
 ***************************************************************************************************/
 
 #include <math.h>
