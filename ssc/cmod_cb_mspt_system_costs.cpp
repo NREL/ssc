46b02e7853257cabaf2468057d71de1686802fd9
Ty Neises <ty.neises@nrel.gov>
2016-01-05 19:26:33 +0000

    updates to mspt system cost compute module

--- ssc/cmod_cb_mspt_system_costs.cpp
+++ ssc/cmod_cb_mspt_system_costs.cpp
@@ -1,6 +1,8 @@
 #include "core.h"
 
 #include "csp_system_costs.h"
+#include "csp_solver_util.h"
+
 
 static var_info _cm_vtab_cb_mspt_system_costs[] = {
 
@@ -47,8 +49,26 @@ static var_info _cm_vtab_cb_mspt_system_costs[] = {
 	{ SSC_INPUT,        SSC_NUMBER,      "sales_tax_frac",              "Percent of cost to which sales tax applies",     "%",            "",            "system_costs",   "*",        "",  "" },
 	{ SSC_INPUT,        SSC_NUMBER,      "sales_tax_rate",              "Sales tax rate",                                 "%",            "",            "system_costs",   "*",        "",  "" },
 
-	{ SSC_OUTPUT,       SSC_NUMBER,      "total_installed_cost",	    "Total installed cost",                           "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.site_improvements",	    "Site improvement cost",                  "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.heliostats",	            "Heliostat cost",                         "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.tower",	                "Tower cost",                             "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.receiver",	            "Receiver cost",                          "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.storage",	                "TES cost",                               "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.power_block",	            "Power cycle cost",                       "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.bop",	                    "BOP cost",                               "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.fossil",	                "Fossil backup cost",                     "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "ui_direct_subtotal",	                "Direct capital precontingency cost",     "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.contingency",	            "Contingency cost",                       "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "total_direct_cost",	                "Total direct cost",                      "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.epc.total",	            "EPC and owner cost",                     "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.plm.total",	            "Total land cost",                        "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.sales_tax.total",	        "Sales tax cost",                         "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "total_indirect_cost",	                "Total indirect cost",                    "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "total_installed_cost",	            "Total installed cost",                   "$",            "",            "system_costs",   "*",        "",  "" },
+	{ SSC_OUTPUT,       SSC_NUMBER,      "csp.pt.cost.installed_per_capacity",  "Estimated installed cost per cap",       "$",            "",            "system_costs",   "*",        "",  "" },
 	
+
+
 	var_info_invalid };
 
 class cm_cb_mspt_system_costs : public compute_module
@@ -80,7 +100,7 @@ public:
 		sys_costs.ms_par.A_rec_ref = as_double("rec_ref_area");
 		sys_costs.ms_par.rec_cost_scaling_exp = as_double("rec_cost_exp");
 
-		sys_costs.ms_par.Q_storage = as_double("csp.pt.cost.storage.mwht");
+		sys_costs.ms_par.Q_storage = as_double("csp.pt.cost.storage_mwht");
 		sys_costs.ms_par.tes_spec_cost = as_double("tes_spec_cost");
 		
 		sys_costs.ms_par.W_dot_design = as_double("P_ref");
@@ -105,9 +125,33 @@ public:
 		sys_costs.ms_par.sales_tax_basis = as_double("sales_tax_frac");
 		sys_costs.ms_par.sales_tax_rate = as_double("sales_tax_rate");
 
+		try
+		{
+			sys_costs.calculate_costs();
+		}
+		catch(C_csp_exception &csp_exception)
+		{
+			throw exec_error("MSPT system costs", util::format("System cost calculations failed. Check that all inputs are properly defined"));
+		}
 		
+		assign("csp.pt.cost.site_improvements", sys_costs.ms_out.site_improvement_cost);
+		assign("csp.pt.cost.heliostats", sys_costs.ms_out.heliostat_cost);
+		assign("csp.pt.cost.tower", sys_costs.ms_out.tower_cost);
+		assign("csp.pt.cost.receiver", sys_costs.ms_out.receiver_cost);
+		assign("csp.pt.cost.storage", sys_costs.ms_out.tes_cost);
+		assign("csp.pt.cost.power_block", sys_costs.ms_out.power_cycle_cost);
+		assign("csp.pt.cost.bop", sys_costs.ms_out.bop_cost);
+		assign("csp.pt.cost.fossil", sys_costs.ms_out.fossil_backup_cost);
+		assign("ui_direct_subtotal", sys_costs.ms_out.direct_capital_precontingency_cost);
+		assign("csp.pt.cost.contingency", sys_costs.ms_out.contingency_cost);
+		assign("total_direct_cost", sys_costs.ms_out.total_direct_cost);
+		assign("csp.pt.cost.epc.total", sys_costs.ms_out.epc_and_owner_cost);
+		assign("csp.pt.cost.plm.total", sys_costs.ms_out.total_land_cost);
+		assign("csp.pt.cost.sales_tax.total", sys_costs.ms_out.sales_tax_cost);
+		assign("total_indirect_cost", sys_costs.ms_out.total_indirect_cost);
+		assign("total_installed_cost", sys_costs.ms_out.total_installed_cost);
+		assign("csp.pt.cost.installed_per_capacity", sys_costs.ms_out.estimated_installed_cost_per_cap);
 
-		return;
 	}
 
 };
