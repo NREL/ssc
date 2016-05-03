f60d35727f45a06943edbe4578bac9c1ba0fd29a
Nick DiOrio <nicholas.diorio@nrel.gov>
2016-05-03 16:15:23 +0000

    Included dispatch class as part of base controller

--- build_vc2013/lib_power_electronics.cpp
+++ build_vc2013/lib_power_electronics.cpp
@@ -1,14 +1,14 @@
 #include "lib_power_electronics.h"
 
-dc_connected_battery_controller::dc_connected_battery_controller(float dc_dc_efficiency, float ac_dc_efficiency) : 
-charge_controller(dc_dc_efficiency, ac_dc_efficiency)
+dc_connected_battery_controller::dc_connected_battery_controller(dispatch_t * dispatch, float dc_dc_efficiency, float ac_dc_efficiency) : 
+charge_controller(dispatch, dc_dc_efficiency, ac_dc_efficiency)
 {
 	_dc_dc_charge_controller = new dc_dc_charge_controller(dc_dc_efficiency);
 	_rectifier = new rectifier(ac_dc_efficiency);
 }
 
-ac_connected_battery_controller::ac_connected_battery_controller(float dc_ac_efficiency, float ac_dc_efficiency) :
-charge_controller(dc_ac_efficiency, ac_dc_efficiency)
+ac_connected_battery_controller::ac_connected_battery_controller(dispatch_t * dispatch, float dc_ac_efficiency, float ac_dc_efficiency) :
+charge_controller(dispatch, dc_ac_efficiency, ac_dc_efficiency)
 {
 	_bidirectional_inverter = new bidirectional_inverter(dc_ac_efficiency, ac_dc_efficiency);
 }
