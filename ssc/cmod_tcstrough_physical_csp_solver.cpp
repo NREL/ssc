1321081cb9b2e830f4e62182ac564891f7dbed1d
Ty Neises <ty.neises@nrel.gov>
2016-05-03 23:31:16 +0000

    updating trough csp solver compute module

--- ssc/cmod_tcstrough_physical_csp_solver.cpp
+++ ssc/cmod_tcstrough_physical_csp_solver.cpp
@@ -8,6 +8,7 @@
 
 #include "csp_solver_trough_collector_receiver.h"
 #include "csp_solver_pc_Rankine_indirect_224.h"
+#include "csp_solver_two_tank_tes.h"
 
 static var_info _cm_vtab_tcstrough_physical_heat[] = {
 //   weather reader inputs
@@ -616,8 +617,37 @@ public:
 			pc->mc_m_dot_htf_ind = as_matrix("ud_m_dot_htf_ind_od");
 		}
 
-
-
+		// ********************************
+		// ********************************
+		// Now add the storage class
+		// ********************************
+		// ********************************
+		C_csp_two_tank_tes storage;
+		C_csp_two_tank_tes::S_params *tes = &storage.ms_params;
+		tes->m_field_fl = as_integer("Fluid");
+		tes->m_field_fl_props = as_matrix("field_fl_props");
+		tes->m_tes_fl = as_integer("Fluid");
+		tes->m_tes_fl_props = as_matrix("field_fl_props");
+		tes->m_is_hx = false;									//[-] Assuming direct storage here
+		tes->m_W_dot_pc_design = as_double("W_pb_design");		//[MWe]
+		tes->m_eta_pc = as_double("eta_ref");					//[-]
+		tes->m_solarm = as_double("solar_mult");				//[-]
+		tes->m_ts_hours = as_double("tshours");					//[hr]
+		tes->m_h_tank = as_double("h_tank");					//[m]
+		tes->m_u_tank = as_double("u_tank");					//[W/m^2-K]
+		tes->m_tank_pairs = as_integer("tank_pairs");			//[-]
+		tes->m_hot_tank_Thtr = as_double("hot_tank_Thtr");		//[C]
+		tes->m_hot_tank_max_heat = as_double("tank_max_heat");	//[MW]
+		tes->m_cold_tank_Thtr = as_double("cold_tank_Thtr");	//[C]
+		tes->m_cold_tank_max_heat = as_double("tank_max_heat");	//[MW]
+		tes->m_dt_hot = 0.0;									//[-] Assuming direct storage here
+		tes->m_T_field_in_des = as_double("T_loop_in_des");		//[C]
+		tes->m_T_field_out_des = as_double("T_loop_out");		//[C]
+		tes->m_T_tank_hot_ini = as_double("T_loop_in_des");		//[C]
+		tes->m_T_tank_cold_ini = as_double("T_loop_out");		//[C]
+		tes->m_h_tank_min = as_double("h_tank_min");			//[m]
+		tes->m_f_V_hot_ini = as_double("V_tank_hot_ini");		//[-]
+		tes->m_htf_pump_coef = as_double("pb_pump_coef");		//[kW/kg/s]
 
 
 
