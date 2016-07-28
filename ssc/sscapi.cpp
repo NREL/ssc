04548118ba8f3bba353aa42abcbd21239a9a6876
Steve Janzou <steven.janzou@nrel.gov>
2016-07-28 09:31:01 +0000

    add cmod_cec_inv_cg.cpp stub for CEC inverter coefficient generator.

--- ssc/sscapi.cpp
+++ ssc/sscapi.cpp
@@ -84,8 +84,8 @@ extern module_entry_info
 	cm_entry_cb_construction_financing,
 	cm_entry_iscc_design_point,
 	cm_entry_battery,
-   	cm_entry_lcoefcr;
-//	cm_entry_pv_get_shade_loss_mpp;
+   	cm_entry_lcoefcr,
+	cm_entry_cec_inv_cg;
 
 /* official module table */
 static module_entry_info *module_table[] = {
@@ -152,7 +152,7 @@ static module_entry_info *module_table[] = {
 	&cm_entry_iscc_design_point,
 	&cm_entry_battery,
 	&cm_entry_lcoefcr,
-//	&cm_entry_pv_get_shade_loss_mpp,
+	&cm_entry_cec_inv_cg,
 	0 };
 
 SSCEXPORT ssc_module_t ssc_module_create( const char *name )
