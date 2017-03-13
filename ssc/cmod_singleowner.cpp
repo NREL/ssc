adc486fe18e6539fcba1f99a89581826454479a1
Steve Janzou <steven.janzou@nrel.gov>
2017-03-13 06:50:09 +0000

    trunk ssc code label clean up and revision 172 not to be confused with 2017.1.17-r1 171

--- ssc/cmod_singleowner.cpp
+++ ssc/cmod_singleowner.cpp
@@ -54,7 +54,7 @@ static var_info _cm_vtab_singleowner[] = {
 	{ SSC_INPUT, SSC_NUMBER, "ppa_multiplier_model", "PPA multiplier model", "0/1", "0=diurnal,1=timestep", "Time of Delivery", "?=0", "INTEGER,MIN=0", "" },
 	{ SSC_INPUT, SSC_ARRAY, "dispatch_factors_ts", "Dispatch payment factor array", "", "", "Time of Delivery", "ppa_multiplier_model=1", "", "" },
 
-	{ SSC_OUTPUT, SSC_ARRAY, "ppa_multipliers", "PPA price multipliers (SO)", "", "", "Time of Delivery", "*", "", "" },
+	{ SSC_OUTPUT, SSC_ARRAY, "ppa_multipliers", "PPA price multipliers", "", "", "Time of Delivery", "*", "", "" },
 
 
 
