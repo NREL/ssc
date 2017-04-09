3b7ce6149d2544b8283c2ac71ee62261b81389eb
Ty Neises <ty.neises@nrel.gov>
2017-04-09 01:26:02 +0000

    temp csp solver updated

--- tcs/csp_solver_core_ADJUSTED.cpp
+++ tcs/csp_solver_core_ADJUSTED.cpp
@@ -7055,7 +7055,7 @@ void C_csp_solver::solver_pc_su_controlled__tes_dc(double step_tol /*s*/,
 	c_solver.settings(1.E-3, 50, std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN(), false);
 
 	double T_htf_hot_guess_hotter = mc_tes.get_hot_temp() - 273.15;	//[C]
-	double T_htf_hot_guess_colder = T_htf_hot_guess_colder - 10.0;	//[C]
+	double T_htf_hot_guess_colder = T_htf_hot_guess_hotter - 10.0;	//[C]
 
 	double T_htf_hot_solved, tol_solved;
 	T_htf_hot_solved = tol_solved = std::numeric_limits<double>::quiet_NaN();
