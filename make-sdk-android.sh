8764f28c0a2dcb7e666763e742cbffd21b322fc6
Steve Janzou <steven.janzou@nrel.gov>
2017-05-29 08:31:26 +0000

    trunk - add script files for ios and android libraries

---
+++ make-sdk-android.sh
@@ -0,0 +1,9 @@
+mkdir sdk-release/android/armeabi
+mkdir sdk-release/android/armeabi-v7a
+mkdir sdk-release/android/x86
+cp -v -R build_android/armeabi/* sdk-release/android/armeabi
+cp -v -R build_android/armeabi-v7a/* sdk-release/android/armeabi-v7a
+cp -v -R build_android/x86/* sdk-release/android/x86
+strip sdk-release/android/armeabi/*
+strip sdk-release/android/armeabi-v7a/*
+strip sdk-release/android/x86/*
