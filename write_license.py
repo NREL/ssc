bc71650e4920439643ffdc72a0a49ae7f6141144
nickdiorio <nadiorio@gmail.com>
2017-07-21 10:01:01 -0600

    Modifications for line endings

--- write_license.py
+++ write_license.py
@@ -6,11 +6,12 @@ import time
 license_file = "LICENSE.txt"
 folders = ['sdktool', 'shared', 'solarpilot', 'ssc', 'tcs', 'tcsconsole']
 
+
 # function to prepend file to source code
 def lines_prepender(filename, lines):
     with open(filename, 'r+') as f:
         content = f.read()
-        f.seek(0,0)
+        f.seek(0, 0)
         f.writelines(lines + '\n\n' + content)
 
 # prepend files
@@ -30,7 +31,8 @@ for folder in folders:
                         with codecs.open(f, 'r', encoding='utf-8') as infile:
                             for line in infile:
                                 if f == license_path:
-                                    outfile.write(line.rstrip('\n'))
+                                    #outfile.write(line)
+                                    outfile.write(line.replace(r"\r\n", r"\n"))
                                 else:
                                     outfile.write(line)
                             outfile.write('\n')
