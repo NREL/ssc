0af073af269083f97e954257df13dee266e8cdaf
nickdiorio <nadiorio@gmail.com>
2017-07-21 08:42:19 -0600

    Update license writer

--- write_license.py
+++ write_license.py
@@ -14,20 +14,32 @@ def lines_prepender(filename, lines):
 
 # prepend files
 top_dir = os.getcwd()
+license_path = os.path.join(top_dir, license_file)
 for folder in folders:
     os.chdir(folder)
     files = os.listdir()
     for file in files:
         if file.endswith("h") or file.endswith("cpp"):
-            tmpfile = "tmp.txt"
-            with open(tmpfile, 'w', encoding='utf-8') as outfile:
-                for f in [os.path.join(top_dir, license_file), file]:
-                     with codecs.open(f, 'r', encoding='utf-8') as infile:
-                        for line in infile:
-                            outfile.write(line)
-                        #shutil.copyfileobj(infile, outfile, 1024 * 1024 * 10)
-            shutil.copyfile(tmpfile, file)
-            os.remove(tmpfile)
+            try:
+                tmpfile = "tmp.txt"
+                with open(tmpfile, 'w', encoding='utf-8') as outfile:
+                    for f in [license_path, file]:
+                        if f == file:
+                            print("Writing: " + file)
+
+                            with codecs.open(f, 'r', encoding='utf-8') as infile:
+                                for line in infile:
+                                    if f == license_path:
+                                        outfile.write(line.rstrip('\n'))
+                                    else:
+                                        outfile.write(line)
+                                outfile.write('\n')
+                shutil.copyfile(tmpfile, file)
+                os.remove(tmpfile)
+            except (UnicodeDecodeError, PermissionError):
+                print("File: " + f + " failed!")
+                os.remove(tmpfile)
+
 
     os.chdir(top_dir)
 
