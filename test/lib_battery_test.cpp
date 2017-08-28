f944f22bac8a0090d1eaf167b123ae4f10f61c16
nickdiorio <nadiorio@gmail.com>
2017-08-28 15:04:25 -0600

    Unit test for lithium-ion battery capacity

--- test/lib_battery_test.cpp
+++ test/lib_battery_test.cpp
@@ -23,7 +23,8 @@ protected:
 
 	void SetUp()
 	{
-		capacity_model = new capacity_lithium_ion_t(q, SOC_min, SOC_max);
+		LithiumIonBatteryProperties::SetUp();
+		capacity_model = new capacity_lithium_ion_t(q, SOC_max, SOC_min);
 	}
 	void TearDown()
 	{
@@ -32,7 +33,35 @@ protected:
 	}
 };
 
-TEST_F(LithiumIonBattery, UnitTest)
+TEST_F(LithiumIonBattery, CapacityUnitTest)
 {
+	// Check that initial capacity is equal to max
 	EXPECT_EQ(capacity_model->SOC(), SOC_max);
+
+	// Check that discharge of battery results in correct capacity
+	capacity_model->updateCapacity(10, 1);
+	EXPECT_EQ(capacity_model->SOC(), 90);
+	EXPECT_EQ(capacity_model->q0(), 90);
+
+	// check that charge of battery results in correct capacity
+	capacity_model->updateCapacity(-10, 1);
+	EXPECT_EQ(capacity_model->SOC(), 100);
+	EXPECT_EQ(capacity_model->q0(), 100);
+
+	// check that updating thermal behavior changes capacity as expected
+	capacity_model->updateCapacityForThermal(95);
+	EXPECT_EQ(capacity_model->q0(), 95);
+	EXPECT_EQ(capacity_model->qmax(), 100);
+	capacity_model->updateCapacityForThermal(100);
+
+	// check that updating lifetime degradation changes capacity
+	capacity_model->updateCapacityForLifetime(95);
+	EXPECT_EQ(capacity_model->q0(), 95);
+	EXPECT_EQ(capacity_model->qmax(), 95);
+
+	// check that battery replacement works
+	capacity_model->replace_battery();
+	EXPECT_EQ(capacity_model->SOC(), SOC_max);
+	EXPECT_EQ(capacity_model->q0(), 100);
+	EXPECT_EQ(capacity_model->qmax(), 100);
 }
