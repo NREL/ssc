832985be3443566661552bcd57d86cd562fdc2d3
sjanzou <steven.janzou@nrel.gov>
2017-11-16 01:16:16 -0700

    Fix bug from Galen using subhourly resource data

--- ssc/cmod_windpower.cpp
+++ ssc/cmod_windpower.cpp
@@ -268,6 +268,7 @@ void cm_windpower::exec() throw(general_error)
 		else if (is_assigned("wind_resource_data"))
 		{
 			wdprov = std::auto_ptr<winddata_provider>(new winddata(lookup("wind_resource_data")));
+			nstep = wdprov->nrecords(); // missing - causing issue from Galen 11/15/17
 		}
 		else
 			throw exec_error("windpower", "no wind resource data supplied");
