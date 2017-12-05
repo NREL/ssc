135c9436c69df771f6ae478ead33494f6f9010c1
Darice <29387736+dguittet@users.noreply.github.com>
2017-12-05 16:48:38 -0700

    remove GMOCK dependency

--- test/shared_test/lib_windwatts_test.cpp
+++ test/shared_test/lib_windwatts_test.cpp
@@ -67,7 +67,7 @@ TEST_F(windPowerCalculatorTest, windPowerUsingResource_lib_windwatts){
 	std::shared_ptr<fakeWakeModel> fakeWM(new fakeWakeModel());
 	double airDensitySaved = 0.0;
 
-	wpc.InitializeModel(fakeWM); // sets up wpc's wake model as mock
+	wpc.InitializeModel(fakeWM); 
 	int run = wpc.windPowerUsingResource(windSpeedData, windDirData, pressureData, tempData, &farmPower, &power[0], &thrust[0],
 		&eff[0], &windSpeed[0], &turbulenceCoeff[0], &distDownwind[0], &distCrosswind[0]); // runs method we want to test
 	EXPECT_EQ(run, 3);
