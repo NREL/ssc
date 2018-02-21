7bfed5fd12719878628509453e540259f8db7ba4
Mike Wagner <mike.wagner@nrel.gov>
2018-02-21 16:38:59 -0700

    linux build fixes

--- solarpilot/sort_method.h
+++ solarpilot/sort_method.h
@@ -161,17 +161,17 @@ const Comparable & median3( std::vector<Comparable> & a, int left, int right )
 {
     int center = ( left + right ) / 2;
     if( a[ center ] < a[ left ] ){
-        swap( a[ left ], a[ center ] );
+        std::swap( a[ left ], a[ center ] );
 		}
     if( a[ right ] < a[ left ] ){
-        swap( a[ left ], a[ right ] );
+        std::swap( a[ left ], a[ right ] );
 		}
     if( a[ right ] < a[ center ] ){
-        swap( a[ center ], a[ right ] );
+        std::swap( a[ center ], a[ right ] );
 		}
 
         // Place pivot at position right - 1
-    swap( a[ center ], a[ right - 1 ] );
+    std::swap( a[ center ], a[ right - 1 ] );
 	
     return a[ right - 1 ];
 }
@@ -222,12 +222,12 @@ void quicksort( std::vector<Comparable> & a, int left, int right )
             while( a[ ++i ] < pivot ) { }
             while( pivot < a[ --j ] ) { }
             if( i < j ){
-                swap( a[ i ], a[ j ] );
+                std::swap( a[ i ], a[ j ] );
 				}
 			else {break;}
         }
 
-        swap( a[ i ], a[ right - 1 ] );  // Restore pivot
+        std::swap( a[ i ], a[ right - 1 ] );  // Restore pivot
 		
 
         quicksort( a, left, i - 1 );     // Sort small elements
