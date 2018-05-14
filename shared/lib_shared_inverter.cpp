445fa93492c5a2804444472a4459f54da0f86908
Nick DiOrio <nadiorio@gmail.com>
2018-05-14 12:56:59 -0600

    Fix inverter model selection in shared inverter

--- shared/lib_shared_inverter.cpp
+++ shared/lib_shared_inverter.cpp
@@ -15,7 +15,7 @@ void SharedInverter::calculateACPower(const double powerDC_Watts, const double D
 	double P_par, P_lr;
 
 	// Power quantities go in and come out in units of W
-	if (m_inverterType == SANDIA_INVERTER)
+	if (m_inverterType == SANDIA_INVERTER || m_inverterType == DATASHEET_INVERTER || m_inverterType == COEFFICIENT_GENERATOR)
 		m_sandiaInverter->acpower(powerDC_Watts / m_numInverters, DCStringVoltage, &powerAC_kW, &P_par, &P_lr, &efficiencyAC, &powerClipLoss_kW, &powerConsumptionLoss_kW, &powerNightLoss_kW);
 	else if (m_inverterType == PARTLOAD_INVERTER)
 		m_partloadInverter->acpower(powerDC_Watts / m_numInverters, &powerAC_kW, &P_lr, &P_par, &efficiencyAC, &powerClipLoss_kW, &powerNightLoss_kW);
