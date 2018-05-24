33433dba1edda7375719a66b54e764f8114df1e1
sjanzou <steven.janzou@nrel.gov>
2018-05-24 04:27:37 -0600

    Fix crashes in CSP models with leap year weather file updates
    Fixes issues with skipping February 29 in lib_weatherfile.cpp used by csp_solver_weatherreader.cpp

--- ssc/cmod_linear_fresnel_dsg_iph.cpp
+++ ssc/cmod_linear_fresnel_dsg_iph.cpp
@@ -228,7 +228,9 @@ public:
 		weather_reader.m_trackmode = 0;
 		weather_reader.m_tilt = 0.0;
 		weather_reader.m_azimuth = 0.0;
+		// Initialize to get weather file info
 		weather_reader.init();
+		if (weather_reader.has_error()) throw exec_error("linear_fresnel_dsg_iph", weather_reader.get_error());
 
 		// Set up ssc output arrays
 		// Set steps per hour
