8b51da9434414f81ab4fc9c3be7ad77df88b2fed
Darice <darice.guittet@nrel.gov>
2018-07-11 17:46:00 -0600

    add temp derate curve to shared_inverter

--- shared/lib_shared_inverter.cpp
+++ shared/lib_shared_inverter.cpp
@@ -1,5 +1,6 @@
 #include "lib_shared_inverter.h"
 #include "lib_util.h"
+#include <algorithm>
 
 SharedInverter::SharedInverter(int inverterType, int numberOfInverters,
 	sandia_inverter_t * sandiaInverter, partload_inverter_t * partloadInverter)
@@ -8,9 +9,81 @@ SharedInverter::SharedInverter(int inverterType, int numberOfInverters,
 	m_numInverters = numberOfInverters;
 	m_sandiaInverter = sandiaInverter;
 	m_partloadInverter = partloadInverter;
+	m_tempEnabled = false;
+	m_tempV = {0, 0};
+	m_tempSlope = {0, 0, 0};
+	m_tempStartC = {-99, -99, -99};
 }
 
-void SharedInverter::calculateACPower(const double powerDC_Watts, const double DCStringVoltage)
+bool sortByVoltage(double[] i, double[] j)
+{
+	return (i[0] < j[0]);
+}
+
+bool setTempDerateCurves(double[] curve1, double[] curve2, double[] curve3 )
+{
+	if (curve1[0] < 0 || curve1[1] < 0 || curve1[2] >= 0) return false;
+
+	// if multiple curves provided, partition operating V range
+	std::vector<double*> derateCurves;
+	if (curve2[0] > 0){
+		if (curve2[1] <= -99 || curve2[2] >= 0) return false;
+		derateCurves.push_back(curve2);
+	}
+	if (curve3[0] > 0){
+		if (curve3[1] <= -99 || curve3[2] >= 0) return false;
+		derateCurves.push_back(curve3);
+	}
+
+	std::sort(derateCurves.begin(), derateCurves.end(), sortByVoltage);
+	m_tempStartC[0] = derateCurves[0][1];
+	m_tempSlope[0] = derateCurves[0][2];
+
+	if (derateCurves.size() > 1){
+		m_tempV[0] = (derateCurves[1][0] - derateCurves[0][0]/2);
+		m_tempStartC[1] = derateCurves[1][1];
+		m_tempSlope[1] = derateCurves[1][2];
+		if (derateCurves.size() > 2) {
+			m_tempV[1] = (derateCurves[2][0] - derateCurves[1][0]/2);
+			m_tempStartC[3] = derateCurves[3][1];
+			m_tempSlope[3] = derateCurves[3][2];
+		}
+	}
+
+	m_tempEnabled = true;
+	return true;
+}
+
+void SharedInverter::calculateTempDerate(double V, double T, double& pAC, double& eff, double& loss)
+{
+	double slope = 0.0;
+	double deltaT = 0.0;
+
+	if (m_tempV[0] == 0 || (m_tempV[0] != 0 && V <= m_tempV[0])){
+		if (T <= m_tempStartC[0]) return;
+		slope = m_tempSlope[0];
+		deltaT = T - m_tempStartC[0];
+	}
+	else{
+		if (m_tempV[1] == 0 || (m_tempV[1] != 0 && V <= m_tempV[1])){
+			if (T <= m_tempStartC[1]) return;
+			slope = m_tempSlope[1];
+			deltaT = T - m_tempStartC[1];
+		}
+		else{
+			if (T <= m_tempStartC[2]) return;
+			slope = m_tempSlope[2];
+			deltaT = T - m_tempStartC[2];
+		}
+	}
+
+	double pDC = pAC/eff;
+	eff -= deltaT*slope;
+	loss = pAC - (pDC * eff);
+	pAC = pDC * eff;	
+}
+
+void SharedInverter::calculateACPower(const double powerDC_Watts, const double DCStringVoltage, double T)
 {
 	double P_par, P_lr;
 
@@ -20,12 +93,18 @@ void SharedInverter::calculateACPower(const double powerDC_Watts, const double D
 	else if (m_inverterType == PARTLOAD_INVERTER)
 		m_partloadInverter->acpower(powerDC_Watts / m_numInverters, &powerAC_kW, &P_lr, &P_par, &efficiencyAC, &powerClipLoss_kW, &powerNightLoss_kW);
 
+	double tempLoss = 0.0;
+	if (m_tempEnabled){
+		calculateTempDerate(DCStringVoltage, T, &powerAC_kW, &efficiencyAC, &tempLoss);
+	}
+
 	// Convert units to kW
 	powerDC_kW = powerDC_Watts * util::watt_to_kilowatt;
 	powerAC_kW *= m_numInverters * util::watt_to_kilowatt;
 	powerClipLoss_kW *= m_numInverters * util::watt_to_kilowatt;
 	powerConsumptionLoss_kW *= m_numInverters * util::watt_to_kilowatt;
 	powerNightLoss_kW *= m_numInverters * util::watt_to_kilowatt;
+	powerTempLoss_kW = tempLoss * m_numInverters * util::watt_to_kilowatt;
 	efficiencyAC *= 100;
 }
 
--- shared/lib_shared_inverter.h
+++ shared/lib_shared_inverter.h
@@ -3,6 +3,7 @@
 
 #include "lib_sandia.h"
 #include "lib_pvinv.h"
+#include <vector>
 
 /**
 *
@@ -15,15 +16,22 @@ class SharedInverter
 {
 public:
 
-	/// Construct a shared inverter by registering the previously constructed inverter 
+	/// Construct a shared inverter by registering the previously constructed inverter
 	SharedInverter(int inverterType, int numberOfInverters,
 		sandia_inverter_t * sandiaInverter, partload_inverter_t * partloadInverter);
 
-	/// Given the combined PV plus battery DC power (W) and voltage, compute the AC power (kW)
-	void calculateACPower(const double powerDC, const double DCStringVoltage);
+	/// Setup efficiency vs ambient T curves at up to 3 input voltages for temp derating, returns true if successful
+	bool setTempDerateCurves(double[] curve1,
+		double[] curve2 = {0, -99, 0}, double[] curve3 = {0, -99, 0} );
+
+	/// Given DC voltage and ambient temperate, calculate derated power, eff and loss
+	void calculateTempDerate(double V, double T, double& pAC, double& eff, double& loss);
+
+	/// Given the combined PV plus battery DC power (W), voltage and ambient T, compute the AC power (kW)
+	void calculateACPower(const double powerDC, const double DCStringVoltage, double ambientT = 0.0);
 
 	/// Return the nominal DC voltage input
-	double getInverterDCNominalVoltage();  
+	double getInverterDCNominalVoltage();
 
 	enum { SANDIA_INVERTER, DATASHEET_INVERTER, PARTLOAD_INVERTER, COEFFICIENT_GENERATOR, NONE };
 
@@ -36,11 +44,18 @@ public:
 	double powerClipLoss_kW;
 	double powerConsumptionLoss_kW;
 	double powerNightLoss_kW;
+	double powerTempLoss_kW;
 
 protected:
 
 	int m_inverterType;  /// The inverter type
 	int m_numInverters;  /// The number of inverters in the system
+
+	// temperature derate curves
+	bool m_tempEnabled;
+	double[2] m_tempV;				/// ordered DC voltages which divide operating V range into up to 2 partitions
+	double[3] m_tempStartC;		/// for each V range, the temperature at which derate begins to be applied
+	double[3] m_tempSlope;		/// for each V range, the slope, efficiency%/degree C
 
 	// Memory managed elsewehre
 	sandia_inverter_t * m_sandiaInverter;
@@ -49,4 +64,3 @@ protected:
 
 
 #endif
-
