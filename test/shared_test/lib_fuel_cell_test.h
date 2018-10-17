ffad1dcc0d3766c232fd39e37f72889504ca216c
nickdiorio <nadiorio@gmail.com>
2018-10-17 13:28:29 -0600

    Add unit test framework for fuel_cell

---
+++ test/shared_test/lib_fuel_cell_test.h
@@ -0,0 +1,32 @@
+#ifndef __LIB_BATTERY_POWERFLOW_TEST_H__
+#define __LIB_BATTERY_POWERFLOW_TEST_H__
+
+#include <gtest/gtest.h>
+
+/**
+* \class FuelCellTest
+*
+* This class contains the setup and teardown structure required to test the fuel cell model
+*
+*/
+class FuelCellTest : public ::testing::Test
+{
+protected:
+
+
+public:
+
+	void SetUp()
+	{
+		
+	}
+	void TearDown()
+	{
+		
+	}
+
+};
+
+
+
+#endif
