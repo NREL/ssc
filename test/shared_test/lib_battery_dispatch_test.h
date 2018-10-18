c3b1ea4784367bd20a48f3c3e72ed93105087a14
nickdiorio <nadiorio@gmail.com>
2018-10-18 08:42:23 -0600

    Add framework for more tests

---
+++ test/shared_test/lib_battery_dispatch_test.h
@@ -0,0 +1,34 @@
+#ifndef __LIB_BATTERY_POWERFLOW_TEST_H__
+#define __LIB_BATTERY_POWERFLOW_TEST_H__
+
+#include <gtest/gtest.h>
+#include "../shared/lib_battery_powerflow.h"
+#include <lib_power_electronics.h>
+
+/**
+* \class BatteryDispatchTest
+*
+* Test various components of the battery dispatch algorithms
+*
+*/
+class BatteryDispatchTest : public ::testing::Test
+{
+protected:
+	
+
+public:
+
+	void SetUp()
+	{
+		
+	}
+	void TearDown()
+	{
+
+	}
+
+};
+
+
+
+#endif
