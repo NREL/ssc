c288ce18b9038d28c7264d526f20198340f5f062
Nick DiOrio <nicholas.diorio@nrel.gov>
2018-11-26 10:04:10 -0700

    Fix Linux Build

--- shared/lib_irradproc.h
+++ shared/lib_irradproc.h
@@ -2,7 +2,7 @@
 *  Copyright 2017 Alliance for Sustainable Energy, LLC
 *
 *  NOTICE: This software was developed at least in part by Alliance for Sustainable Energy, LLC
-*  (“Alliance”) under Contract No. DE-AC36-08GO28308 with the U.S. Department of Energy and the U.S.
+*  (ï¿½Allianceï¿½) under Contract No. DE-AC36-08GO28308 with the U.S. Department of Energy and the U.S.
 *  The Government retains for itself and others acting on its behalf a nonexclusive, paid-up,
 *  irrevocable worldwide license in the software to reproduce, prepare derivative works, distribute
 *  copies to the public, perform publicly and display publicly, and to permit others to do so.
@@ -26,8 +26,8 @@
 *  4. Redistribution of this software, without modification, must refer to the software by the same
 *  designation. Redistribution of a modified version of this software (i) may not refer to the modified
 *  version by the same designation, or by any confusingly similar designation, and (ii) must refer to
-*  the underlying software originally provided by Alliance as “System Advisor Model” or “SAM”. Except
-*  to comply with the foregoing, the terms “System Advisor Model”, “SAM”, or any confusingly similar
+*  the underlying software originally provided by Alliance as ï¿½System Advisor Modelï¿½ or ï¿½SAMï¿½. Except
+*  to comply with the foregoing, the terms ï¿½System Advisor Modelï¿½, ï¿½SAMï¿½, or any confusingly similar
 *  designation may not be used to refer to any modified version of this software or any modified
 *  version of the underlying software originally provided by Alliance without the prior written consent
 *  of Alliance.
@@ -285,23 +285,28 @@ double backtrack(double solazi, double solzen, double tilt, double azimuth, doub
 class irrad
 {
 protected:
-
-	poaDecompReq * poaAll;			/// Data required to decompose input plane-of-array irradiance
 	
-	// Time inputs
-	int year, month, day, hour;
-	double minute, delt;
-
 	// Position inputs
 	double latitudeDegrees;			///< latitude in degrees, north positive
 	double longitudeDegrees;		///< longitude in degrees, east positive
 	double timezone;				///< time zone, west longitudes negative
 
 	// Model settings
-	int radiationMode;				///< radiation input mode as defined in \link Irradiance_IO::RADMODE
 	int skyModel;					///< sky model selection as defined in \link Irradiance_IO::SKYMODEL 
+	int radiationMode;				///< radiation input mode as defined in \link Irradiance_IO::RADMODE
 	int trackingMode;				///< the subarray tracking model as defined in \link Subarray_IO::tracking
 	bool enableBacktrack;			///< Boolean value for whether backtracking is enabled or not
+
+	// Time inputs
+	int year, month, day, hour;
+	double minute, delt;
+
+	// Subarray properties
+	double tiltDegrees;				///< Surface tilt of subarray in degrees
+	double surfaceAzimuthDegrees;	///< Surface azimuth of subarray in degrees
+	double rotationLimitDegrees;	///< Rotation limit for subarray in degrees
+	double groundCoverageRatio;		///< Ground coverage ratio of subarray
+	poaDecompReq * poaAll;			///< Data required to decompose input plane-of-array irradiance
 
 	// Input Front-Side Irradiation components 
 	double globalHorizontal;		///< Input global horizontal irradiance (W/m2)
@@ -313,12 +318,6 @@ protected:
 	// Calculated Front-Side Irradiation components
 	double calculatedDirectNormal;		///< Calculated direct normal irradiance (W/m2)
 	double calculatedDiffuseHorizontal; ///< Calculated diffuse horizontal irradiance (W/m2)
-
-	// Subarray properties
-	double tiltDegrees;				///< Surface tilt of subarray in degrees
-	double surfaceAzimuthDegrees;	///< Surface azimuth of subarray in degrees
-	double rotationLimitDegrees;	///< Rotation limit for subarray in degrees
-	double groundCoverageRatio;		///< Ground coverage ratio of subarray
 
 	// Outputs
 	double sunAnglesRadians[9];				///< Sun angles in radians calculated from solarpos()	
