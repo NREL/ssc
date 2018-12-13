7410df7f2b4eb087f38cac50df7c9ba61f7cc157
nickdiorio <nadiorio@gmail.com>
2018-12-13 10:31:56 -0700

    Start fix for CMTcsMoltenSalt test

--- test/ssc_test/cmod_tcsmolten_salt_test.cpp
+++ test/ssc_test/cmod_tcsmolten_salt_test.cpp
@@ -5,7 +5,7 @@
 #include "../input_cases/weather_inputs.h"
 
 /// Test tcsmolten_salt with all defaults and the single owner financial model
-TEST_F(CMTcsMoltenSalt, DISABLED_DefaultSingleOwnerFinancialModel) {
+TEST_F(CMTcsMoltenSalt, DefaultSingleOwnerFinancialModel) {
 
     ssc_data_t data = ssc_data_create();
     int test_errors = tcsmolten_salt_daggett(data);
