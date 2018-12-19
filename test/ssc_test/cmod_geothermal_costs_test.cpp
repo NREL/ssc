f7d963344fa3176be66905e611f11821dca5e81e
Parangat Bhaskar <parangat.bhaskar94@gmail.com>
2018-12-19 11:14:09 -0700

    Add geothermal test framework

---
+++ test/ssc_test/cmod_geothermal_costs_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "cmod_geothermal_costs_test.h"
+
+/// Test Geothermal with all defaults and single owner model
+TEST_F(CMGeothermal, SingleOwnerDefault) {
+
+	// Run with fixed output
+	int geothermal_errors = run_module(data, "geothermal");
+	EXPECT_EQ(geothermal_errors, 0);
+
+	int singleowner_errors = run_module(data, "singleowner");
+	EXPECT_EQ(singleowner_errors, 0);
+
+	if (!geothermal_errors)
+	{
+		ssc_number_t annual_energy;
+		ssc_data_get_number(data, "annual_energy", &annual_energy);		
+		EXPECT_NEAR(annual_energy, 262800000, 0.1);
+	}
+}
