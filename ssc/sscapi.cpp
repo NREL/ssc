020a830253a54f140e2e50c1aface69c0dae2cc2
yassineennassiri <36824782+yassineennassiri@users.noreply.github.com>
2019-04-03 10:40:24 +0100

    Update sscapi.cpp

--- ssc/sscapi.cpp
+++ ssc/sscapi.cpp
@@ -157,6 +157,7 @@ extern module_entry_info
 	cm_entry_pv_get_shade_loss_mpp,
 	cm_entry_inv_cec_cg,
 	cm_entry_thermalrate;
+	cm_alkaline_electrolyzer;
 
 /* official module table */
 static module_entry_info *module_table[] = {
@@ -243,6 +244,7 @@ static module_entry_info *module_table[] = {
 	&cm_entry_pv_get_shade_loss_mpp,
 	&cm_entry_inv_cec_cg,
 	&cm_entry_thermalrate,
+	&cm_alkaline_electrolyzer,
 	0 };
 
 SSCEXPORT ssc_module_t ssc_module_create( const char *name )
