1de646f31d236ce5522a204147c4da8af4865bc8
Nick DiOrio <nadiorio@gmail.com>
2019-04-23 14:49:17 -0600

    Add lifetime test for grid test

--- test/input_cases/grid_common_data.h
+++ test/input_cases/grid_common_data.h
@@ -9,10 +9,13 @@ namespace {
 	char gen_path_grid[256];
 	char load_profile_path_grid_30[256];
 	char gen_path_grid_30[256];
+	char gen_path_grid_30_2yr[256];
+
 	int nfc1 = sprintf(gen_path_grid, "%s/test/input_cases/generic_system_data/energy_output_array.csv", SSCDIR);
 	int nfc2 = sprintf(load_profile_path_grid, "%s/test/input_cases/general_data/commercial_load.csv", SSCDIR);
 	int nfc3 = sprintf(gen_path_grid_30, "%s/test/input_cases/generic_system_data/energy_output_array_30min.csv", SSCDIR);
 	int nfc4 = sprintf(load_profile_path_grid_30, "%s/test/input_cases/generic_system_data/load_30min.csv", SSCDIR);
+	int nfc5 = sprintf(gen_path_grid_30_2yr, "%s/test/input_cases/generic_system_data/energy_output_array_30min-2yr.csv", SSCDIR);
 
 }
 
@@ -30,6 +33,15 @@ void grid_default_30_min(ssc_data_t &data)
 	set_array(data, "gen", gen_path_grid_30, 8760*2);
 	ssc_data_set_number(data, "system_use_lifetime_output", 0);
 	ssc_data_set_number(data, "analysis_period", 30);
+	set_array(data, "load", load_profile_path_grid_30, 8760 * 2);
+	ssc_data_set_number(data, "grid_interconnection_limit_kwac", 180000);
+}
+
+void grid_default_30_min_lifetime(ssc_data_t &data)
+{
+	set_array(data, "gen", gen_path_grid_30_2yr, 8760 * 2 * 2);
+	ssc_data_set_number(data, "system_use_lifetime_output", 1);
+	ssc_data_set_number(data, "analysis_period", 2);
 	set_array(data, "load", load_profile_path_grid_30, 8760 * 2);
 	ssc_data_set_number(data, "grid_interconnection_limit_kwac", 180000);
 }
