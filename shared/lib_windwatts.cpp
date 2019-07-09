6d5c77896f526eaa5e0ae74285ba0e847e1f4512
dguittet <dguittet@nrel.gov>
2019-07-09 11:11:11 -0600

    fix windpower tests

--- shared/lib_windwatts.cpp
+++ shared/lib_windwatts.cpp
@@ -492,8 +492,8 @@ bool windPowerCalculator::windPowerUsingDistribution(std::vector<std::vector<dou
         double freq = 8760.0 * row[2];
         for (i = 0; i<nTurbines; i++){
             farmpower += freq * power[i];
-            farmgross += freq * fTurbine_gross;
         }
+        farmgross += freq * fTurbine_gross * nTurbines;
     }
 
 
