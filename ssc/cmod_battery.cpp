fd59af457ba389763a98e59137520e60376b050b
Nick DiOrio <nadiorio@gmail.com>
2019-09-09 14:46:13 -0600

    Fixes bug in front of meter standalone battery

--- ssc/cmod_battery.cpp
+++ ssc/cmod_battery.cpp
@@ -1433,10 +1433,8 @@ public:
 
 			// Create battery structure and initialize
 			battstor batt(*this, true, n_rec_single_year, dt_hour_gen);
+			batt.initialize_automated_dispatch(power_input_lifetime, load_lifetime);
 
-			if (batt.batt_vars->batt_meter_position == dispatch_t::BEHIND){
-				batt.initialize_automated_dispatch(power_input_lifetime, load_lifetime);
-			}
 
 			if (load_lifetime.size() != n_rec_lifetime) {
 				throw exec_error("battery", "Load length does not match system generation length");
