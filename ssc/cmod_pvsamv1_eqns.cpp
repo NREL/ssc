cab142a2e303574f0dfd2c1937cbbfde3effc649
dguittet <dguittet@nrel.gov>
2019-09-16 14:00:20 -0600

    reopt_size_battery convert some inputs to optional

--- ssc/cmod_pvsamv1_eqns.cpp
+++ ssc/cmod_pvsamv1_eqns.cpp
@@ -23,6 +23,22 @@ void map_input(var_table* vt, const std::string& sam_name, var_table* reopt_tabl
     }
 }
 
+void map_optional_input(var_table* vt, const std::string& sam_name, var_table* reopt_table, const std::string& reopt_name,
+        double def_val, bool to_ratio = false){
+    double sam_input;
+    try{
+        vt_get_double(vt, sam_name, &sam_input);
+        if (to_ratio) sam_input /= 100.;
+    }
+    catch (std::runtime_error&) {
+        sam_input = def_val;
+    }
+    if (var_data* vd = reopt_table->lookup(reopt_name)){
+        throw std::runtime_error(reopt_name + " variable already exists in 'reopt_table'.");
+    }
+    reopt_table->assign(reopt_name, sam_input);
+}
+
 SSCEXPORT void Reopt_size_battery_params(ssc_data_t data) {
     auto vt = static_cast<var_table*>(data);
     if (!vt){
@@ -47,22 +63,22 @@ SSCEXPORT void Reopt_size_battery_params(ssc_data_t data) {
     map_input(vt, "system_capacity", &reopt_pv, "existing_kw");
     map_input(vt, "system_capacity", &reopt_pv, "max_kw");
     map_input(vt, "degradation", &reopt_pv, "degradation_pct", false, true);
-    
-	if (auto vd = vt->lookup("module_type"))
-		reopt_pv.assign("module_type", vd->num[0]);
-	else
-		reopt_pv.assign("module_type", 1);
+
+    map_optional_input(vt, "module_type", &reopt_pv, "module_type", 1);
 
     int opt1, opt2;
-    try{
-        vt_get_int(vt, "subarray1_track_mode", &opt1);
-        vt_get_int(vt, "subarray1_backtrack", &opt2);
+    var_data* vd, *vd2;
+    vd = vt->lookup("subarray1_track_mode");
+    vd2 = vt->lookup("subarray1_backtrack");
+    if (vd && vd2){
+        opt1 = (int)vd->num[0];
+        opt2 = (int)vd2->num[0];
         if (opt1 == 2 && opt2 == 1)
             opt1 = 3;
         std::vector<int> opt_map = {0, 0, 2, 3, 4};
         reopt_pv.assign("array_type", opt_map[opt1]);
     }
-    catch(std::runtime_error&){
+    else{
         map_input(vt, "array_type", &reopt_pv, "array_type");
     }
 
@@ -82,65 +98,72 @@ SSCEXPORT void Reopt_size_battery_params(ssc_data_t data) {
 
 
     // Get appropriate inverter efficiency input and transform to ratio from percent
-	int inv_model = 0;
-    try{
+    int inv_model = 0;
+    double val1, val2, system_cap;
+    vt_get_double(vt, "system_capacity", &system_cap);
+	vd = vt->lookup("inverter_model");
+	if (vd){
         std::vector<std::string> inv_eff_names = {"inv_snl_eff_cec", "inv_ds_eff", "inv_pd_eff", "inv_cec_cg_eff"};
         double eff;
-        vt_get_int(vt, "inverter_model", &inv_model);
+        inv_model = (int)vd->num[0];
         if (inv_model == 4)
             throw std::runtime_error("Inverter Mermoud Lejeune Model not supported.");
         vt_get_double(vt, inv_eff_names[inv_model], &eff);
         eff /= 100.;
         reopt_pv.assign("inv_eff", eff);
         reopt_batt.assign("inverter_efficiency_pct", eff);
-    }
-    catch(std::runtime_error&){
-        map_input(vt, "inv_eff", &reopt_pv, "inv_eff", false, true);
-        map_input(vt, "inv_eff", &reopt_batt, "inverter_efficiency_pct", false, true);
-    }
 
-    double val1, val2, system_cap;
-	vt_get_double(vt, "system_capacity", &system_cap);
-
-    // calculate the dc ac ratio
-    try{
+        // calculate the dc ac ratio
         std::vector<std::string> inv_power_names = {"inv_snl_paco", "inv_ds_paco", "inv_pd_paco", "inv_cec_cg_paco"};
         vt_get_double(vt, inv_power_names[inv_model], &val1);
         vt_get_double(vt, "inverter_count", &val2);
         reopt_pv.assign("dc_ac_ratio", system_cap * 1000. / (val2 * val1));
     }
-    catch(std::runtime_error&) {
+	else{
+        map_input(vt, "inv_eff", &reopt_pv, "inv_eff", false, true);
+        map_input(vt, "inv_eff", &reopt_batt, "inverter_efficiency_pct", false, true);
         map_input(vt, "dc_ac_ratio", &reopt_pv, "dc_ac_ratio");
     }
 
     // financial inputs
-    map_input(vt, "itc_fed_percent", &reopt_pv, "federal_itc_pct", false, true);
-    map_input(vt, "pbi_fed_amount", &reopt_pv, "pbi_us_dollars_per_kwh");
-    map_input(vt, "pbi_fed_term", &reopt_pv, "pbi_years");
-    auto vd = reopt_pv.lookup("pbi_years");
+    map_optional_input(vt, "itc_fed_percent", &reopt_pv, "federal_itc_pct", 0., true);
+    map_optional_input(vt, "pbi_fed_amount", &reopt_pv, "pbi_us_dollars_per_kwh", 0.);
+    map_optional_input(vt, "pbi_fed_term", &reopt_pv, "pbi_years", 0.);
+    vd = reopt_pv.lookup("pbi_years");
     if (vd->num[0] < 1) vd->num[0] = 1;    // minimum is 1, set pbi_fed_amount to 0 to disable
 
-    map_input(vt, "ibi_sta_percent", &reopt_pv, "state_ibi_pct", false, true);
-    map_input(vt, "ibi_sta_percent_maxvalue", &reopt_pv, "state_ibi_max_us_dollars");
+    map_optional_input(vt, "ibi_sta_percent", &reopt_pv, "state_ibi_pct", 0., true);
+    map_optional_input(vt, "ibi_sta_percent_maxvalue", &reopt_pv, "state_ibi_max_us_dollars", 10000000000);
     vd = reopt_pv.lookup("state_ibi_max_us_dollars");
     if (vd->num[0] > 10000000000) vd->num[0] = 10000000000;
 
-    map_input(vt, "ibi_uti_percent", &reopt_pv, "utility_ibi_pct", false, true);
-    map_input(vt, "ibi_uti_percent_maxvalue", &reopt_pv, "utility_ibi_max_us_dollars");
+    map_optional_input(vt, "ibi_uti_percent", &reopt_pv, "utility_ibi_pct", 0., true);
+    map_optional_input(vt, "ibi_uti_percent_maxvalue", &reopt_pv, "utility_ibi_max_us_dollars", 10000000000);
     vd = reopt_pv.lookup("utility_ibi_max_us_dollars");
     if (vd->num[0] > 10000000000) vd->num[0] = 10000000000;
 
-    vt_get_double(vt, "om_fixed", &val1);
-    vt_get_double(vt, "om_production", &val2);
-    reopt_pv.assign("om_cost_us_dollars_per_kw", (val1 / system_cap) + val2);
+    vd = vt->lookup("om_fixed");
+    vd2 = vt->lookup("om_production");
 
-    map_input(vt, "total_installed_cost", &reopt_pv, "installed_cost_us_dollars_per_kw");
-    reopt_pv.lookup("installed_cost_us_dollars_per_kw")->num[0] /= system_cap;
+    if (vd && !vd2){
+        reopt_pv.assign("om_cost_us_dollars_per_kw", vd->num[0] / system_cap);
+    }
+    else if (!vd && vd2) {
+        reopt_pv.assign("om_cost_us_dollars_per_kw", vd2->num[0]);
+    }
+    else if (vd && vd2){
+        reopt_pv.assign("om_cost_us_dollars_per_kw", (vd->num[0] / system_cap) + vd2->num[0]);
+    }
+
+    vd = vt->lookup("total_installed_cost");
+    if (vd){
+        reopt_pv.assign("installed_cost_us_dollars_per_kw", vd->num[0]/system_cap);
+    }
 
     vd = vt->lookup("depr_bonus_fed");
     if (vd){
-        map_input(vt, "depr_bonus_fed", &reopt_pv, "macrs_bonus_pct", false, true);
-        map_input(vt, "depr_bonus_fed", &reopt_batt, "macrs_bonus_pct", false, true);
+        reopt_pv.assign("macrs_bonus_pct", vd->num[0]/100.);
+        reopt_batt.assign("macrs_bonus_pct", vd->num[0]/100.);
     }
     vd = vt->lookup("depr_bonus_fed_macrs_5");
     if (vd && vd->num[0] == 1){
@@ -148,23 +171,33 @@ SSCEXPORT void Reopt_size_battery_params(ssc_data_t data) {
         reopt_batt.assign("macrs_option_years", 5);
     }
 
-    // ReOpt's internal_efficient_pct = SAM's (batt_dc_ac_efficiency + batt_ac_dc_efficiency)/2
-    map_input(vt, "battery_per_kW", &reopt_batt, "installed_cost_us_dollars_per_kw");
-    map_input(vt, "battery_per_kWh", &reopt_batt, "installed_cost_us_dollars_per_kwh");
+    vd = vt->lookup("battery_per_kW");
+    if (vd)
+        reopt_batt.assign("installed_cost_us_dollars_per_kw", vd->num[0]);
+    vd = vt->lookup("battery_per_kWh");
+    if (vd)
+        reopt_batt.assign("installed_cost_us_dollars_per_kwh", vd->num[0]);
 
-    try{
-        map_input(vt, "batt_dc_ac_efficiency", &reopt_batt, "internal_efficiency_pct");
-        map_input(vt, "batt_ac_dc_efficiency", &reopt_batt, "internal_efficiency_pct", true);
-        reopt_batt.lookup("internal_efficiency_pct")->num[0] /= 200.;
+    vd = vt->lookup("batt_dc_ac_efficiency");
+    vd2 = vt->lookup("batt_ac_dc_efficiency");
+    if (vd && vd2){
+        // ReOpt's internal_efficient_pct = SAM's (batt_dc_ac_efficiency + batt_ac_dc_efficiency)/2
+        reopt_batt.assign("internal_efficiency_pct", (vd->num[0] + vd2->num[0])/200.);
     }
-    catch(std::runtime_error&) {
-        reopt_batt.assign("internal_efficiency_pct", 0.96);
+    else if (vd && !vd2){
+        reopt_batt.assign("internal_efficiency_pct", vd->num[0]/100.);
     }
-    try{
-        map_input(vt, "batt_initial_SOC", &reopt_batt, "soc_init_pct", false, true);
-        map_input(vt, "batt_minimum_SOC", &reopt_batt, "soc_min_pct", false, true);
+    else if (!vd && vd2){
+        reopt_batt.assign("internal_efficiency_pct", vd2->num[0]/100.);
     }
-    catch(std::runtime_error&) {
+
+    vd = vt->lookup("batt_initial_SOC");
+    vd2 = vt->lookup("batt_minimum_SOC");
+    if (vd && vd2){
+        reopt_batt.assign("soc_init_pct", vd->num[0]/100.);
+        reopt_batt.assign("soc_min_pct", vd2->num[0]/100.);
+    }
+    else {
         reopt_batt.assign("soc_init_pct", 0.5);
         reopt_batt.assign("soc_min_pct", 0.15);
     }
@@ -263,19 +296,31 @@ SSCEXPORT void Reopt_size_battery_params(ssc_data_t data) {
     // convert financial inputs and set variables not modeled by SAM to 0
     //
     map_input(vt, "analysis_period", &reopt_fin, "analysis_years");
-    map_input(vt, "federal_tax_rate", &reopt_fin, "offtaker_tax_pct", false, true);
-    map_input(vt, "state_tax_rate", &reopt_fin, "offtaker_tax_pct", true, true);
     map_input(vt, "rate_escalation", &reopt_fin, "escalation_pct");
     map_input(vt, "value_of_lost_load", &reopt_fin, "value_of_lost_load_us_dollars_per_kwh");
     reopt_fin.assign("microgrid_upgrade_cost_pct", 0);
+
+    vd = vt->lookup("federal_tax_rate");
+    vd2 = vt->lookup("state_tax_rate");
+    if (vd && vd2){
+        reopt_fin.assign("offtaker_tax_pct", vd->num[0]/100. + vd2->num[0]/100.);
+    }
 
     vt_get_double(vt, "inflation_rate", &val1);
     vt_get_double(vt, "real_discount_rate", &val2);
     reopt_fin.assign("offtaker_discount_pct", (1 + val1/100.)*(1 + val2/100.) - 1);
 
-    vt_get_double(vt, "om_fixed_escal", &val1);
-    vt_get_double(vt, "om_production_escal", &val2);
-    reopt_fin.assign("om_cost_escalation_pct", (val1 / system_cap) + val2/100.);
+    vd = vt->lookup("om_fixed_escal");
+    vd2 = vt->lookup("om_production_escal");
+    if (vd && !vd2){
+        reopt_pv.assign("om_cost_escalation_pct", vd->num[0] / system_cap);
+    }
+    else if (!vd && vd2) {
+        reopt_pv.assign("om_cost_escalation_pct", vd2->num[0]);
+    }
+    else if (vd && vd2){
+        reopt_pv.assign("om_cost_escalation_pct", (vd->num[0] / system_cap) + vd2->num[0]);
+    }
 
     // convert load profile inputs, which are not net loads
     VT_GET_ARRAY_VEC(vt, "load_user_data", vec);
