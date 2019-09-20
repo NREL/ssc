bc47c58be29a5fa197bbe88df9113e39f7605364
nickdiorio <nadiorio@gmail.com>
2019-09-20 09:40:45 -0600

    Fix build error

--- test/input_cases/battery_common_data.h
+++ test/input_cases/battery_common_data.h
@@ -89,8 +89,6 @@ namespace {
 		ssc_data_set_array(data, "batt_replacement_schedule_percent", p_batt_replacement_schedule_percent, 10);
 		ssc_number_t p_replacement_cost[1] = { 68 };
 		ssc_data_set_array(data, "om_replacement_cost1", p_replacement_cost, 1);
-		ssc_number_t p_batt_replacement_schedule_percent[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 100 };
-		ssc_data_set_array(data, "batt_replacement_schedule_percent", p_batt_replacement_schedule_percent, 10);
 		ssc_data_set_number(data, "batt_mass", 3102.717041015625);
 		ssc_data_set_number(data, "batt_length", 1.0689570903778076);
 		ssc_data_set_number(data, "batt_width", 1.0689570903778076);
