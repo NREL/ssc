NREL/ssc

commit 896baf37f5182b59b1b17f0db1fa08269b73b4f7

reconfigured the DC degradation input for pvwattsv5 and pvwattsv7 to actually represent dc degradation in lifetime mode. in the PVWatts UI, the degradation is still applied on the AC side in the financial model, unless we choose to enable lifetime mode for PVWatts

ssc/cmod_pvwattsv5.cpp
@@ -39,7 +39,7 @@ static var_info _cm_vtab_pvwattsv5_part1[] = {
         /*   VARTYPE           DATATYPE          NAME                         LABEL                                               UNITS        META                      GROUP          REQUIRED_IF                 CONSTRAINTS                      UI_HINTS*/
         { SSC_INOUT,        SSC_NUMBER,      "system_use_lifetime_output",     "Run lifetime simulation",                    "0/1",        "",                       "Lifetime",            "?=0",                        "",                              "" },
         { SSC_INPUT,        SSC_NUMBER,      "analysis_period",                "Analysis period",                            "years",      "",                       "Lifetime",            "system_use_lifetime_output=1", "",                          "" },
-        { SSC_INPUT,        SSC_ARRAY,       "dc_degradation",                 "Annual AC degradation",                      "%/year",    "",                        "Lifetime",            "system_use_lifetime_output=1", "",                          "" },
+        { SSC_INPUT,        SSC_ARRAY,       "dc_degradation",                 "Annual DC degradation for lifetime simulations","%/year",  "",                       "Lifetime",            "system_use_lifetime_output=1", "",                          "" },
         { SSC_INPUT,        SSC_STRING,      "solar_resource_file",            "Weather file path",                           "",          "",                       "Location and Resource",     "?",                        "",                              "" },
         { SSC_INPUT,        SSC_TABLE,       "solar_resource_data",            "Weather data",                                "",          "dn,df,tdry,wspd,lat,lon,tz", "Location and Resource", "?",                        "",                              "" },
 
@@ -240,7 +240,7 @@ public:
 		return code;
 	}
 
-	void powerout(double time, double &shad_beam, double shad_diff, double dni, double dhi, double alb, double wspd, double tdry)
+	void powerout(double time, double degradationFactor, double &shad_beam, double shad_diff, double dni, double dhi, double alb, double wspd, double tdry)
 	{
 
 		if (sunup > 0)
@@ -308,6 +308,9 @@ public:
 
 			// dc losses
 			dc = dc * (1 - loss_percent / 100);
+
+			// dc degradation
+			dc *= degradationFactor; //this will be 1.0 in year 1 or if degradation isn't specified
 
 			// inverter efficiency
 			double etanom = inv_eff_percent / 100.0;
@@ -427,18 +430,18 @@ public:
 		std::vector<double> degradationFactor;
 		if (as_boolean("system_use_lifetime_output")) {
 			nyears = as_unsigned_long("analysis_period");
-			std::vector<double> ac_degradation = as_vector_double("dc_degradation");
-			if (ac_degradation.size() != nyears)
+			std::vector<double> dc_degradation = as_vector_double("dc_degradation");
+			if (dc_degradation.size() != nyears)
 				throw exec_error("pvwattsv5", "length of degradation array must be equal to analysis period");
-			if (ac_degradation.size() == 1) {
+			if (dc_degradation.size() == 1) {
 				degradationFactor.push_back(1.0);
 				for (size_t y = 1; y < nyears; y++) {
-					degradationFactor.push_back(pow((1.0 - ac_degradation[0] / 100.0), y));
+					degradationFactor.push_back(pow((1.0 - dc_degradation[0] / 100.0), y));
 				}
 			}
 			else {
 				for (size_t y = 0; y < nyears; y++) {
-					degradationFactor.push_back(1.0 - ac_degradation[y] / 100.0);
+					degradationFactor.push_back(1.0 - dc_degradation[y] / 100.0);
 				}
 			}
 		}
@@ -535,7 +538,7 @@ public:
 
 					if (sunup > 0)
 					{
-						powerout((double)idx, shad_beam, shad.fdiff(), wf.dn, wf.df, alb, wf.wspd, wf.tdry);
+						powerout((double)idx, degradationFactor[y], shad_beam, shad.fdiff(), wf.dn, wf.df, alb, wf.wspd, wf.tdry);
 						p_shad_beam[idx] = (ssc_number_t)shad_beam; // might be updated by 1 axis self shading so report updated value
 
 						p_poa[idx] = (ssc_number_t)poa; // W/m2
@@ -544,7 +547,7 @@ public:
 						p_dc[idx] = (ssc_number_t)dc; // power, Watts
 						p_ac[idx] = (ssc_number_t)ac; // power, Watts
 
-						p_gen[idx_life] = (ssc_number_t)(ac * haf(hour) * util::watt_to_kilowatt * degradationFactor[y]);
+						p_gen[idx_life] = (ssc_number_t)(ac * haf(hour) * util::watt_to_kilowatt);
 
 						if (y == 0) {
 							annual_kwh += p_gen[idx] / step_per_hour;
@@ -667,7 +670,7 @@ public:
 		double last_poa = as_double("poa");
 
 		double shad_beam = 1.0;
-		powerout(0, shad_beam, 1.0, beam, diff, alb, wspd, tamb);
+		powerout(0, 1.0, shad_beam, 1.0, beam, diff, alb, wspd, tamb);
 
 		int code = process_irradiance(year, month, day, hour, minute,
 			IRRADPROC_NO_INTERPOLATE_SUNRISE_SUNSET,
@@ -676,7 +679,7 @@ public:
 		if (code != 0)
 			throw exec_error("pvwattsv5_1ts", "failed to calculate plane of array irradiance with given input parameters");
 
-		powerout(0, shad_beam, 1.0, beam, diff, alb, wspd, tamb);
+		powerout(0, 1.0, shad_beam, 1.0, beam, diff, alb, wspd, tamb);
 
 		assign("poa", var_data((ssc_number_t)poa));
 		assign("tcell", var_data((ssc_number_t)pvt));

ssc/cmod_pvwattsv7.cpp
@@ -113,7 +113,7 @@ static var_info _cm_vtab_pvwattsv7[] = {
 	
 		{ SSC_INOUT,        SSC_NUMBER,      "system_use_lifetime_output",     "Run lifetime simulation",                    "0/1",        "",                                             "Lifetime",            "?=0",                        "",                              "" },
 		{ SSC_INPUT,        SSC_NUMBER,      "analysis_period",                "Analysis period",                            "years",      "",                                             "Lifetime",            "system_use_lifetime_output=1", "",                          "" },
-		{ SSC_INPUT,        SSC_ARRAY,       "dc_degradation",                 "Annual AC degradation",                      "%/year",     "",                                             "Lifetime",            "system_use_lifetime_output=1", "",                          "" },
+		{ SSC_INPUT,        SSC_ARRAY,       "dc_degradation",                 "Annual DC degradation for lifetime simulations","%/year",  "",                                             "Lifetime",            "system_use_lifetime_output=1", "",                          "" },
 
 		{ SSC_INPUT,        SSC_NUMBER,      "system_capacity",                "System size (DC nameplate)",                  "kW",        "",											   "System Design",      "*",                       "",                      "" },
 		{ SSC_INPUT,        SSC_NUMBER,      "module_type",                    "Module type",                                 "0/1/2",     "Standard,Premium,Thin film",                   "System Design",      "?=0",                     "MIN=0,MAX=2,INTEGER",           "" },
@@ -617,18 +617,18 @@ public:
 		std::vector<double> degradationFactor;
 		if (as_boolean("system_use_lifetime_output")) {
 			nyears = as_unsigned_long("analysis_period");
-			std::vector<double> ac_degradation = as_vector_double("dc_degradation");
-			if (ac_degradation.size() != nyears)
+			std::vector<double> dc_degradation = as_vector_double("dc_degradation");
+			if (dc_degradation.size() != nyears)
 				throw exec_error("pvwattsv5", "length of degradation array must be equal to analysis period");
-			if (ac_degradation.size() == 1) {
+			if (dc_degradation.size() == 1) {
 				degradationFactor.push_back(1.0);
 				for (size_t y = 1; y < nyears; y++) {
-					degradationFactor.push_back(pow((1.0 - ac_degradation[0] / 100.0), y));
+					degradationFactor.push_back(pow((1.0 - dc_degradation[0] / 100.0), y));
 				}
 			}
 			else {
 				for (size_t y = 0; y < nyears; y++) {
-					degradationFactor.push_back(1.0 - ac_degradation[y] / 100.0);
+					degradationFactor.push_back(1.0 - dc_degradation[y] / 100.0);
 				}
 			}
 		}
@@ -1130,6 +1130,9 @@ public:
 						// apply common DC losses here (independent of module model)
 						dc *= f_nonlinear * f_snow * f_losses;
 
+						// apply DC degradation
+						dc *= degradationFactor[y];
+
 						// inverter efficiency
 						double etanom = pv.inv_eff_percent*0.01;
 						double etaref = 0.9637;
@@ -1187,7 +1190,7 @@ public:
 					p_ac[idx] = (ssc_number_t)ac; // power, Watts
 
 					// accumulate hourly energy (kWh) (was initialized to zero when allocated)
-					p_gen[idx_life] = (ssc_number_t)(ac * haf(hour) * util::watt_to_kilowatt * degradationFactor[y]);
+					p_gen[idx_life] = (ssc_number_t)(ac * haf(hour) * util::watt_to_kilowatt);
 
 					if (y == 0) { //report first year annual energy
 						annual_kwh += p_gen[idx] / step_per_hour;

commit 777e473616510500ae348cea42310891253a5703

fixed bug in pvwattsv5 and v7 that I introduced in 896baf3 where lifetime mode wouldn't work if degradation array length was equal to 1, and added tests for pvwattsv7 lifetime mode

ssc/cmod_pvwattsv5.cpp
@@ -431,8 +431,6 @@ public:
 		if (as_boolean("system_use_lifetime_output")) {
 			nyears = as_unsigned_long("analysis_period");
 			std::vector<double> dc_degradation = as_vector_double("dc_degradation");
-			if (dc_degradation.size() != nyears)
-				throw exec_error("pvwattsv5", "length of degradation array must be equal to analysis period");
 			if (dc_degradation.size() == 1) {
 				degradationFactor.push_back(1.0);
 				for (size_t y = 1; y < nyears; y++) {
@@ -440,6 +438,8 @@ public:
 				}
 			}
 			else {
+				if (dc_degradation.size() != nyears)
+					throw exec_error("pvwattsv5", "length of degradation array must be equal to analysis period");
 				for (size_t y = 0; y < nyears; y++) {
 					degradationFactor.push_back(1.0 - dc_degradation[y] / 100.0);
 				}

ssc/cmod_pvwattsv7.cpp
@@ -618,8 +618,6 @@ public:
 		if (as_boolean("system_use_lifetime_output")) {
 			nyears = as_unsigned_long("analysis_period");
 			std::vector<double> dc_degradation = as_vector_double("dc_degradation");
-			if (dc_degradation.size() != nyears)
-				throw exec_error("pvwattsv5", "length of degradation array must be equal to analysis period");
 			if (dc_degradation.size() == 1) {
 				degradationFactor.push_back(1.0);
 				for (size_t y = 1; y < nyears; y++) {
@@ -627,6 +625,8 @@ public:
 				}
 			}
 			else {
+				if (dc_degradation.size() != nyears)
+					throw exec_error("pvwattsv5", "length of degradation array must be equal to analysis period");
 				for (size_t y = 0; y < nyears; y++) {
 					degradationFactor.push_back(1.0 - dc_degradation[y] / 100.0);
 				}

test/ssc_test/cmod_pvwattsv7_test.cpp
@@ -143,3 +143,51 @@ TEST_F(CMPvwattsV7Integration, SubhourlyWeather) {
 
 	}
 }
+
+/// Test PVWattsV7 in lifetime mode
+TEST_F(CMPvwattsV7Integration, LifetimeModeTest) {
+
+	// set lifetime mode
+	std::map<std::string, double> pairs;
+	pairs["system_use_lifetime_output"] = 1;
+	pairs["analysis_period"] = 25;
+
+	// test degradation array with a length of 1, which should work
+	// annual energy of this test should be higher than the array length 25, because this is year 1 energy
+	// and with a single value array, degradation doesn't start applying until year 2
+	double dc_degradation_single[1];
+	dc_degradation_single[0] = 0.5;
+	ssc_data_set_array(data, "dc_degradation", (ssc_number_t*)dc_degradation_single, 1);
+	int pvwatts_errors = modify_ssc_data_and_run_module(data, "pvwattsv7", pairs);
+	EXPECT_FALSE(pvwatts_errors);
+	if (!pvwatts_errors)
+	{
+		ssc_number_t annual_energy;
+		ssc_data_get_number(data, "annual_energy", &annual_energy);
+		EXPECT_NEAR(annual_energy, 6750.424, error_tolerance) << "Annual energy degradation array length 1.";
+	}
+
+	// next, test degradation array with length the same as analysis period, which should also work
+	double dc_degradation[25];
+	for (size_t i = 0; i < 25; i++) {
+		dc_degradation[i] = 0.5;
+	}
+	ssc_data_set_array(data, "dc_degradation", (ssc_number_t*)dc_degradation, 25);
+	pvwatts_errors = modify_ssc_data_and_run_module(data, "pvwattsv7", pairs);
+	EXPECT_FALSE(pvwatts_errors);
+	if (!pvwatts_errors)
+	{
+		ssc_number_t annual_energy;
+		ssc_data_get_number(data, "annual_energy", &annual_energy);
+		EXPECT_NEAR(annual_energy, 6716.589, error_tolerance) << "Annual energy degradation array length 25.";
+	}
+
+	// lastly, test degradation array with the wrong length, which should fail
+	double dc_degradation_fail[2];
+	for (size_t i = 0; i < 22; i++) {
+		dc_degradation_fail[i] = 0.5;
+	}
+	ssc_data_set_array(data, "dc_degradation", (ssc_number_t*)dc_degradation_fail, 2);
+	pvwatts_errors = modify_ssc_data_and_run_module(data, "pvwattsv7", pairs);
+	EXPECT_TRUE(pvwatts_errors);
+}