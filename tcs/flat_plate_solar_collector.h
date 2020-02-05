6c110a02b4190d8566a222d12d69d96679a4426d
Matthew Boyd <mr.matthew.boyd@gmail.com>
2020-02-05 16:41:35 -0700

    Simplify flat plate collector call interface

--- tcs/flat_plate_solar_collector.h
+++ tcs/flat_plate_solar_collector.h
@@ -166,8 +166,8 @@ public:
     const double area_total();                             // [m2]
     void resize_array(ArrayDimensions array_dimensions);
     void resize_array(double m_dot_array_design /*kg/s*/, double specific_heat /*kJ/kg-K*/, double temp_rise_array_design /*K*/);
-    const double UsefulPowerGain(const TimeAndPosition &time_and_position, const ExternalConditions &external_conditions);      // [W]
-    const double T_out(const TimeAndPosition &time_and_position, const ExternalConditions &external_conditions);                // [C]
+    const double UsefulPowerGain(const tm &timestamp, const ExternalConditions &external_conditions);      // [W]
+    const double T_out(const tm &timestamp, const ExternalConditions &external_conditions);                // [C]
 private:
     FlatPlateCollector flat_plate_collector_;       // just scale a single collector for now -> premature optimization??
     
