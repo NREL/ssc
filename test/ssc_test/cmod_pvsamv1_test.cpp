NREL/ssc

commit 5c2a13000d8621964faa6c9465e1ceef1aeb6c7b

Fixes issue #277, bifaciality coefficient

ssc/cmod_pvsamv1.cpp
@@ -966,6 +966,7 @@ void cm_pvsamv1::exec( ) throw (compute_module::general_error)
 	weather_header hdr = Irradiance->weatherHeader;
 	weather_data_provider * wdprov = Irradiance->weatherDataProvider.get();
 	int radmode = Irradiance->radiationMode;
+	double bifaciality = 0.0;
 
 	// Get System or Subarray Inputs
 	double aspect_ratio = Subarrays[0]->moduleAspectRatio;
@@ -1509,11 +1510,12 @@ void cm_pvsamv1::exec( ) throw (compute_module::general_error)
 					// Calculate rear-side irradiance for bifacial modules
 					if (Subarrays[0]->Module->isBifacial)
 					{
+						bifaciality = Subarrays[0]->Module->bifaciality;
 						double slopeLength = Subarrays[nn]->selfShadingInputs.length * Subarrays[nn]->selfShadingInputs.nmody;
 						if (Subarrays[nn]->selfShadingInputs.mod_orient == 1) {
 							slopeLength = Subarrays[nn]->selfShadingInputs.width * Subarrays[nn]->selfShadingInputs.nmody;
 						}
-						irr.calc_rear_side(Subarrays[0]->Module->bifacialTransmissionFactor, Subarrays[0]->Module->bifaciality, Subarrays[0]->Module->groundClearanceHeight, slopeLength);
+						irr.calc_rear_side(Subarrays[0]->Module->bifacialTransmissionFactor, Subarrays[0]->Module->groundClearanceHeight, slopeLength);
 						ipoa_rear[nn] = irr.get_poa_rear();
 						ipoa_rear_after_losses[nn] = ipoa_rear[nn] * (1 - Subarrays[nn]->rearIrradianceLossPercent);
 					}
@@ -1546,7 +1548,7 @@ void cm_pvsamv1::exec( ) throw (compute_module::general_error)
 					Subarrays[nn]->poa.poaDiffuseFront = iskydiff;
 					Subarrays[nn]->poa.poaGroundFront = ignddiff;
 					Subarrays[nn]->poa.poaRear = ipoa_rear_after_losses[nn];
-					Subarrays[nn]->poa.poaTotal = (radmode == irrad::POA_R) ? ipoa[nn] :(ipoa_front[nn] + ipoa_rear_after_losses[nn]);
+					Subarrays[nn]->poa.poaTotal = (radmode == irrad::POA_R) ? ipoa[nn] :(ipoa_front[nn] + ipoa_rear_after_losses[nn] * bifaciality);
 					Subarrays[nn]->poa.angleOfIncidenceDegrees = aoi;
 					Subarrays[nn]->poa.sunUp = sunup;
 					Subarrays[nn]->poa.surfaceTiltDegrees = stilt;
@@ -1749,10 +1751,10 @@ void cm_pvsamv1::exec( ) throw (compute_module::general_error)
 						{
 							ipoa_front[nn] *= out[nn].AOIModifier;
 							PVSystem->p_poaFront[nn][idx] = (radmode == irrad::POA_R) ? (ssc_number_t)ipoa[nn] : (ssc_number_t)(ipoa_front[nn]);
-							PVSystem->p_poaTotal[nn][idx] = (radmode == irrad::POA_R) ? (ssc_number_t)ipoa[nn] : (ssc_number_t)(ipoa_front[nn] + ipoa_rear[nn]);
+							PVSystem->p_poaTotal[nn][idx] = (radmode == irrad::POA_R) ? (ssc_number_t)ipoa[nn] : (ssc_number_t)(ipoa_front[nn] + ipoa_rear_after_losses[nn] * bifaciality);
 
 							ts_accum_poa_front_total += ipoa_front[nn] * ref_area_m2 * Subarrays[nn]->nModulesPerString * Subarrays[nn]->nStrings;
-							ts_accum_poa_total_eff += ((radmode == irrad::POA_R) ? ipoa[nn] : (ipoa_front[nn] + ipoa_rear_after_losses[nn])) * ref_area_m2 * Subarrays[nn]->nModulesPerString * Subarrays[nn]->nStrings;
+							ts_accum_poa_total_eff += ((radmode == irrad::POA_R) ? ipoa[nn] : (ipoa_front[nn] + ipoa_rear_after_losses[nn] * bifaciality)) * ref_area_m2 * Subarrays[nn]->nModulesPerString * Subarrays[nn]->nStrings;
 
 							//assign final string voltage output
 							PVSystem->p_dcStringVoltage[nn][idx] = (ssc_number_t)Subarrays[nn]->Module->dcVoltage * Subarrays[nn]->nModulesPerString;

shared/lib_irradproc.cpp
@@ -1250,7 +1250,7 @@ int irrad::calc()
 
 }
 
-int irrad::calc_rear_side(double transmissionFactor, double bifaciality, double groundClearanceHeight, double slopeLength)
+int irrad::calc_rear_side(double transmissionFactor, double groundClearanceHeight, double slopeLength)
 {
 	// do irradiance calculations if sun is up
 	if (timeStepSunPosition[2] > 0)
@@ -1293,7 +1293,7 @@ int irrad::calc_rear_side(double transmissionFactor, double bifaciality, double 
 		std::vector<double> rearIrradiancePerCellrow;
 		double rearAverageIrradiance = 0;
 		getBackSurfaceIrradiances(pvBackShadeFraction, rowToRow, verticalHeight, clearanceGround, distanceBetweenRows, horizontalLength, rearGroundGHI, frontGroundGHI, frontReflected, rearIrradiancePerCellrow, rearAverageIrradiance);
-		planeOfArrayIrradianceRearAverage = rearAverageIrradiance * bifaciality;
+		planeOfArrayIrradianceRearAverage = rearAverageIrradiance;
 	}
 	return true;
 }

shared/lib_irradproc.h
@@ -388,7 +388,7 @@ public:
 	int calc();
 
 	/// Run the irradiance processor for the rear-side of the surface to calculate rear-side plane-of-array irradiance
-	int calc_rear_side(double transmissionFactor, double bifaciality, double groundClearanceHeight, double slopeLength);
+	int calc_rear_side(double transmissionFactor, double groundClearanceHeight, double slopeLength);
 	
 	/// Return the calculated sun angles, some of which are converted to degrees
 	void get_sun( double *solazi,

commit ab65852b9464cd94f6c09caf67d9ec7a7717d50b

updated bifacial model so that bifaciality is applied to rear irradiance in module model. bug was introduced in 5c2a130 and is present in 2020 Beta version but no other released versions. fixes #277 fixes #334

shared/lib_pvmodel.h
@@ -41,7 +41,7 @@ public:
 	double Ibeam; // beam irradiance, W/m2
 	double Idiff; // sky diffuse irradiance, W/m2
 	double Ignd; // ground reflected irradiance, W/m2
-	double Irear; // rear side irradiance, W/m2
+	double Irear; // rear side irradiance, W/m2 **needs to be the amount actually usable by panel, so must be multiplied by bifaciality
 	double poaIrr; // plane of array irradiance, W/m2
 	double Tdry; // dry bulb temp, C
 	double Tdew; // dew point temp, C

ssc/cmod_pvsamv1.cpp
@@ -1636,7 +1636,7 @@ void cm_pvsamv1::exec( ) throw (general_error)
 								double V = stringV / (double)Subarrays[nn]->nModulesPerString; //voltage of an individual module on a string on this subarray
 
 								//initalize pvinput and pvoutput structures for the model
-								pvinput_t in(Subarrays[nn]->poa.poaBeamFront, Subarrays[nn]->poa.poaDiffuseFront, Subarrays[nn]->poa.poaGroundFront, Subarrays[nn]->poa.poaRear, Subarrays[nn]->poa.poaTotal,
+								pvinput_t in(Subarrays[nn]->poa.poaBeamFront, Subarrays[nn]->poa.poaDiffuseFront, Subarrays[nn]->poa.poaGroundFront, Subarrays[nn]->poa.poaRear * bifaciality, Subarrays[nn]->poa.poaTotal,
 									wf.tdry, wf.tdew, wf.wspd, wf.wdir, wf.pres,
 									solzen, Subarrays[nn]->poa.angleOfIncidenceDegrees, hdr.elev,
 									Subarrays[nn]->poa.surfaceTiltDegrees, Subarrays[nn]->poa.surfaceAzimuthDegrees,
@@ -1677,7 +1677,7 @@ void cm_pvsamv1::exec( ) throw (general_error)
 					{
 						int nn = SubarraysOnMpptInput[nSubarray]; //get the index of the subarray we're checking here
 						//initalize pvinput and pvoutput structures for the model
-						pvinput_t in_temp(Subarrays[nn]->poa.poaBeamFront, Subarrays[nn]->poa.poaDiffuseFront, Subarrays[nn]->poa.poaGroundFront, Subarrays[nn]->poa.poaRear, Subarrays[nn]->poa.poaTotal,
+						pvinput_t in_temp(Subarrays[nn]->poa.poaBeamFront, Subarrays[nn]->poa.poaDiffuseFront, Subarrays[nn]->poa.poaGroundFront, Subarrays[nn]->poa.poaRear * bifaciality, Subarrays[nn]->poa.poaTotal,
 							wf.tdry, wf.tdew, wf.wspd, wf.wdir, wf.pres,
 							solzen, Subarrays[nn]->poa.angleOfIncidenceDegrees, hdr.elev,
 							Subarrays[nn]->poa.surfaceTiltDegrees, Subarrays[nn]->poa.surfaceAzimuthDegrees,

test/ssc_test/cmod_pvsamv1_test.cpp
@@ -637,3 +637,41 @@ TEST_F(CMPvsamv1PowerIntegration_cmod_pvsamv1, TiltEqualsLat_cmod_pvsamv1) {
 		EXPECT_NEAR(subarray1SurfaceTilt, 33.4, 0.1) << "Subarray 1 tilt should be equal to latitude.";
 	}
 }
+
+/// Integration test for bifacial model in SAM
+TEST_F(CMPvsamv1PowerIntegration_cmod_pvsamv1, bifacial_cmod_pvsamv1) {
+
+	std::map<std::string, double> pairs;
+
+	//update bifacial inputs
+	pairs["cec_is_bifacial"] = 1;
+	pairs["cec_bifacial_transmission_factor"] = 0.013;
+	pairs["cec_bifaciality"] = 0.65;
+	pairs["cec_bifacial_ground_clearance_height"] = 1;
+
+	//these are the inputs that need to be updated from pvsaamv1_common_data to make inverter inputs match version 2018.11.11 defaults
+	pairs["cec_adjust"] = 4.86;
+	pairs["cec_i_o_ref"] = 3.9880000000000000e-12;
+	pairs["mppt_low_inverter"] = 100;
+	pairs["inv_snl_c0"] = -3.0810000000000000e-06;
+	pairs["inv_snl_c1"] = -4.8000000000000000e-05;
+	pairs["inv_snl_c2"] = 0.000123;
+	pairs["inv_snl_c3"] = -0.00163;
+	pairs["inv_snl_paco"] = 3850;
+	pairs["inv_snl_pdco"] = 3964.0;
+	pairs["inv_snl_pnt"] = 1.15;
+	pairs["inv_snl_pso"] = 17.9;
+	pairs["inv_snl_vdco"] = 400.0;
+	pairs["inv_snl_vdcmax"] = 480.0;
+
+	//run the test
+	int pvsam_errors = modify_ssc_data_and_run_module(data, "pvsamv1", pairs);
+
+	EXPECT_FALSE(pvsam_errors);
+	if (!pvsam_errors)
+	{
+		ssc_number_t annualEnergy;
+		ssc_data_get_number(data, "annual_energy", &annualEnergy);
+		EXPECT_NEAR(annualEnergy, 9141, 1.0) << "Bifacial annual energy from SAM version 2018.11.11 using Phoenix TMY2";
+	}
+}