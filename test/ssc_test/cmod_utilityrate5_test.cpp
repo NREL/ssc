53e5b8f5b9e5876fdff847f0a136f4166c9872a7
dguittet <dguittet@nrel.gov>
2020-03-17 16:48:39 -0600

    fix urdb equation conversions to flatdemandmonths & flatdemandstructure

--- test/ssc_test/cmod_utilityrate5_test.cpp
+++ test/ssc_test/cmod_utilityrate5_test.cpp
@@ -30,8 +30,8 @@ TEST(URDBv7_cmod_utilityrate5_eqns, ElectricityRates_format_as_URDBv7){
                                         2, 1, 100, 8.0909996032714844,
                                         2, 2, 9.9999996802856925e+37, 4.6760001182556152 };
     ssc_data_set_matrix( data, "ur_dc_tou_mat", p_ur_dc_tou_mat, 4, 4 );
-    ssc_number_t p_ur_dc_flat_mat[48] ={ 0, 1, 9.9999996802856925e+37, 0, 1, 1, 9.9999996802856925e+37, 0, 2, 1, 9.9999996802856925e+37, 0, 3, 1, 9.9999996802856925e+37, 0, 4, 1, 9.9999996802856925e+37, 0, 5, 1, 9.9999996802856925e+37, 0, 6, 1, 9.9999996802856925e+37, 0, 7, 1, 9.9999996802856925e+37, 0, 8, 1, 9.9999996802856925e+37, 0, 9, 1, 9.9999996802856925e+37, 0, 10, 1, 9.9999996802856925e+37, 0, 11, 1, 9.9999996802856925e+37, 0 };
-    ssc_data_set_matrix( data, "ur_dc_flat_mat", p_ur_dc_flat_mat, 12, 4 );
+    ssc_number_t p_ur_dc_flat_mat[52] ={0, 1, 100, 4, 0, 2, 1e+38, 6.46, 1, 1, 1e+38, 6.46, 2, 1, 1e+38, 6.46, 3, 1, 1e+38, 6.46, 4, 1, 1e+38, 13.87, 5, 1, 1e+38, 13.87, 6, 1, 1e+38, 13.87, 7, 1, 1e+38, 13.87, 8, 1, 1e+38, 13.87, 9, 1, 1e+38, 13.87, 10, 1, 1e+38, 6.46, 11, 1, 1e+38, 6.46};
+    ssc_data_set_matrix( data, "ur_dc_flat_mat", p_ur_dc_flat_mat, 13, 4 );
 
     ElectricityRates_format_as_URDBv7(data);
 
@@ -59,13 +59,27 @@ TEST(URDBv7_cmod_utilityrate5_eqns, ElectricityRates_format_as_URDBv7){
         EXPECT_EQ(dc_we_sched[i], p_ur_dc_sched_weekend[i] - 1);
     }
 
-    auto dc_flat = urdb_data.lookup("flatdemandstructure")->arr_vector();
+    auto dc_flat = urdb_data.lookup("flatdemandmonths")->num;
+    std::vector<double> flat_demand_months = {0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1};
     for (size_t i = 0; i < 12; i++){
-        EXPECT_NEAR(dc_flat[i], p_ur_dc_flat_mat[i * 4 + 3], 1e-3);
+        EXPECT_NEAR(dc_flat[i], dc_flat[i], 1e-3);
     }
 
+    auto dc_flat_struct = urdb_data.lookup_match_case("flatdemandstructure")->mat;
+    auto period = dc_flat_struct[0];
+    EXPECT_NEAR(period[0].table.lookup("max")->num[0], 100, 1e-3);
+    EXPECT_NEAR(period[0].table.lookup("rate")->num, 4, 1e-3);
+    EXPECT_GT(period[1].table.lookup("max")->num[0], 9.99e+33);
+    EXPECT_NEAR(period[1].table.lookup("rate")->num, 6.46, 1e-3);
+    period = dc_flat_struct[1];
+    EXPECT_GT(period[0].table.lookup("max")->num[0], 9.99e+33);
+    EXPECT_NEAR(period[0].table.lookup("rate")->num, 6.46, 1e-3);
+    period = dc_flat_struct[2];
+    EXPECT_GT(period[0].table.lookup("max")->num[0], 9.99e+33);
+    EXPECT_NEAR(period[0].table.lookup("rate")->num, 13.87, 1e-3);
+
     auto ec_tou_mat = urdb_data.lookup("energyratestructure")->mat;
-    auto period = ec_tou_mat[0];
+    period = ec_tou_mat[0];
     EXPECT_NEAR(period[0].table.lookup("max")->num, 100, 1e-3);
     EXPECT_NEAR(period[0].table.lookup("rate")->num, 0.070, 1e-3);
     EXPECT_GT(period[1].table.lookup("max")->num[0], 9.99e+33);
