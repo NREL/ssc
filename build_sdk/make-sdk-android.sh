bfa5653155f0c6474fa1f0879a2ddc03fca079e1
dguittet <dguittet@nrel.gov>
2020-04-06 11:38:11 -0600

    remove build files

--- build_sdk/make-sdk-android.sh
+++
@@ -1,6 +0,0 @@
-mkdir sdk-release/android/armeabi
-mkdir sdk-release/android/armeabi-v7a
-mkdir sdk-release/android/x86
-cp -v -R build_android/armeabi/* sdk-release/android/armeabi
-cp -v -R build_android/armeabi-v7a/* sdk-release/android/armeabi-v7a
-cp -v -R build_android/x86/* sdk-release/android/x86
