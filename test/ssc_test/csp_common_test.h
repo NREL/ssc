415bbe62b1db1fbbf8a945ed90d8f0da23cac882
Matthew Boyd <mr.matthew.boyd@gmail.com>
2020-12-30 11:16:48 -0700

    Refactor tcsmolten_salt tests

---
+++ test/ssc_test/csp_common_test.h
@@ -0,0 +1,44 @@
+#ifndef _CSP_COMMON_TEST_H_
+#define _CSP_COMMON_TEST_H_
+
+#include <gtest/gtest.h>
+#include "../ssc/common.h"
+//#include "csp_financial_defaults.h"
+#include "../input_cases/code_generator_utilities.h"
+
+#define EXPECT_NEAR_FRAC(val1, val2, frac_error) EXPECT_NEAR(val1, val2, val2 * frac_error)
+
+const double kErrorToleranceLo = 0.001;    // 0.1%
+const double kErrorToleranceHi = 0.01;     // 1.0%
+
+class CmodUnderTest {
+public:
+    CmodUnderTest(std::string module_name, ssc_data_t defaults)
+        : module_name_{ module_name }, data_{ defaults } {}
+    ~CmodUnderTest() {
+        if (data_) {
+            ssc_data_free(data_);
+            data_ = nullptr;
+        }
+    }
+    int RunModule() {
+        int errors = run_module(this->data_, this->module_name_);
+        return errors;
+    }
+    void SetInput(std::string name, ssc_number_t value) {
+        ssc_data_set_number(this->data_, name.c_str(), value);
+    }
+    ssc_number_t GetOutput(std::string name) const {
+        ssc_number_t output;
+        ssc_data_get_number(this->data_, name.c_str(), &output);
+        return output;
+    }
+private:
+    const std::string module_name_;
+    ssc_data_t data_;
+};
+
+
+ssc_data_t singleowner_defaults();
+
+#endif
