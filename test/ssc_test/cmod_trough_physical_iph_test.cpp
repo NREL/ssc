87065371ccd3108fb5edd04d901f1140cae96410
tyneises <ty.neises@nrel.gov>
2021-04-12 17:59:43 -0500

    update physical trough iph test for bug fix

--- test/ssc_test/cmod_trough_physical_iph_test.cpp
+++ test/ssc_test/cmod_trough_physical_iph_test.cpp
@@ -15,10 +15,10 @@ NAMESPACE_TEST(csp_trough, HeatTroughCmod, Default_NoFinancial)
     EXPECT_FALSE(errors);
     if (!errors) {
         EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_gross_energy"), 24328026, kErrorToleranceLo);
-        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_energy"), 24327778, kErrorToleranceLo);
-        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_electricity_consumption"), 93310, kErrorToleranceLo);
-        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_thermal_consumption"), 247.67, kErrorToleranceLo);
-        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_tes_freeze_protection"), 247.67, kErrorToleranceLo);
+        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_energy"), 24267070, kErrorToleranceLo);
+        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_electricity_consumption"), 92979, kErrorToleranceLo);
+        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_thermal_consumption"), 215.00, kErrorToleranceLo);
+        EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_tes_freeze_protection"), 215.00, kErrorToleranceLo);
         EXPECT_NEAR(heat_trough.GetOutput("annual_field_freeze_protection"), 0., kErrorToleranceLo);
         EXPECT_NEAR_FRAC(heat_trough.GetOutput("annual_total_water_use"), 176.3, kErrorToleranceLo);
     }
