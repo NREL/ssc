a71ba704e75c65ed317d0ac4bb352faf1b0cb39b
Matthew Boyd <mr.matthew.boyd@gmail.com>
2021-04-15 10:06:25 -0600

    Fix csp_dtr_sca_calc_theta argument

--- ssc/cmod_csp_trough_eqns.cpp
+++ ssc/cmod_csp_trough_eqns.cpp
@@ -340,18 +340,8 @@ void Physical_Trough_Collector_Type_Equations(ssc_data_t data)
     csp_dtr_sca_ap_lengths = Csp_dtr_sca_ap_lengths(csp_dtr_sca_lengths, csp_dtr_sca_ncol_per_scas);
     ssc_data_t_set_matrix(data, "csp_dtr_sca_ap_lengths", csp_dtr_sca_ap_lengths);
 
-    // csp_dtr_sca_calc_theta
-    ssc_data_t_get_number(data, "lat", &lat);
-    csp_dtr_sca_calc_theta_1 = Csp_dtr_sca_calc_theta(lat);
-    ssc_data_t_set_number(data, "csp_dtr_sca_calc_theta_1", csp_dtr_sca_calc_theta_1);
-
-    // csp_dtr_sca_calc_end_gain
-    ssc_data_t_get_matrix(vt, "csp_dtr_sca_ave_focal_lens", csp_dtr_sca_ave_focal_lens);
-    ssc_data_t_get_matrix(vt, "csp_dtr_sca_piping_dists", csp_dtr_sca_piping_dists);
-    csp_dtr_sca_calc_end_gain_1 = Csp_dtr_sca_calc_end_gain(csp_dtr_sca_ave_focal_lens.at(0), csp_dtr_sca_calc_theta_1, csp_dtr_sca_piping_dists.at(0));
-    ssc_data_t_set_number(data, "csp_dtr_sca_calc_end_gain_1", csp_dtr_sca_calc_end_gain_1);
-
     // csp_dtr_sca_calc_zenith
+    ssc_data_t_get_number(data, "lat", &lat);
     csp_dtr_sca_calc_zenith_1 = Csp_dtr_sca_calc_zenith(lat);
     ssc_data_t_set_number(data, "csp_dtr_sca_calc_zenith_1", csp_dtr_sca_calc_zenith_1);
 
@@ -360,6 +350,16 @@ void Physical_Trough_Collector_Type_Equations(ssc_data_t data)
     ssc_data_t_get_number(data, "azimuth", &azimuth);
     csp_dtr_sca_calc_costh_1 = Csp_dtr_sca_calc_costh(csp_dtr_sca_calc_zenith_1, tilt, azimuth);
     ssc_data_t_set_number(data, "csp_dtr_sca_calc_costh_1", csp_dtr_sca_calc_costh_1);
+
+    // csp_dtr_sca_calc_theta
+    csp_dtr_sca_calc_theta_1 = Csp_dtr_sca_calc_theta(csp_dtr_sca_calc_costh_1);
+    ssc_data_t_set_number(data, "csp_dtr_sca_calc_theta_1", csp_dtr_sca_calc_theta_1);
+
+    // csp_dtr_sca_calc_end_gain
+    ssc_data_t_get_matrix(vt, "csp_dtr_sca_ave_focal_lens", csp_dtr_sca_ave_focal_lens);
+    ssc_data_t_get_matrix(vt, "csp_dtr_sca_piping_dists", csp_dtr_sca_piping_dists);
+    csp_dtr_sca_calc_end_gain_1 = Csp_dtr_sca_calc_end_gain(csp_dtr_sca_ave_focal_lens.at(0), csp_dtr_sca_calc_theta_1, csp_dtr_sca_piping_dists.at(0));
+    ssc_data_t_set_number(data, "csp_dtr_sca_calc_end_gain_1", csp_dtr_sca_calc_end_gain_1);
 
     // csp_dtr_sca_calc_end_loss
     ssc_data_t_get_number(data, "nSCA", &nSCA);
