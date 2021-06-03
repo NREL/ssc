d870e47aa949333c5d01ceba0609aec1205bc992
Prilliman <Matthew.Prilliman@nrel.gov>
2021-06-03 13:48:31 -0600

    Fixed direction of slashes on string for Linux build

--- test/ssc_test/cmod_mhk_wave_test.cpp
+++ test/ssc_test/cmod_mhk_wave_test.cpp
@@ -164,7 +164,7 @@ TEST_F(CM_MHKWave, ReadJson_TSFile) {
     ssc_data_unassign(data, "wave_resource_matrix");
     const char* SSCDIR = std::getenv("SSCDIR");
     char file[256];
-    sprintf(file, "%s\\test\\input_cases\\mhk\\time_series_edited.csv", SSCDIR);
+    sprintf(file, "%s/test/input_cases/mhk/time_series_edited.csv", SSCDIR);
     std::string js = "{\"use_specific_wf_wave\": 0.0, \"wave_resource_model_choice\": 1.0}";
     auto copy = json_to_ssc_data(js.c_str());
     ssc_data_set_string(copy, "wave_resource_filename_ts", file);
