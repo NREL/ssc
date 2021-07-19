9b9bae90a3a8e4c7191c9609cb3802cd89c3af39
Brian Mirletz <brian.mirletz@nrel.gov>
2021-07-19 11:09:02 -0600

    ensure interconnection and curtailment variables are properly provided to battwatts

--- ssc/cmod_battwatts.cpp
+++ ssc/cmod_battwatts.cpp
@@ -59,7 +59,7 @@ var_info_invalid  };
 
 std::shared_ptr<batt_variables>
 battwatts_create(size_t n_recs, size_t n_years, int chem, int meter_pos, double size_kwh, double size_kw, double inv_eff,
-                 int dispatch, std::vector<double> dispatch_custom){
+                 int dispatch, std::vector<double> dispatch_custom, double interconnection_limit, std::vector<double> curtailment_limit){
     auto batt_vars = std::make_shared<batt_variables>();
 
     // allocate vectors
@@ -215,6 +215,13 @@ battwatts_create(size_t n_recs, size_t n_years, int chem, int meter_pos, double 
     batt_vars->batt_minimum_SOC = 15.;
     batt_vars->batt_minimum_modetime = 10;
 
+    // Interconnection and curtailment
+    batt_vars->gridCurtailmentLifetime_MW = curtailment_limit;
+    batt_vars->grid_interconnection_limit_kW = interconnection_limit;
+    if (interconnection_limit < 1e+38) {
+        batt_vars->enable_interconnection_limit = true;
+    }
+
     // Storage dispatch controllers
     switch (dispatch){
         default:
@@ -272,7 +279,8 @@ cm_battwatts::cm_battwatts()
 std::shared_ptr<batt_variables> cm_battwatts::setup_variables(size_t n_recs)
 {
     size_t nyears = 1;
-    if (as_boolean("system_use_lifetime_output"))
+    bool system_use_lifetime_output = as_boolean("system_use_lifetime_output");
+    if (system_use_lifetime_output)
         nyears = (size_t)as_double("analysis_period");
     int chem = as_integer("batt_simple_chemistry");
     int pos = as_integer("batt_simple_meter_position");
@@ -286,7 +294,37 @@ std::shared_ptr<batt_variables> cm_battwatts::setup_variables(size_t n_recs)
         if (dispatch_custom.size()!=n_recs) throw exec_error("battwatts",
                 "'batt_custom_dispatch' length must be equal to length of 'ac'.");
     }
-    return battwatts_create(n_recs, nyears, chem, pos, kwh, kw, inv_eff, dispatch, dispatch_custom);
+    // Interconnection and curtailment
+    std::vector<double> scaleFactors(nyears, 1.0); // No scaling factors for curtailment
+
+    std::vector<double> curtailment_year_one;
+    if (is_assigned("grid_curtailment")) {
+        curtailment_year_one = as_vector_double("grid_curtailment");
+    }
+    std::vector<double> curtaliment_lifetime;
+    double interpolation_factor = 1.0;
+    double dt_hour = 8760.0 / (double)n_recs;
+    single_year_to_lifetime_interpolated<double>(
+        system_use_lifetime_output,
+        (size_t)nyears,
+        n_recs * nyears,
+        curtailment_year_one,
+        scaleFactors,
+        interpolation_factor,
+        curtaliment_lifetime,
+        n_recs,
+        dt_hour);
+
+    bool enable_interconnection_limit = false;
+    double interconnection_limit = 1e+38;
+    if (is_assigned("enable_interconnection_limit")) {
+        enable_interconnection_limit = as_boolean("enable_interconnection_limit");
+        if (enable_interconnection_limit && is_assigned("grid_interconnection_limit_kwac")) {
+            interconnection_limit = as_double("grid_interconnection_limit_kwac");
+        }
+    }
+
+    return battwatts_create(n_recs, nyears, chem, pos, kwh, kw, inv_eff, dispatch, dispatch_custom, interconnection_limit, curtaliment_lifetime);
 }
 
 
