9b4087feb4e6ea19ee1274616b9cb744a07c4655
Brian Mirletz <brian.mirletz@nrel.gov>
2021-08-27 10:37:08 -0600

    Connect grid outage ssc inputs to the powerflow code. Add length check for grid outage inputs.

--- ssc/cmod_battery.cpp
+++ ssc/cmod_battery.cpp
@@ -749,6 +749,8 @@ battstor::battstor(var_table& vt, bool setup_model, size_t nrec, double dt_hr, c
     analyze_outage = false;
     if (vt.is_assigned("grid_outage")) {
         batt_vars->grid_outage_steps = vt.as_vector_bool("grid_outage");
+        if (batt_vars->grid_outage_steps.size() != total_steps)
+            throw exec_error("battery", "Grid outage status needs to be specified for the length of the simulation: " + std::to_string(total_steps) + " steps");
         // If not all false, we need the outage vars
         analyze_outage = !std::all_of(batt_vars->grid_outage_steps.begin(), batt_vars->grid_outage_steps.end(), [](bool x) {return x; });
     }
@@ -1638,6 +1640,10 @@ void battstor::advance(var_table*, double P_gen, double V_gen, double P_load, do
     }
     if (index < batt_vars->gridCurtailmentLifetime_MW.size()) {
         powerflow->powerCurtailmentLimit = batt_vars->gridCurtailmentLifetime_MW[index] * 1000.0;
+    }
+    if (index < batt_vars->grid_outage_steps.size()) {
+        // Set to false in reset() above, so don't need else here.
+        powerflow->isOutageStep = batt_vars->grid_outage_steps[index];
     }
 
     powerflow->powerGeneratedBySystem = P_gen;
