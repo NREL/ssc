4cd404c25060003ccefee1b99ce07007a3b40d47
janinefreeman <jfreeman@nrel.gov>
2021-09-16 12:00:10 -0600

    added pvwattsv8 tests

---
+++ test/ssc_test/cmod_pvwattsv8_test.h
@@ -0,0 +1,54 @@
+
+#ifndef _CMOD_PVWATTSV8_TEST_H_
+#define _CMOD_PVWATTSV8_TEST_H_
+
+#include <gtest/gtest.h>
+#include "../ssc/core.h"
+#include "../ssc/vartab.h"
+#include "../ssc/common.h"
+#include "../input_cases/pvwattsv7_cases.h"
+
+/**
+* CMPVWattsv8 tests cmod_pvwattsv8 using a solar resource file. SetUp() creates default case,
+* which can be modified within each individual test before running compute() and tests.
+*/
+class CMPvwattsv8Integration_cmod_pvwattsv8 : public ::testing::Test {
+protected: //doesn't really matter if this is protected or public, but you need to declare one or the other or it will default to private which doesn't work
+	ssc_data_t data;
+
+	double error_tolerance = 1.0e-3;
+
+	bool compute();
+	void SetUp() { //if you always want to set up with the same default case, this can go in the class. otherwise it probably makes sense in the test itself.
+		data = ssc_data_create();
+		int errors = pvwattsv7_nofinancial_testfile(data);
+		EXPECT_FALSE(errors); //make sure that the test ran ok
+	}
+	void TearDown()
+	{
+		ssc_data_free(data);
+		data = nullptr;
+	} 
+};
+
+//this function will be available to run the pvwattsv8 compute module from within tests
+bool CMPvwattsv8Integration_cmod_pvwattsv8::compute() {
+	ssc_module_t module = ssc_module_create("pvwattsv8");
+	if (NULL == module)
+	{
+		printf("error: could not create 'pvwattsv8' module.");
+		ssc_data_free(data);
+		return false;
+	}
+	if (ssc_module_exec(module, data) == 0)
+	{
+		printf("error during simulation.");
+		ssc_module_free(module);
+		ssc_data_free(data);
+		return false;
+	}
+	ssc_module_free(module);
+	return 0;
+}
+
+#endif // _CMOD_PVWATTSv8_TEST_H_
