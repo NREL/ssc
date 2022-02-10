402c057c9774d8b7c0dcd20a1e60a4a1c3b89f2c
Steven Janzou <sjanzou@gmail.com>
2022-02-10 01:07:12 -0700

    Fix faulty Github merge of patch into ssc_719

--- test/ssc_test/save_as_JSON_test.cpp
+++ test/ssc_test/save_as_JSON_test.cpp
@@ -189,12 +189,12 @@ TEST(save_as_JSON_test_run, pv_batt_mechant_plant_rapidjson) {
     ssc_number_t npv;
     ssc_data_get_number(data, "project_return_aftertax_npv", &npv);
     EXPECT_NEAR(npv, -81154037, fabs(-81154037) / 1e6);
-   
+
     ssc_module_free(mod_pv);
     ssc_module_free(mod_grid);
     ssc_module_free(mod_mp);
     ssc_data_free(data);
-
+}
    
 
 TEST(save_as_JSON_test_run, pt_mechant_plant_rapidjson) {
