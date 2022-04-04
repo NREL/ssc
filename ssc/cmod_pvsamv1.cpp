29de6e54e09828263e36355c68e2b19dd9c1ef46
Brian Mirletz <brian.mirletz@nrel.gov>
2022-04-04 15:43:15 -0600

    extend weather file variables for comparison over system lifetime

--- ssc/cmod_pvsamv1.cpp
+++ ssc/cmod_pvsamv1.cpp
@@ -1363,7 +1363,7 @@ void cm_pvsamv1::exec()
                         wf.year, wf.month, wf.day, wf.hour, wf.minute), SSC_NOTICE, (float)idx);
 
                 // p_irrad_calc is only weather file records long...
-                if (iyear == 0)
+                if (iyear == 0 || save_full_lifetime_variables == 1)
                 {
                     if (radmode == irrad::POA_R || radmode == irrad::POA_P) {
                         double gh_temp, df_temp, dn_temp;
@@ -1408,14 +1408,14 @@ void cm_pvsamv1::exec()
                 irr.get_poa(&ibeam, &iskydiff, &ignddiff, 0, 0, 0);
                 alb = irr.getAlbedo();
 
-                if (iyear == 0)
+                if (iyear == 0 || save_full_lifetime_variables == 1)
                     Irradiance->p_sunPositionTime[idx] = (ssc_number_t)irr.get_sunpos_calc_hour();
 
                 // save weather file beam, diffuse, and global for output and for use later in pvsamv1- year 1 only
                 /*jmf 2016: these calculations are currently redundant with calculations in irrad.calc() because ibeam and idiff in that function are DNI and DHI, **NOT** in the plane of array
                 we'll have to fix this redundancy in the pvsamv1 rewrite. it will require allowing irradproc to report the errors below
                 and deciding what to do if the weather file DOES contain the third component but it's not being used in the calculations.*/
-                if (iyear == 0)
+                if (iyear == 0 || save_full_lifetime_variables == 1)
                 {
                     // Apply all irradiance component data from weather file (if it exists)
                     Irradiance->p_weatherFilePOA[0][idx] = (ssc_number_t)wf.poa;
@@ -2119,7 +2119,7 @@ void cm_pvsamv1::exec()
             }
 
             // save other array-level environmental and irradiance outputs	- year 1 only outputs
-            if (iyear == 0)
+            if (iyear == 0 || save_full_lifetime_variables == 1)
             {
                 Irradiance->p_weatherFileWindSpeed[idx] = (ssc_number_t)wf.wspd;
                 Irradiance->p_weatherFileAmbientTemp[idx] = (ssc_number_t)wf.tdry;
