c20eb7ae43085e8a478c528f81cff3fd0f9b3ef7
tyneises <ty.neises@nrel.gov>
2022-04-27 13:15:30 -0500

    Standalone mspt rec compute module (#811)
    * remove stow deploy from rec classes
    
    * add tower height to cmod
    
    cmod now runs steady state rec init
    
    * move base class member data to protected
    
    define member data via constructor
    
    * move htf code to pt base init method
    
    convert units in base class constructor
    
    * move more code to base init method
    
    * move shared mspt rec parameters to base class
    
    * start converting transient model to derived of steady state
    
    * convert init call to base and derived classes
    
    * move some common methods to base class
    
    * move common methods to base 222 class
    
    * move more common methods to base 222 class
    
    * use common 222 converged method
    
    * move more common methods to base 222 class
    
    * clean up ON method
    
    move transient code to after common Call code
    
    * move steady state solution to separate method
    
    * clean up is_rec_off setting outputs
    
    * move transient mspt rec parameters to constructor
    
    * clean up stand alone cmod
    
    * move mspt performance inputs out of structure
    
    * clean up mspt reorg
    
    * move clear sky dni calc to heliostat field class
    
    * modify mspt call method to use specific inputs rather than structures
    
    * clean up old clear sky method
    
    * check clearsky dni in receiver model
    
    * clean up receiever weather inputs
    
    * add flux map check
    
    * continue building standalone mspt cmod
    
    * replace dni and clearsky inputs with ratio
    
    * add receiver outputs
    
    * clean up outputs structure in mspt inheritance
    
    * clean up stand-alone mspt rec cmod
    
    * clean up cmod

--- tcs/csp_solver_mspt_collector_receiver.cpp
+++ tcs/csp_solver_mspt_collector_receiver.cpp
@@ -77,8 +77,8 @@ void C_csp_mspt_collector_receiver::init(const C_csp_collector_receiver::S_csp_c
 	mc_pt_heliostatfield.init();
 	mc_pt_receiver.init();
 
-	solved_params.m_T_htf_cold_des = mc_pt_receiver.m_T_htf_cold_des;       //[K]
-	solved_params.m_q_dot_rec_des = mc_pt_receiver.m_q_rec_des / 1.E6;		//[MW]
+    solved_params.m_T_htf_cold_des = mc_pt_receiver.get_T_htf_cold_des();   // mc_pt_receiver.m_T_htf_cold_des;       //[K]
+    solved_params.m_q_dot_rec_des = mc_pt_receiver.get_q_dot_rec_des();     // mc_pt_receiver.m_q_rec_des / 1.E6;		//[MW]
 	solved_params.m_A_aper_total = mc_pt_heliostatfield.ms_params.m_A_sf;	//[m^2]
 
 	return;
@@ -106,12 +106,14 @@ double C_csp_mspt_collector_receiver::get_pumping_parasitic_coef()  //MWe/MWt
 
 double C_csp_mspt_collector_receiver::get_min_power_delivery()    //MWt
 {
-    return mc_pt_receiver.m_f_rec_min * mc_pt_receiver.m_q_rec_des*1.e-6;
+    //return mc_pt_receiver.m_f_rec_min * mc_pt_receiver.m_q_rec_des*1.e-6;
+    return mc_pt_receiver.get_min_power_delivery();     //[MWt]
 }
 
 double C_csp_mspt_collector_receiver::get_max_power_delivery(double T_htf_cold_in /*C*/)    //MWt
 {
-    return mc_pt_receiver.m_m_dot_htf_max_frac * mc_pt_receiver.m_q_rec_des * 1.e-6;
+    //return mc_pt_receiver.m_m_dot_htf_max_frac * mc_pt_receiver.m_q_rec_des * 1.e-6;
+    return mc_pt_receiver.get_max_power_delivery();     //[MWt]
 }
 
 double C_csp_mspt_collector_receiver::get_tracking_power()
@@ -123,7 +125,6 @@ double C_csp_mspt_collector_receiver::get_col_startup_power()
 {
 	return mc_pt_heliostatfield.ms_params.m_p_start * mc_pt_heliostatfield.ms_params.m_N_hel *1.e-3;	//MWe-hr
 }
-
 
 void C_csp_mspt_collector_receiver::call(const C_csp_weatherreader::S_outputs &weather,
 	const C_csp_solver_htf_1state &htf_state_in,
@@ -146,10 +147,11 @@ void C_csp_mspt_collector_receiver::call(const C_csp_weatherreader::S_outputs &w
     receiver_inputs.m_plant_defocus = mc_pt_heliostatfield.ms_outputs.m_plant_defocus_out;  //[-]
 	receiver_inputs.m_input_operation_mode = inputs.m_input_operation_mode;
 	receiver_inputs.m_flux_map_input = &mc_pt_heliostatfield.ms_outputs.m_flux_map_out;
+    receiver_inputs.m_clearsky_dni = mc_pt_heliostatfield.ms_outputs.m_clearsky_dni;        //[W/m2]
 	mc_pt_receiver.call(weather, htf_state_in, receiver_inputs, sim_info);
 		
 	// Set collector/receiver parent class outputs and return
-	cr_out_solver.m_q_thermal = mc_pt_receiver.ms_outputs.m_Q_thermal;				//[MW]
+	cr_out_solver.m_q_thermal = mc_pt_receiver.ms_outputs.m_Q_thermal;				//[MWt]
 	cr_out_solver.m_q_startup = mc_pt_receiver.ms_outputs.m_q_startup;				//[MWt-hr]
 	cr_out_solver.m_m_dot_salt_tot = mc_pt_receiver.ms_outputs.m_m_dot_salt_tot;		//[kg/hr]
 	cr_out_solver.m_T_salt_hot = mc_pt_receiver.ms_outputs.m_T_salt_hot;				//[C]
@@ -192,7 +194,7 @@ void C_csp_mspt_collector_receiver::call(const C_csp_weatherreader::S_outputs &w
 	mc_reported_outputs.value(E_T_RISER, mc_pt_receiver.ms_outputs.m_Triser);	//[C]
 	mc_reported_outputs.value(E_T_DOWNC, mc_pt_receiver.ms_outputs.m_Tdownc);	//[C]
 
-	mc_reported_outputs.value(E_CLEARSKY, mc_pt_receiver.ms_outputs.m_clearsky);
+	mc_reported_outputs.value(E_CLEARSKY, mc_pt_heliostatfield.ms_outputs.m_clearsky_dni);
 	mc_reported_outputs.value(E_Q_DOT_THERMAL_CSKY_SS, mc_pt_receiver.ms_outputs.m_Q_thermal_csky_ss); //[MWt]
 	mc_reported_outputs.value(E_Q_DOT_THERMAL_SS, mc_pt_receiver.ms_outputs.m_Q_thermal_ss); //[MWt]
 }
@@ -200,12 +202,11 @@ void C_csp_mspt_collector_receiver::call(const C_csp_weatherreader::S_outputs &w
 void C_csp_mspt_collector_receiver::off(const C_csp_weatherreader::S_outputs &weather,
 	const C_csp_solver_htf_1state &htf_state_in,
 	C_csp_collector_receiver::S_csp_cr_out_solver &cr_out_solver,
-	//C_csp_collector_receiver::S_csp_cr_out_report &cr_out_report,
 	const C_csp_solver_sim_info &sim_info)
 {
 	// First call heliostat field class
 	// In OFF call, looking specifically for weather STOW parasitics apply
-	mc_pt_heliostatfield.off(sim_info);
+	mc_pt_heliostatfield.off(weather, sim_info);
 
 	// Now, call the tower-receiver model
 	mc_pt_receiver.off(weather, htf_state_in, sim_info);
@@ -253,7 +254,7 @@ void C_csp_mspt_collector_receiver::off(const C_csp_weatherreader::S_outputs &we
 	mc_reported_outputs.value(E_T_RISER, mc_pt_receiver.ms_outputs.m_Triser);	//[C]
 	mc_reported_outputs.value(E_T_DOWNC, mc_pt_receiver.ms_outputs.m_Tdownc);	//[C]
 
-	mc_reported_outputs.value(E_CLEARSKY, mc_pt_receiver.ms_outputs.m_clearsky);
+	mc_reported_outputs.value(E_CLEARSKY, mc_pt_heliostatfield.ms_outputs.m_clearsky_dni);
 	mc_reported_outputs.value(E_Q_DOT_THERMAL_CSKY_SS, mc_pt_receiver.ms_outputs.m_Q_thermal_csky_ss); //[MWt]
 	mc_reported_outputs.value(E_Q_DOT_THERMAL_SS, mc_pt_receiver.ms_outputs.m_Q_thermal_ss); //[MWt]
 
@@ -350,26 +351,27 @@ double C_csp_mspt_collector_receiver::calculate_thermal_efficiency_approx( const
     A very approximate thermal efficiency used for quick optimization performance projections
     */
 
-    double T_eff = (mc_pt_receiver.m_T_htf_cold_des + mc_pt_receiver.m_T_htf_hot_des)*.55;
+    //double T_eff = (mc_pt_receiver.m_T_htf_cold_des + mc_pt_receiver.m_T_htf_hot_des)*.55;
+    //
+    //double T_amb = weather.m_tdry + 273.15;
+    //double T_eff4 = T_eff * T_eff;
+    //T_eff4 *= T_eff4;
+    //double T_amb4 = T_amb * T_amb;
+    //T_amb4 *= T_amb4;
+    //
+    //double Arec = mc_pt_receiver.area_proj();
+    //
+    //double q_rad = 5.67e-8*mc_pt_receiver.m_epsilon * Arec * (T_eff4 - T_amb4) * 1.e-6;   //MWt
+    //
+    //double v = weather.m_wspd;
+    //double v2 = v*v;
+    //double v3 = v2*v;
+    //
+    //double q_conv = q_rad/2. * (-0.001129*v3 + 0.031229*v2 - 0.01822*v +0.962476);  //convection is about half radiation, scale by wind speed. surrogate regression from molten salt run.
+    //
+    //return max(1. - (q_rad + q_conv)/q_inc, 0.);
 
-    double T_amb = weather.m_tdry + 273.15;
-    double T_eff4 = T_eff * T_eff;
-    T_eff4 *= T_eff4;
-    double T_amb4 = T_amb * T_amb;
-    T_amb4 *= T_amb4;
-
-    double Arec = mc_pt_receiver.area_proj();
-
-    double q_rad = 5.67e-8*mc_pt_receiver.m_epsilon * Arec * (T_eff4 - T_amb4) * 1.e-6;   //MWt
-
-    double v = weather.m_wspd;
-    double v2 = v*v;
-    double v3 = v2*v;
-
-    double q_conv = q_rad/2. * (-0.001129*v3 + 0.031229*v2 - 0.01822*v +0.962476);  //convection is about half radiation, scale by wind speed. surrogate regression from molten salt run.
-
-    return max(1. - (q_rad + q_conv)/q_inc, 0.);
-
+    return mc_pt_receiver.estimate_thermal_efficiency(weather, q_inc);
 }
 
 
