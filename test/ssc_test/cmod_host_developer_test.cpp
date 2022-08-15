18c0927a4fa63cce574ac5655042a65e97ac7ac3
Steven Janzou <sjanzou@gmail.com>
2022-08-15 03:55:01 -0600

    host developer tests constructed
    Failing because "utility_bill_w_sys" missing from input to compute module.

---
+++ test/ssc_test/cmod_host_developer_test.cpp
@@ -0,0 +1,109 @@
+/**
+BSD-3-Clause
+Copyright 2019 Alliance for Sustainable Energy, LLC
+Redistribution and use in source and binary forms, with or without modification, are permitted provided
+that the following conditions are met :
+1.	Redistributions of source code must retain the above copyright notice, this list of conditions
+and the following disclaimer.
+2.	Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+and the following disclaimer in the documentation and/or other materials provided with the distribution.
+3.	Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER, CONTRIBUTORS, UNITED STATES GOVERNMENT OR UNITED STATES
+DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <gtest/gtest.h>
+
+#include "cmod_host_developer_test.h"
+
+
+TEST_F(CmodHostDeveloperTest, PVWatts) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_PVWatts_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_PVWatts_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
+TEST_F(CmodHostDeveloperTest, PVWattsBattery) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_PVWatts_Battery_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_PVWatts_Battery_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added", "cf_after_tax_net_equity_cost_flow", "cf_parasitic_cost", "cf_util_escal_rate" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
+TEST_F(CmodHostDeveloperTest, PV) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Flat_Plate_PV_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Flat_Plate_PV_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
+
+TEST_F(CmodHostDeveloperTest, PVBattery) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_PV_Battery_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_PV_Battery_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
+
+TEST_F(CmodHostDeveloperTest, Generic) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Generic_System_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Generic_System_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
+
+TEST_F(CmodHostDeveloperTest, GenericBattery) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Generic_Battery_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Generic_Battery_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
+
+
+TEST_F(CmodHostDeveloperTest, StandaloneBattery) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Standalone_Battery_Host_Developer_cmod_host_developer.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/host_developer/2022.08.08_develop_branch_Standalone_Battery_Host_Developer_cmod_host_developer_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "payback" };
+    std::vector<std::string> compare_array_variables = { "cf_after_tax_cash_flow", "cf_value_added", "cf_after_tax_net_equity_cost_flow", "cf_parasitic_cost", "cf_util_escal_rate" };
+
+    Test(file_inputs, file_outputs, compare_number_variables, compare_array_variables);
+}
+
