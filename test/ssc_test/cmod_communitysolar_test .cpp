266cd8f29e1f129c99d8cb15243df2e028113a20
sjanzou <sjanzou@gmail.com>
2022-08-22 04:13:54 -0600

    Community solar financial test

--- test/ssc_test/cmod_communitysolar_test .cpp
+++ test/ssc_test/cmod_communitysolar_test .cpp
@@ -19,44 +19,19 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON A
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#if defined( _WINDOWS) && defined(_DEBUG)
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-#endif
 
-#include <stdlib.h>
-#include <iostream>
 #include <gtest/gtest.h>
 
-// in order to get MS V2017 update 2 to build without a bunch of C4996 "std::tr1:warning..."
-#define _SILENCE_TR1_NAMESPACE_DEPRECIATION_WARNING
+#include "cmod_communitysolar_test.h"
 
-GTEST_API_ int main(int argc, char **argv) {
 
-#if defined( _WINDOWS) && defined(_DEBUG)
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
+TEST_F(CmodCommunitySolarTest, PVWatts) {
+    std::string file_inputs = SSCDIR;
+    file_inputs += "/test/input_json/FinancialModels/communitysolar/2022.08.08_develop_branch_PVWatts_Community_Solar_cmod_communitysolar.json";
+    std::string file_outputs = SSCDIR;
+    file_outputs += "/test/input_json/FinancialModels/communitysolar/2022.08.08_develop_branch_PVWatts_Community_Solar_cmod_communitysolar_outputs.json";
+    std::vector<std::string> compare_number_variables = { "lcoe_nom", "npv", "subscriber1_npv", "subscriber2_npv" };
+    std::vector<std::string> compare_array_variables = { "cf_project_return_aftertax", "cf_project_return_aftertax_npv" };
 
-	printf("Running main() from gtest_main.cc\n");
-	testing::InitGoogleTest(&argc, argv);
-    
-    //    filter to include
-    //    ::testing::GTEST_FLAG(filter) = "CMPvsamv1PowerIntegration_cmod_pvsamv1*";
-    //   ::testing::GTEST_FLAG(filter) = "CmodHostDeveloperTest.*:CmodCashLoanTest.*:CmodThirdPartyOwnershipTest.*";
-       ::testing::GTEST_FLAG(filter) = "CmodCommunitySolarTest.*";
-
-    //    filter to exclude
-    //    ::testing::GTEST_FLAG(filter) = "-PVSmoothing_lib_battery_dispatch*";
-
-    // run multiple test
-    //    ::testing::GTEST_FLAG(filter) = "CMPvwattsv8Integration_cmod_pvwattsv8.DefaultNoFinancialModel_cmod_pvwattsv8:CMPvwattsv8Integration_cmod_pvwattsv8.NonAnnual";
-
-    int status = RUN_ALL_TESTS();
-
-//    sleep(10); //used for single test instruments leak detector on macOS
-	
-    if (!status)
-		printf("Tests Pass!\n");
-	return status;
+    Test("communitysolar", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
