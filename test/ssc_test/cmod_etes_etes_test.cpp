5f4bafabdedcdce46f1af1a9aa40a69a2a62ad47
tyneises <ty.neises@nrel.gov>
2022-10-28 21:40:52 -0500

    add etes plus dispatch test

--- test/ssc_test/cmod_etes_etes_test.cpp
+++ test/ssc_test/cmod_etes_etes_test.cpp
@@ -36,13 +36,19 @@ NAMESPACE_TEST(etes_etes_test, EtesEtesCmod, Default_NoFinancial)
     etes_system.SetInput("is_dispatch", 0);
     int errors = etes_system.RunModule();
     double ann_energy = etes_system.GetOutput("annual_energy");
-    double abc = 1.23;
-    //int errors = 0;
     EXPECT_FALSE(errors);
     if (!errors) {
         EXPECT_NEAR_FRAC(std::abs(etes_system.GetOutput("annual_energy")), std::abs(- 746093232), kErrorToleranceHi);
     }
-    
+
+    etes_system.SetInput("is_dispatch", 1);
+    errors = etes_system.RunModule();
+    ann_energy = etes_system.GetOutput("annual_energy");
+    EXPECT_FALSE(errors);
+    if (!errors) {
+        EXPECT_NEAR_FRAC(std::abs(etes_system.GetOutput("annual_energy")), std::abs(-497825000 ), kErrorToleranceHi);
+    }
+
     //if (!errors) {
     //    EXPECT_NEAR_FRAC(power_tower.GetOutput("annual_energy"), 602247767, kErrorToleranceHi);
     //    EXPECT_NEAR_FRAC(power_tower.GetOutput("land_area_base_calc"), 1847, kErrorToleranceHi);
