0ba1cb1da7e99c55c794a5fccc2214433eab6131
Steven Janzou <steven@janzouconsulting.com>
2023-07-03 00:19:30 -0600

    Update hybrid / single owner test and match with SAM

--- test/ssc_test/cmod_hybrid_test.cpp
+++ test/ssc_test/cmod_hybrid_test.cpp
@@ -78,7 +78,7 @@ TEST_F(CmodHybridTest, PVWattsv8WindBatterySingleOwner) {
 
         auto hybrid_outputs = ssc_data_get_table(outputs, "Hybrid");
         ssc_data_get_number(hybrid_outputs, "project_return_aftertax_npv", &npv);
-        EXPECT_NEAR(npv, -54907, 54907 * 0.001);
+        EXPECT_NEAR(npv, -42392, 42392 * 0.001);
     }
     ssc_data_free(dat);
     dat = nullptr;
