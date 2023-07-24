b8d0304d2b186ad317fa63d43902135762844b8a
WHamilt2 <william.hamilton@nrel.gov>
2023-07-24 16:26:58 -0600

    adding particle tower cmod skeleton

--- ssc/csp_common.cpp
+++ ssc/csp_common.cpp
@@ -199,8 +199,57 @@ bool solarpilot_invoke::run(std::shared_ptr<weather_data_provider> wdata)
 
         rf->absorptance.val = 1.0;      // don't apply absorptivity in solarpilot for cavity receivers - performance model will do this
     }
+    else if (rec_type == 3) {
+        rf->rec_type.val = "Falling particle";
+        rf->rec_height.val = m_cmod->as_double("rec_height");
+        rf->rec_width.val = m_cmod->as_double("rec_width");
+
+        // Curtain dimenisons
+        rf->norm_curtain_height.val = m_cmod->as_double("norm_curtain_height");  // [-]
+        rf->norm_curtain_width.val = m_cmod->as_double("norm_curtain_width");    // [-]
+        rf->max_curtain_depth.val = m_cmod->as_double("max_curtain_depth");      // [m]
+        // Reading in normizalized curtain heights and depths
+        // TODO (Bill): needs to be fixed, doesn't seem to be importing correctly
+        util::matrix_t<double> norm_heights_depths = m_cmod->as_matrix("norm_heights_depths");  // [-]  
+        std::string format = "%f,%f;";
+        std::string nhd_input;
+        char row[200];
+        for (size_t i = 0; i < norm_heights_depths.nrows(); i++)
+        {
+            sprintf(row, format.c_str(), norm_heights_depths.at(i, 0), norm_heights_depths.at(i, 1));
+            nhd_input.append(row);
+        }
+        rf->norm_heights_depths.set_from_string(nhd_input.c_str());
+
+
+        // TODO (Bill): Do I need a helper function here to pre-calculate values like curtain height and width?
+        rf->curtain_total_height.Setval(rf->norm_curtain_height.val * rf->rec_height.val);
+        rf->max_curtain_width.Setval(rf->norm_curtain_width.val * rf->rec_width.val);
+
+        int curtain_type = m_cmod->as_integer("curtain_type");
+        if (curtain_type == 0) {
+            rf->curtain_type.val = "Flat";
+        }
+        else if (curtain_type == 1) {
+            rf->curtain_type.val = "Curved";
+            rf->curtain_radius.val = m_cmod->as_double("curtain_radius");    // [m]
+        }
+        else {
+            throw exec_error("solarpilot cmod", "curtain type must be 0 (flat) or 1 (curved)");
+        }
+
+        rf->is_snout.val = m_cmod->as_boolean("is_snout");
+        if (rf->is_snout.val) {
+            rf->snout_depth.val = m_cmod->as_double("snout_depth");                     // [m]
+            rf->snout_horiz_angle.val = m_cmod->as_double("snout_horiz_angle");         // [deg]
+            rf->snout_vert_bot_angle.val = m_cmod->as_double("snout_vert_bot_angle");   // [deg]
+            rf->snout_vert_top_angle.val = m_cmod->as_double("snout_vert_top_angle");   // [deg]
+        }
+
+        rf->absorptance.val = 1.0;      // don't apply absorptivity in solarpilot for falling particle receivers - performance model will do this
+    }
     else {
-        throw exec_error("solarpilot cmod", "receiver type must be 0 (external) or 1 (cavity)");
+        throw exec_error("solarpilot cmod", "receiver type must be 0 (external), 1 (cavity), or 3 (falling particle)");
     }
 
 	rf->therm_loss_base.val = m_cmod->as_double("rec_hl_perm2");
@@ -252,8 +301,15 @@ bool solarpilot_invoke::run(std::shared_ptr<weather_data_provider> wdata)
 	wdata->header(&hdr);
 
     // If cavity in southern hemisphere, then receiver faces south
-    if (rec_type == 1 && hdr.lat < 0) {
-        rf->rec_azimuth.val = 180;
+    if (rec_type == 1 || rec_type == 3) {
+        sf.is_opt_zoning.val = false;   // Turning off optical zoning for flat receivers
+        if (hdr.lat < 0) {
+            rf->rec_azimuth.val = 180;
+        }
+        else {
+            sf.accept_max.val = 90.0;
+            sf.accept_min.val = -90.0;
+        }
     }
 
     amb.latitude.val = hdr.lat;
@@ -382,6 +438,31 @@ bool solarpilot_invoke::run(std::shared_ptr<weather_data_provider> wdata)
                     if (!m_sapi->Optimize(/*opt.algorithm.mapval(),*/ optvars, upper, lower, stepsize, &names))
                         return false;
 
+                }
+                else if (rec_type == 3){
+
+                    int nv = 3;
+                    vector<double*> optvars(nv);
+                    vector<double> upper(nv, HUGE_VAL);
+                    vector<double> lower(nv, -HUGE_VAL);
+                    vector<double> stepsize(nv);
+                    vector<string> names(nv);
+
+                    //pointers
+                    optvars.at(0) = &sf.tht.val;
+                    optvars.at(1) = &recs.front().rec_height.val;   // TODO (Bill): Set-up receiver cost to use aperture area
+                    optvars.at(2) = &recs.front().rec_width.val;
+                    //names
+                    names.at(0) = (split(sf.tht.name, ".")).back();
+                    names.at(1) = (split(recs.front().rec_height.name, ".")).back();
+                    names.at(2) = (split(recs.front().rec_width.name, ".")).back();
+                    //step size
+                    stepsize.at(0) = sf.tht.val * opt.max_step.val;
+                    stepsize.at(1) = recs.front().rec_height.val * opt.max_step.val;
+                    stepsize.at(2) = recs.front().rec_width.val * opt.max_step.val;
+
+                    if (!m_sapi->Optimize(/*opt.algorithm.mapval(),*/ optvars, upper, lower, stepsize, &names))
+                        return false;
                 }
             }
 
@@ -694,7 +775,7 @@ double solarpilot_invoke::GetTotalLandArea()
 double solarpilot_invoke::GetBaseLandArea()
 {
     // Base land area occupied by heliostats [acres]
-    return land.bound_area.Val() / 4046.86 /*acres/m^2*/;  // [acres] Land area occupied by heliostats
+    return land.bound_area.Val();  // [acres] Land area occupied by heliostats
 }
 
 bool ssc_cmod_solarpilot_callback( simulation_info *siminfo, void *data )
