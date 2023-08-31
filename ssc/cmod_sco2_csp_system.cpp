a17ae9d647e3bf0e909d1170fb87d61e1d6335ba
tyneises <ty.neises@nrel.gov>
2023-08-31 08:21:05 -0500

    comment code to test for mac test failure

--- ssc/cmod_sco2_csp_system.cpp
+++ ssc/cmod_sco2_csp_system.cpp
@@ -334,8 +334,6 @@ public:
         //FILE* fp = fopen("sco2_cmod_to_lk.lk", "w");
         //write_cmod_to_lk_script(fp, m_vartab);
 
-        return;
-
         C_sco2_phx_air_cooler c_sco2_cycle;
 
 		int sco2_des_err = sco2_design_cmod_common(this, c_sco2_cycle);
