e2745551ea5730246b41fcb523949a73db567c21
Taylor Brown <taylor.brown75@outlook.com>
2024-09-03 14:31:07 -0600

    Swap tes_type integers of nt and packed bed

--- tcs/csp_solver_core.h
+++ tcs/csp_solver_core.h
@@ -718,6 +718,13 @@ class C_csp_tes
 
 public:
 
+    enum csp_tes_types
+    {
+        E_TES_TWO_TANK,
+        E_TES_PACKED_BED,
+        E_TES_NT
+    };
+
     // Class to save messages for up stream classes
     C_csp_messages mc_csp_messages;
 
