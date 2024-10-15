08c685193da3658a23c4bccc0f6e37168393bb9a
Paul Gilman <cpaulgilman@gmail.com>
2024-10-15 15:44:30 -0700

    fix error messages in util::hour_of_year()

--- shared/lib_util.cpp
+++ shared/lib_util.cpp
@@ -924,7 +924,6 @@ size_t util::hour_of_day(size_t hour_of_year)
 size_t util::hour_of_year(size_t month, size_t day, size_t hour)
 {
 	size_t h = 0;
-	bool ok = true;
 	std::vector<size_t> days_in_months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     //first add the days from all the preceding completed months
 	if (month >= 1 && month <= 12)
@@ -932,20 +931,21 @@ size_t util::hour_of_year(size_t month, size_t day, size_t hour)
 		for (size_t m = 0; m < (month - 1); m++)
 			h += days_in_months[m] * 24;
 	}
-	else ok = false;
+	else
+        throw std::runtime_error(util::format("unable to determine hour of year (0-8759) because month is out of range for time stamp month-day-hour: %d-%d-%d", month, day, hour));
     //then add days in the current month up to the current day
     if (day >= 1 && day <= days_in_months[month - 1])
         h += (day - 1) * 24;
     else if (month == 2 && day == 29) //special check for leap day present in data
         h += (27 * 24); //for leap day, repeat Feb 28 in annual indexes, because hour_of_year is used to index 8760 non-leap-year arrays.
-    else ok = false;
+    else
+        throw std::runtime_error(util::format("unable to determine hour of year (0 - 8759) because day is out of range for time stamp month-day-hour: %d-%d-%d", month, day, hour));
 	if (hour >= 0 && hour <= 23)
 		h += hour;
-	else ok = false;
+	else
+        throw std::runtime_error(util::format("unable to determine hour of year (0 - 8759) because hour is out of range for time stamp month-day-hour: %d-%d-%d", month, day, hour));
 	if (hour > 8759)
-	    throw std::runtime_error("hour_of_year range is (0-8759) but calculated hour is > 8759: " + std::to_string(hour));
-	if (!ok)
-		throw std::runtime_error("hour_of_year input month, day, or hour out of correct range for m-d-h: " + std::to_string(month) + "-" + std::to_string(day) + "-" + std::to_string(hour));
+	    throw std::runtime_error(util::format("unable to determine hour of year (0 - 8759) because hour is greater than 8759 for time stamp month-day-hour: %d-%d-%d", month, day, hour));
 	return h;
 }
 
