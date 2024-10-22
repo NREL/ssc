ab890dd570eb96a5bcf34ef908836fbe68337129
Paul Gilman <cpaulgilman@gmail.com>
2024-10-22 14:36:57 -0700

    Add IRR to cashloan and migrate functions to lib_financial.h (#1223)
    * initial move irr functions to lib_financial
    
    * Add IRR and Nominal Discount Rate outputs
    
    * Change some instances of fabs() to std::abs()
    
    Per https://github.com/NREL/SAM/issues/1223
    
    * Add NPV function to lib_financial
    
    Also clean up IRR code
    
    * Remove commented code
    
    * Replace irr() with libfin::irr()
    
    * Functions to lib_financial for all financial models
    
    * Code cleanup for functions to lib_financial
    
    Declare all functions in lib_financial.h
    
    Clean up comments and update URLs to code references
    
    * Define DBL_MAX in lib_financial.cpp
    
    * Clean up includes
    
    * Add comments for min() max() functions

--- shared/lib_financial.cpp
+++ shared/lib_financial.cpp
@@ -35,153 +35,248 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <limits>
 #include "lib_financial.h"
 
-using namespace libfin;
+#ifndef WIN32
+#include <float.h>
+#endif
 
-/* financial code here */
-/* ported from http://code.google.com/p/irr-newtonraphson-calculator/ */
-
-static bool is_valid_iter_bound(double estimatedReturnRate)
+// find minimum cash flow value when some values are NaN, e.g., DSCR is NaN after end of repayment period
+// https://stackoverflow.com/questions/72296440/how-to-prevent-stdmin-and-max-to-return-nan-if-the-first-element-of-the-array
+double libfin::min(double a, double b)
 {
-	return estimatedReturnRate != -1 && (estimatedReturnRate < std::numeric_limits<int>::max()) && (estimatedReturnRate > std::numeric_limits<int>::min());
+    if ((a != a) || (b != b))
+        return 0;
+    else
+        return (a < b) ? a : b;
 }
 
-static double irr_poly_sum(double estimatedReturnRate, const std::vector<double> &CashFlows, int Count)
+// find maximum cash flow value when some values are NaN, e.g., IRR is NaN in Year zero
+// https://stackoverflow.com/questions/72296440/how-to-prevent-stdmin-and-max-to-return-nan-if-the-first-element-of-the-array
+double libfin::max(double a, double b)
 {
-    double sumOfPolynomial = 0;
-    if (is_valid_iter_bound(estimatedReturnRate))
-	{
-		for (int j = 0; j < Count && j<(int)CashFlows.size(); j++)
-        {
-			double val = (pow((1 + estimatedReturnRate), j));
-			if (val != 0.0)
-				sumOfPolynomial += CashFlows[j]/val;
-			else
-				break;
-        }
-	}
-    return sumOfPolynomial;
+    if ((a != a) || (b != b))
+        return 0;
+    else
+        return (a > b) ? a : b;
 }
 
-static double irr_derivative_sum(double estimatedReturnRate, const std::vector<double> &CashFlows, int Count)
+
+// irs rounding rules https://www.irs.gov/pub/irs-pdf/n1036.pdf
+// original code from http://www.codeproject.com/Articles/58289/C-Round-Function.aspx
+long libfin::round_irs(double number)
 {
-    double sumOfDerivative = 0;
-    if (is_valid_iter_bound(estimatedReturnRate))
-		for (int i = 1; i < Count && i < (int)CashFlows.size(); i++)
-        {
-            sumOfDerivative += CashFlows[i]*(i)/pow((1 + estimatedReturnRate), i);
-        }
-    return sumOfDerivative*-1;
+    return (number >= 0) ? (long)(number + 0.5) : (long)(number - 0.5);
 }
 
-double libfin::irr(double tolerance, int maxIterations, const std::vector<double> &CashFlows, int Count)
+// irr functions
+// orignally ported from http://code.google.com/p/irr-newtonraphson-calculator/
+
+bool is_valid_iter_bound(double estimated_return_rate)
 {
-/* Validation check - can write to log if move to FinModel or include SimModel
-	if ((count < 2) || (CashFlows[0] > 0))
+    return estimated_return_rate != -1 && (estimated_return_rate < std::numeric_limits<int>::max()) && (estimated_return_rate > std::numeric_limits<int>::min());
+}
+
+double irr_poly_sum(double estimated_return_rate, const std::vector<double>& cf_vector, int count)
+{
+    double sum_of_polynomial = 0;
+    if (is_valid_iter_bound(estimated_return_rate))
     {
-		Messages.Add( "Cash flow for the first period  must be negative and there should");
+        for (int j = 0; j <= count; j++)
+        {
+            double val = (pow((1 + estimated_return_rate), j));
+            if (val != 0.0)
+                sum_of_polynomial += cf_vector.at(j) / val;
+            else
+                break;
+        }
     }
-*/
-	int numberOfIterations=0;
-	double calculatedIRR=0;
-	double initialGuess = 0.1; // 10% is default used in Excel IRR function
+    return sum_of_polynomial;
+}
 
-	if (CashFlows.size() < 3)
-		return initialGuess;
+double irr_derivative_sum(double estimated_return_rate, const std::vector<double>& cf_vector, int count)
+{
+    double sum_of_derivative = 0;
+    if (is_valid_iter_bound(estimated_return_rate))
+        for (int i = 1; i <= count; i++)
+        {
+            sum_of_derivative += cf_vector.at(i) * (i) / pow((1 + estimated_return_rate), i + 1);
+        }
+    return sum_of_derivative * -1;
+}
 
-    if ( (Count > 1) && (CashFlows[0] <= 0))
+double irr_scale_factor(const std::vector<double>& cf_vector_unscaled, int count)
+{
+    if (count < 1) return 1.0;
+    int i = 0;
+    double max = std::abs(cf_vector_unscaled.at(0));
+    for (i = 0; i <= count; i++)
+        if (std::abs(cf_vector_unscaled.at(i)) > max) max = std::abs(cf_vector_unscaled.at(i));
+    return (max > 0 ? max : 1);
+}
+
+bool is_valid_irr(const std::vector<double>& cf_vector, int count, double residual, double tolerance, int number_of_iterations, int max_iterations, double calculated_irr, double scale_factor)
+{
+    double npv_of_irr = libfin::npv(cf_vector, count, calculated_irr) + cf_vector.at(0);
+    double npv_of_irr_plus_delta = libfin::npv(cf_vector, count, calculated_irr + 0.001) + cf_vector.at(0);
+    bool is_valid = ((number_of_iterations < max_iterations) && (std::abs(residual) < tolerance) && (npv_of_irr > npv_of_irr_plus_delta) && (std::abs(npv_of_irr / scale_factor) < tolerance));
+    //if (!is_valid)
+    //{
+    //std::stringstream outm;
+    //outm <<  "cf_line=" << cf_line << "count=" << count << "residual=" << residual << "number_of_iterations=" << number_of_iterations << "calculated_irr=" << calculated_irr
+    //	<< "npv of irr=" << npv_of_irr << "npv of irr plus delta=" << npv_of_irr_plus_delta;
+    //log( outm.str() );
+    //}
+    return is_valid;
+}
+
+double irr_calc(const std::vector<double>& cf_vector, int count, double initial_guess, double tolerance, int max_iterations, double scale_factor, int& number_of_iterations, double& residual) {
+    double calculated_irr = std::numeric_limits<double>::quiet_NaN();
+    double deriv_sum = irr_derivative_sum(initial_guess, cf_vector, count);
+    if (deriv_sum != 0.0)
+        calculated_irr = initial_guess - irr_poly_sum(initial_guess, cf_vector, count) / deriv_sum;
+    else
+        return initial_guess;
+
+    number_of_iterations++;
+
+    residual = irr_poly_sum(calculated_irr, cf_vector, count) / scale_factor;
+
+    while (!(std::abs(residual) <= tolerance) && (number_of_iterations < max_iterations))
     {
-		double deriv_sum = irr_derivative_sum(initialGuess,CashFlows,Count);
-		if (deriv_sum != 0)
-			calculatedIRR = initialGuess - irr_poly_sum(initialGuess,CashFlows,Count)/deriv_sum;
-		else
-			return initialGuess;
+        deriv_sum = irr_derivative_sum(initial_guess, cf_vector, count);
+        if (deriv_sum != 0.0)
+            calculated_irr = calculated_irr - irr_poly_sum(calculated_irr, cf_vector, count) / deriv_sum;
+        else
+            break;
 
-		numberOfIterations++;
-		while (!(std::abs(irr_poly_sum(calculatedIRR,CashFlows,Count)) <= tolerance) && (numberOfIterations < maxIterations))
-		{
-			deriv_sum = irr_derivative_sum(initialGuess,CashFlows,Count);
-			if (deriv_sum != 0.0)
-				calculatedIRR = calculatedIRR - irr_poly_sum(calculatedIRR,CashFlows,Count)/deriv_sum;
-			else
-				break;
-
-			numberOfIterations++;
-		}
-	}
-    return calculatedIRR;
+        number_of_iterations++;
+        residual = irr_poly_sum(calculated_irr, cf_vector, count) / scale_factor;
+    }
+    return calculated_irr;
 }
 
-
-/*ported directly from Delphi simple geometric sum*/
-double libfin::npv(double Rate, const std::vector<double> &CashFlows, int Count) //, PaymentTime: TPaymentTime)
+double libfin::irr(const std::vector<double>& cf_vector, int count, double initial_guess, double tolerance, int max_iterations)
 {
-	/*
-{ Caution: The sign of NPV is reversed from what would be expected for standard
-   cash flows!}
-*/
-	if (Rate <= -1.0)
-	{
-		// TODO - throw exception?
-		return -999;
-	}
+    int number_of_iterations = 0;
+    double calculated_irr = std::numeric_limits<double>::quiet_NaN();
 
-	if (Count > (int)CashFlows.size())
-		Count = (int)CashFlows.size();
+    if (count < 1)
+        return calculated_irr;
 
-	double rr = 1/(1+Rate);
-	double result = 0;
-	for (int i=Count-1;i>0;i--)
-	{
-		result = rr * result + CashFlows[i];
-	}
-//	if PaymentTime = ptEndOfPeriod then result := rr * result;
-	return result*rr; // assumes end of period payments!!
+    // only possible for first value negative
+    if ((cf_vector.at(0) <= 0))
+    {
+        // initial guess from http://zainco.blogspot.com/2008/08/internal-rate-of-return-using-newton.html
+        if ((initial_guess < -1) && (count > 1)) // second order
+        {
+            if (cf_vector.at(0) != 0)
+            {
+                double b = 2.0 + cf_vector.at(1) / cf_vector.at(0);
+                double c = 1.0 + cf_vector.at(1) / cf_vector.at(0) + cf_vector.at(2) / cf_vector.at(0);
+                initial_guess = -0.5 * b - 0.5 * sqrt(b * b - 4.0 * c);
+                if ((initial_guess <= 0) || (initial_guess >= 1)) initial_guess = -0.5 * b + 0.5 * sqrt(b * b - 4.0 * c);
+            }
+        }
+        else if (initial_guess < 0) // first order
+        {
+            if (cf_vector.at(0) != 0) initial_guess = -(1.0 + cf_vector.at(1) / cf_vector.at(0));
+        }
+
+        double scale_factor = irr_scale_factor(cf_vector, count);
+        double residual = DBL_MAX;
+
+        calculated_irr = irr_calc(cf_vector, count, initial_guess, tolerance, max_iterations, scale_factor, number_of_iterations, residual);
+
+        if (!is_valid_irr(cf_vector, count, residual, tolerance, number_of_iterations, max_iterations, calculated_irr, scale_factor)) // try 0.1 as initial guess
+        {
+            initial_guess = 0.1;
+            number_of_iterations = 0;
+            residual = 0;
+            calculated_irr = irr_calc(cf_vector, count, initial_guess, tolerance, max_iterations, scale_factor, number_of_iterations, residual);
+        }
+
+        if (!is_valid_irr(cf_vector, count, residual, tolerance, number_of_iterations, max_iterations, calculated_irr, scale_factor)) // try -0.1 as initial guess
+        {
+            initial_guess = -0.1;
+            number_of_iterations = 0;
+            residual = 0;
+            calculated_irr = irr_calc(cf_vector, count, initial_guess, tolerance, max_iterations, scale_factor, number_of_iterations, residual);
+        }
+
+        if (!is_valid_irr(cf_vector, count, residual, tolerance, number_of_iterations, max_iterations, calculated_irr, scale_factor)) // try 0 as initial guess
+        {
+            initial_guess = 0;
+            number_of_iterations = 0;
+            residual = 0;
+            calculated_irr = irr_calc(cf_vector, count, initial_guess, tolerance, max_iterations, scale_factor, number_of_iterations, residual);
+        }
+
+        if (!is_valid_irr(cf_vector, count, residual, tolerance, number_of_iterations, max_iterations, calculated_irr, scale_factor)) // try 0.1 as initial guess
+        {
+            calculated_irr = std::numeric_limits<double>::quiet_NaN(); // did not converge
+        }
+
+    }
+    return calculated_irr;
 }
 
-double libfin::payback(const std::vector<double> &CumulativePayback, const std::vector<double> &Payback, int Count)
+double libfin::npv(const std::vector<double>& cf_vector, int nyears, double rate)
 {
-/*
-Return payback in years of inputs streams
-Payback occures when cumulative stream is > 0
-Find exact payback by subtracting cumulative / payback
-*/
-  double dPayback = 1e99; // report as > analysis period
-  bool bolPayback = false;
-  int iPayback = 0;
-  int i = 1;
-  while ((i<Count) && (!bolPayback))
-  {
-    if (CumulativePayback[i] > 0)
-	{
-      bolPayback = true;
-      iPayback = i;
-	}
-	i++;
-  }
+    //if (rate == -1.0) throw general_error("cannot calculate NPV with discount rate equal to -1.0");
+    double rr = 1.0;
+    if (rate != -1.0) rr = 1.0 / (1.0 + rate);
+    double result = 0;
+    for (int i = nyears; i > 0; i--)
+        result = rr * result + cf_vector.at(i);  //result = rr * result + cf.at(cf_line, i);
 
-  if (bolPayback)
+    return result * rr;
+}
+
+// Return payback in years of inputs streams
+// Payback occures when cumulative stream is > 0
+// Find exact payback by subtracting cumulative / payback
+double libfin::payback(const std::vector<double> &cumulative_payback, const std::vector<double> &payback, int nyears)
+{
+
+    double dpb = std::numeric_limits<double>::quiet_NaN();
+    bool bpb = false;
+    int ipb = 0;
+    int i = 1;
+    while ((i<nyears) && (!bpb))
+    {
+        if (cumulative_payback[i] > 0)
+	    {
+            bpb = true;
+            ipb = i;
+	    }
+	    i++;
+    }
+
+  if (bpb)
   {
-	  if (Payback[iPayback] !=0)
+	  if (payback[ipb] !=0)
 	  {
-		  dPayback = iPayback - CumulativePayback[iPayback] /Payback[iPayback];
+		  dpb = ipb - cumulative_payback[ipb] / payback[ipb];
 	  }
 	  else
 	  {
-		  dPayback = iPayback;
+		  dpb = ipb;
 	  }
   }
-  return dPayback;
+
+  return dpb;
 }
 
+// ppmpt and supporting functions
+/* original code source http://www.linkedin.com/answers/technology/software-development/TCH_SFT/445353-4527099?browseCategory=TCH_SFT
+updated link as of October 2024: https://corporatefinanceinstitute.com/resources/excel/ppmt-function/
 
-/* code source http://www.linkedin.com/answers/technology/software-development/TCH_SFT/445353-4527099?browseCategory=TCH_SFT
 Returns the payment on the principal for a given period for an investment based on periodic, constant payments and a constant interest rate.
 
 Syntax
 
-PPMT(rate,per,nper,pv,fv,type)
+libfin::ppmt(rate,per,nper,pv,fv,type)
 
-For a more complete description of the arguments in PPMT, see PV.
+For a more complete description of the arguments in libfin::ppmt, see PV.
 
 Rate   is the interest rate per period.
 
@@ -201,33 +296,31 @@ Set type equal to If payments are due
 
 Remark
 Make sure that you are consistent about the units you use for specifying rate and nper. If you make monthly payments on a four-year loan at 12 percent annual interest, use 12%/12 for rate and 4*12 for nper. If you make annual payments on the same loan, use 12% for rate and 4 for nper.
-
-
 */
-double libfin::pow1pm1 (double x, double y)
+double pow1pm1(double x, double y)
 {
 	return (x <= -1) ? pow (1 + x, y) - 1 : exp(y * log(1.0 + x)) - 1;
 }
-double libfin::pow1p (double x, double y)
+double pow1p(double x, double y)
 {
-	return (fabs (x) > 0.5) ? pow (1 + x, y) : exp (y * log(1.0 + x));
+	return (std::abs(x) > 0.5) ? pow (1 + x, y) : exp (y * log(1.0 + x));
 }
-double libfin::fvifa (double rate, double nper)
+double fvifa(double rate, double nper)
 {
 	return (rate == 0) ? nper : pow1pm1 (rate, nper) / rate;
 }
 
-double libfin::pvif (double rate, double nper)
+double pvif(double rate, double nper)
 {
 	return pow1p (rate, nper);
 }
 
-double libfin::pmt (double rate, double nper, double pv, double fv, int type)
+double pmt(double rate, double nper, double pv, double fv, int type)
 {
 	return ((-pv * pvif (rate, nper) - fv ) / ((1.0 + rate * type) * fvifa (rate, nper)));
 }
 
-double libfin::ipmt (double rate, double per, double nper, double pv, double fv, int type)
+double ipmt(double rate, double per, double nper, double pv, double fv, int type)
 {
 	double p = pmt (rate, nper, pv, fv, 0);
 	double ip = -(pv * pow1p (rate, per - 1) * rate + p * pow1pm1 (rate, per - 1));
@@ -243,8 +336,3 @@ double libfin::ppmt (double rate, double per, double nper, double pv, double fv,
 	return p - ip;
 }
 
-// from http://www.codeproject.com/Articles/58289/C-Round-Function.aspx
-long libfin::round_irs(double number)
-{
-    return (number >= 0) ? (long)(number + 0.5) : (long)(number - 0.5);
-}
