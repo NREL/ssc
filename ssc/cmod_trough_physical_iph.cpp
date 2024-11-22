8e67452b9fc5e52af5a8ca1b3951a79da3823af6
Taylor Brown <60201147+taylorbrown75@users.noreply.github.com>
2024-11-22 11:53:14 -0700

    Add physical heat sink variables to IPH trough cmod.

--- ssc/cmod_trough_physical_iph.cpp
+++ ssc/cmod_trough_physical_iph.cpp
@@ -169,7 +169,16 @@ static var_info _cm_vtab_trough_physical_iph[] = {
     { SSC_INPUT,        SSC_NUMBER,      "p_start",                   "Collector startup energy, per SCA",                                                "kWhe",       "",               "solar_field",    "*",                       "",                      "" },
 
     // Heat Sink
-    { SSC_INPUT,     SSC_NUMBER,         "pb_pump_coef",                "Pumping power to move 1kg of HTF through PB loop",                                      "kW/kg",               "",                              "Heat Sink",           "*",                "",                 "" },
+    { SSC_INPUT,     SSC_NUMBER,         "pb_pump_coef",              "Pumping power to move 1kg of HTF through PB loop",                                 "kW/kg",        "",               "Heat Sink",      "*",                       "",                      "" },
+
+    { SSC_INPUT,     SSC_NUMBER,         "hs_type",                   "0: ideal model, 1: physical steam model",                                          "",             "",               "Heat Sink",      "?=0",                     "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_N_sub",             "Number physical heat sink HX nodes",                                               "",             "",               "Heat Sink",      "hs_type=1",               "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_tol",               "Physical heat sink solve tolerance",                                               "",             "",               "Heat Sink",      "hs_type=1",               "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_f_mdot_steam_min",  "Min steam mdot fraction for physical heat sink",                                   "",             "",               "Heat Sink",      "hs_type=1",               "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_f_mdot_steam_max",  "Max steam mdot fraction for physical heat sink",                                   "",             "",               "Heat Sink",      "hs_type=1",               "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_T_steam_cold_des",  "Steam inlet temperature for physical heat sink",                                   "C",            "",               "Heat Sink",      "hs_type=1",               "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_P_steam_hot_des",   "Steam outlet (and inlet) pressure for physical heat sink",                         "bar",          "",               "Heat Sink",      "hs_type=1",               "",                      "" },
+    { SSC_INPUT,     SSC_NUMBER,         "hs_phys_Q_steam_hot_des",   "Steam outlet quality for physical heat sink",                                      "",             "",               "Heat Sink",      "hs_type=1",               "",                      "" },
 
     // Parallel heater parameters
     { SSC_INPUT,        SSC_NUMBER,      "heater_mult",                 "Heater multiple relative to design cycle thermal power",                         "-",            "",           "Parallel Heater",    "is_parallel_htr=1",              "",               "" },
@@ -1492,7 +1501,12 @@ public:
         }
 
         // Heat Sink
+        int hs_type = as_integer("hs_type");
+        C_csp_power_cycle* c_heat_sink_pointer;
         C_pc_heat_sink c_heat_sink;
+
+            // Ideal heat sink
+        if(hs_type == 0)
         {
             //size_t n_f_turbine1 = 0;
             //ssc_number_t* p_f_turbine1 = as_array("f_turb_tou_periods", &n_f_turbine1);   // heat sink, not turbine
@@ -1522,6 +1536,12 @@ public:
             c_heat_sink.mc_reported_outputs.assign(C_pc_heat_sink::E_M_DOT_HTF, allocate("m_dot_htf_heat_sink", n_steps_fixed), n_steps_fixed);
             c_heat_sink.mc_reported_outputs.assign(C_pc_heat_sink::E_T_HTF_IN, allocate("T_heat_sink_in", n_steps_fixed), n_steps_fixed);
             c_heat_sink.mc_reported_outputs.assign(C_pc_heat_sink::E_T_HTF_OUT, allocate("T_heat_sink_out", n_steps_fixed), n_steps_fixed);
+
+            c_heat_sink_pointer = &c_heat_sink;
+        }
+        else
+        {
+            throw exec_error("trough_physical_iph", "hs_type != 0; other heat sink models are not currently supported");
         }
 
         // Electricity pricing schedule
@@ -1665,7 +1685,7 @@ public:
         // Instantiate Solver
         C_csp_solver csp_solver(weather_reader, 
                                 c_trough,
-                                c_heat_sink,
+                                *c_heat_sink_pointer,
                                 *storage_pointer, 
                                 tou,
                                 dispatch,
