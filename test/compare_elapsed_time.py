a74e53e3d04c37bed7f1387aa5e6dad8659204f3
Darice <darice.guittet@nrel.gov>
2025-03-03 13:50:21 -0700

    update comparison

--- test/compare_elapsed_time.py
+++ test/compare_elapsed_time.py
@@ -16,6 +16,7 @@ Command Line Options to run this script in order to generate CSVs of time elapse
 'python compare_elapsed_time.py compare <path to csv or gtest log> <branch to compare>' 
     Compares the results from a previous run of Github actions by downloading the artifact csv of the given branch
 """
+access_token = os.getenv("GH_TOKEN")
 
 def convert_log_to_csv(gtest_log_path):
     test_groups = []
@@ -56,8 +57,6 @@ def convert_log_to_csv(gtest_log_path):
     return test_df
 
 def get_workflow_artifact_branch(base_branch):
-    access_token = os.getenv("GITHUB_TOKEN")
-
     headers = {
         'Accept': 'application/vnd.github+json',
         'Authorization': f'Bearer {access_token}',
@@ -66,7 +65,9 @@ def get_workflow_artifact_branch(base_branch):
 
     response = requests.get('https://api.github.com/repos/NREL/ssc/actions/artifacts', headers=headers)
 
-    print(response)
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception("Failed to Get Workflow Artifacts List")
 
     artifacts = response.json()['artifacts']
 
@@ -103,9 +104,30 @@ def get_workflow_artifact_branch(base_branch):
     os.remove(file_dir / "gtest_elapsed_times.csv")
     return test_df_base
     
-def compare_time_elapsed(new_test_df, base_test_df):
-    new_test_df.compare(base_test_df)
-    print(new_test_df.describe())
+def get_feature_branch():
+    workflow_id = os.getenv("WORKFLOW_ID")
+    if workflow_id is None:
+        raise Exception("Environment variable 'workflow_id' not defined")
+
+    headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f'Bearer {access_token}',
+    'X-GitHub-Api-Version': '2022-11-28',
+    }
+
+    response = requests.get(f'https://api.github.com/repos/NREL/ssc/actions/runs/{workflow_id}', headers=headers)
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception(f"Failed to Get Feature Branch from Workflow ID {workflow_id}")
+    
+    return response.json()['head_branch']
+
+
+def compare_time_elapsed(new_test_df, base_test_df, default_branch):
+    feature_branch = get_feature_branch()
+    compare_df = new_test_df.merge(base_test_df, how='outer', suffixes=[f" {feature_branch}", f" {default_branch}"], on=['Test Group', 'Test Name'])
+    print(compare_df)
+    compare_df.to_csv(Path(__file__).parent / "compare_times.csv", index=False)
 
 
 if __name__ == "__main__":
@@ -126,12 +148,13 @@ if __name__ == "__main__":
             raise RuntimeError("Provide path to csv or gtest log file, and the name of the branch for comparison")
         filename = Path(sys.argv[2])
         base_branch = sys.argv[3]
+        base_test_df = get_workflow_artifact_branch(base_branch)
         if Path(filename).suffix != ".csv":
             test_df = convert_log_to_csv(filename)
-            base_test_df = get_workflow_artifact_branch(base_branch)
-            compare_time_elapsed(test_df, base_test_df)
+        else:
+            test_df = pd.read_csv(filename)
+        compare_time_elapsed(test_df, base_test_df, default_branch=base_branch)
     else:
         raise RuntimeError("Options are 'gtest_log' or 'compare'. Use 'help' to see details")
  
         
-
