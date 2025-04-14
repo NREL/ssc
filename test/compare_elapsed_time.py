8fafd753f5ad4842e89ff34df1e529287de0ab12
Darice L Guittet <dguittet@nrel.gov>
2025-04-14 12:25:51 -0700

    Compare tests (#1289)
    * add compare_elapsed_time.py
    
    * fix
    
    * upload artifacts separately
    
    * add functions
    
    * add test_times.yml
    
    * access context
    
    * fix context
    
    * download after workflow
    
    * temp gtest_filter to test quickly
    
    * add comparison
    
    * update
    
    * remove filter and fix names
    
    * remove filter and fix names
    
    * fix windows upload
    
    * Merge remote-tracking branch 'origin/patch' into compare_tests
    
    * update comparison
    
    * Add comparison
    
    * convert to float
    
    * always upload
    
    * fix windows
    
    * add test times
    
    * fix naming
    
    * compare with release
    
    * add release.yml
    
    * switch base to develop
    
    * fix REL_DIFF_TOL
    
    * during release: add retries to request for test csv artifact download

--- test/compare_elapsed_time.py
+++ test/compare_elapsed_time.py
@@ -5,6 +5,7 @@ import sys
 import os
 import zipfile
 import io
+import time
 from platform import processor
 
 help_info = """
@@ -15,7 +16,27 @@ Command Line Options to run this script in order to generate CSVs of time elapse
 
 'python compare_elapsed_time.py compare <path to csv or gtest log> <branch to compare>' 
     Compares the results from a previous run of Github actions by downloading the artifact csv of the given branch
+
+'python compare_elapsed_time.py download_csv <github sha> <output_dir>'
+    Downloads the results from workflow for the given SHA and saves it to output dir
 """
+access_token = os.getenv("GH_TOKEN")
+
+tested_platforms = ["Windows", "Mac Arm", "Mac Intel", "Linux"]
+
+platform = None
+if sys.platform == 'linux':
+    platform = "Linux"
+elif sys.platform == 'win32':
+    platform = "Windows"
+elif sys.platform == 'darwin':
+    proc = processor()
+    if "x86_64" in proc:
+        platform = "Mac Intel"
+    elif "arm" in proc:
+        platform = "Mac Arm"
+else:
+    raise RuntimeError(f"Unrecognized platform {sys.platform}")
 
 def convert_log_to_csv(gtest_log_path):
     test_groups = []
@@ -55,9 +76,8 @@ def convert_log_to_csv(gtest_log_path):
     print(f"Output csv: {out_path}")
     return test_df
 
-def get_workflow_artifact_branch(base_branch):
-    access_token = os.getenv("GITHUB_TOKEN")
 
+def get_workflow_artifact_branch(base_branch):
     headers = {
         'Accept': 'application/vnd.github+json',
         'Authorization': f'Bearer {access_token}',
@@ -66,25 +86,13 @@ def get_workflow_artifact_branch(base_branch):
 
     response = requests.get('https://api.github.com/repos/NREL/ssc/actions/artifacts', headers=headers)
 
-    print(response)
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception("Failed to Get Workflow Artifacts List")
 
     artifacts = response.json()['artifacts']
 
     artifacts = [a for a in artifacts if a['workflow_run']['head_branch'] == base_branch]
-
-    platform = None
-    if sys.platform == 'linux':
-        platform = "Linux"
-    elif sys.platform == 'win32':
-        platform = "Windows"
-    elif sys.platform == 'darwin':
-        proc = processor()
-        if "x86_64" in proc:
-            platform = "Mac Intel"
-        elif "arm" in proc:
-            platform = "Mac Arm"
-    else:
-        raise RuntimeError(f"Unrecognized platform {sys.platform}")
     
     artifacts = [a for a in artifacts if (platform in a['name']) and ("Test Time Elapsed" in a['name'])]
 
@@ -102,9 +110,133 @@ def get_workflow_artifact_branch(base_branch):
     test_df_base = pd.read_csv(file_dir / "gtest_elapsed_times.csv")
     os.remove(file_dir / "gtest_elapsed_times.csv")
     return test_df_base
+
+def retry_request_with_timeout(url, timeout, headers, sha, retry_delay=20 * 60):
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            artifacts = response.json()['artifacts']
+            artifacts_sha = [a for a in artifacts if a['workflow_run']['head_sha'] == sha]
+            if len(artifacts_sha) > 0:
+                artifacts_count = 0
+                for platform in tested_platforms:
+                    artifacts = [a for a in artifacts_sha if (platform in a['name']) and ("Test Time Elapsed" in a['name'])]
+                    if len(artifacts):
+                        artifacts_count += 1
+                if artifacts_count == len(tested_platforms):
+                    return artifacts_sha
+        else:
+            print(response.json())
+            raise requests.exceptions.RequestException
+        print(f"Request failed: retrying in {retry_delay} seconds...")
+        time.sleep(retry_delay)
+    raise TimeoutError(f"Request to {url} timed out after {timeout} seconds")
+
+def get_artifact_from_sha(sha, output_dir=None):
+    """
+    Give 3 hours for the tests for finish
+    """
+
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {access_token}',
+        'X-GitHub-Api-Version': '2022-11-28',
+    }
+    try:
+        artifacts_sha = retry_request_with_timeout('https://api.github.com/repos/NREL/ssc/actions/artifacts', 60 * 60 * 3, headers, sha)
+    except TimeoutError as e:
+        print(e)
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed after multiple retries: {e}")
     
-def compare_time_elapsed(new_test_df, base_test_df):
-    new_test_df.compare(base_test_df)
+    for platform in tested_platforms:
+        artifacts = [a for a in artifacts_sha if (platform in a['name']) and ("Test Time Elapsed" in a['name'])]
+
+        headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {access_token}',
+        'X-GitHub-Api-Version': '2022-11-28',
+        }
+
+        response = requests.get(artifacts[0]['archive_download_url'], headers=headers)
+
+        z = zipfile.ZipFile(io.BytesIO(response.content)) 
+        file_dir = Path(__file__).parent
+        z.extractall(file_dir)
+        test_df_base = pd.read_csv(file_dir / "gtest_elapsed_times.csv")
+        os.remove(file_dir / "gtest_elapsed_times.csv")
+        if output_dir is not None:
+            test_df_base.to_csv(output_dir / f"gtest_elapsed_times_{platform}.csv", index=False)
+            print(f"Saved to {str(output_dir / f'gtest_elapsed_times_{platform}.csv')}")
+    return
+
+def get_feature_branch():
+    workflow_id = os.getenv("WORKFLOW_ID")
+    if workflow_id is None:
+        raise Exception("Environment variable 'workflow_id' not defined")
+
+    headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f'Bearer {access_token}',
+    'X-GitHub-Api-Version': '2022-11-28',
+    }
+
+    response = requests.get(f'https://api.github.com/repos/NREL/ssc/actions/runs/{workflow_id}', headers=headers)
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception(f"Failed to Get Feature Branch from Workflow ID {workflow_id}")
+    
+    return response.json()['head_branch']
+
+
+def compare_df(new_test_df, base_test_df, new_name, base_name, diff_threshold, diff_rel):
+    compare_df = new_test_df.merge(base_test_df, how='outer', suffixes=[f" {new_name}", f" {base_name}"], on=['Test Group', 'Test Name'])
+    
+    feat_col = f"Test Times [ms] {new_name}"
+    def_col = f"Test Times [ms] {base_name}"
+    compare_df = compare_df[(compare_df[feat_col] > diff_threshold) & (compare_df[def_col] > diff_threshold)] 
+
+    compare_df.loc[:, f"Diff {base_name} [ms]"] = compare_df[feat_col] - compare_df[def_col]
+    compare_df = compare_df[compare_df[f"Diff {base_name} [ms]"] != 0]
+
+    if len(compare_df) == 0:
+        return True, compare_df
+
+    compare_df.loc[compare_df[def_col] == 0, f"Diff {base_name} [%]"] = 0
+    compare_df.loc[compare_df[def_col] != 0, f"Diff {base_name} [%]"] = round((compare_df[feat_col] - compare_df[def_col]) / compare_df[def_col] * 100, 2)
+
+    compare_df = compare_df[compare_df[f"Diff {base_name} [%]"] >= diff_rel * 100]
+    if len(compare_df) == 0:
+        return True, compare_df
+    else:
+        return False, compare_df
+
+
+def compare_time_elapsed(new_test_df, head_test_df, head_branch):
+    diff_tol_rel = float(os.getenv("REL_DIFF_TOL"))
+    diff_tol_head = float(os.getenv("HEAD_DIFF_TOL"))
+    diff_threshold = float(os.getenv("DIFF_THRESHOLD_MS"))
+    feature_branch = get_feature_branch()
+    if feature_branch == head_branch:
+        return True
+    
+    pass_head, compare_head_df = compare_df(new_test_df, head_test_df, feature_branch, head_branch, diff_threshold, diff_tol_head)
+
+    rel_test_df = pd.read_csv(Path(__file__).parent / "elapsed_time_release" / f"gtest_elapsed_times_{platform}.csv")
+    pass_rel, compare_rel_df = compare_df(new_test_df, rel_test_df, feature_branch, "Release", diff_threshold, diff_tol_rel)
+
+    if pass_head and pass_rel:
+        return True
+    
+    comare_df = pd.merge(compare_head_df, compare_rel_df, how='outer')
+    comare_df.to_csv(Path(__file__).parent / "failing_test_times.csv", index=False)
+
+    if len(comare_df) > 0:
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
@@ -123,14 +255,26 @@ if __name__ == "__main__":
     elif sys.argv[1] == "compare":
         if len(sys.argv) < 4:
             raise RuntimeError("Provide path to csv or gtest log file, and the name of the branch for comparison")
+        
         filename = Path(sys.argv[2])
         base_branch = sys.argv[3]
+        head_test_df = get_workflow_artifact_branch(base_branch)
         if Path(filename).suffix != ".csv":
             test_df = convert_log_to_csv(filename)
-            base_test_df = get_workflow_artifact_branch(base_branch)
-            compare_time_elapsed(test_df, base_test_df)
+        else:
+            test_df = pd.read_csv(filename)
+        if compare_time_elapsed(test_df, head_test_df, head_branch=base_branch):
+            print('Pass')
+        else:
+            print('Fail')
+    elif sys.argv[1] == "download_csv":
+        if len(sys.argv) < 3:
+            raise RuntimeError("Provide the GitHub SHA to download the artifact")
+        sha = sys.argv[2]
+        output_dir = None
+        if len(sys.argv) == 4:
+            output_dir = Path(sys.argv[3])
+        get_artifact_from_sha(sha, output_dir)
     else:
         raise RuntimeError("Options are 'gtest_log' or 'compare'. Use 'help' to see details")
  
-        
-
