f992c1d64d976c7734844c2b63d69380c92c1f82
Steven Janzou <steven@janzouconsulting.com>
2025-05-12 03:41:25 -0600

    benchmarking partnership flip tests

--- test/main.cpp
+++ test/main.cpp
@@ -61,7 +61,8 @@ GTEST_API_ int main(int argc, char **argv) {
     //::testing::GTEST_FLAG(filter) = "CMGeothermal.*";
     //::testing::GTEST_FLAG(filter) = "csp_tower.PowerTowerCmod.Default_NoFinancial";
     //::testing::GTEST_FLAG(filter) = "CMPvsamv1PowerIntegration_cmod_pvsamv1.NoFinancialModelSystemDesign";
-    ::testing::GTEST_FLAG(filter) = "CmodCashLoanTest*:CmodSingleOwnerTest*";
+    //::testing::GTEST_FLAG(filter) = "CmodCashLoanTest*:CmodSingleOwnerTest*";
+    ::testing::GTEST_FLAG(filter) = "CmodLeveragedPartnershipFlipTest*:CmodAllEquityPartnershipFlipTest*";
     //::testing::GTEST_FLAG(filter) = "Solesca*";
     //::testing::GTEST_FLAG(filter) = "etes_ptes_test*";
     //::testing::GTEST_FLAG(filter) = "csp_tower.PowerTowerCmod*";
