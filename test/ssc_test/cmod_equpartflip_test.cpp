9b363bf1b8513323ae37e5f3a6ae8036c4912f1d
Steven Janzou <steven@janzouconsulting.com>
2025-05-12 04:28:58 -0600

    increasing coverage

--- test/ssc_test/cmod_equpartflip_test.cpp
+++ test/ssc_test/cmod_equpartflip_test.cpp
@@ -68,7 +68,7 @@ TEST_F(CmodAllEquityPartnershipFlipTest, EmpiricalTrough) {
 
     Test("equpartflip", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
-
+*/
 
 TEST_F(CmodAllEquityPartnershipFlipTest, PV) {
     std::string file_inputs = SSCDIR;
@@ -80,7 +80,7 @@ TEST_F(CmodAllEquityPartnershipFlipTest, PV) {
 
     Test("equpartflip", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
-*/
+/*
 
 TEST_F(CmodAllEquityPartnershipFlipTest, CustomGenerationBattery) {
     std::string file_inputs = SSCDIR;
@@ -92,7 +92,7 @@ TEST_F(CmodAllEquityPartnershipFlipTest, CustomGenerationBattery) {
 
     Test("equpartflip", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
-/*
+*/
 TEST_F(CmodAllEquityPartnershipFlipTest, GenericCSP) {
     std::string file_inputs = SSCDIR;
     file_inputs += "/test/input_json/FinancialModels/equpartflip/2023.10.27_om-expense-cash-flow_Generic_CSP_System_All_Equity_Partnership_Flip_cmod_equpartflip.json";
@@ -115,7 +115,7 @@ TEST_F(CmodAllEquityPartnershipFlipTest, CustomGeneration) {
     Test("equpartflip", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
 
-*/
+
 TEST_F(CmodAllEquityPartnershipFlipTest, Geotherrmal) {
     std::string file_inputs = SSCDIR;
     file_inputs += "/test/input_json/FinancialModels/equpartflip/2023.10.27_om-expense-cash-flow_Geothermal_Power_All_Equity_Partnership_Flip_cmod_equpartflip.json";
@@ -150,7 +150,7 @@ TEST_F(CmodAllEquityPartnershipFlipTest, MSLF) {
     Test("equpartflip", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
 
-*/
+
 TEST_F(CmodAllEquityPartnershipFlipTest, MSPT) {
     std::string file_inputs = SSCDIR;
     file_inputs += "/test/input_json/FinancialModels/equpartflip/2023.10.27_om-expense-cash-flow_MSPT_All_Equity_Partnership_Flip_cmod_equpartflip.json";
@@ -161,7 +161,7 @@ TEST_F(CmodAllEquityPartnershipFlipTest, MSPT) {
 
     Test("equpartflip", file_inputs, file_outputs, compare_number_variables, compare_array_variables);
 }
-/*
+
 TEST_F(CmodAllEquityPartnershipFlipTest, PhysicalTrough) {
     std::string file_inputs = SSCDIR;
     file_inputs += "/test/input_json/FinancialModels/equpartflip/2023.10.27_om-expense-cash-flow_Physical_Trough_All_Equity_Partnership_Flip_cmod_equpartflip.json";
